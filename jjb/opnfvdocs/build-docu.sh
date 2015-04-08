bb53ad031cefd54b306e04cf1f4690acd4e9e4f2
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-08 15:05:07 +0300

    JIRA: DOCU-2 - Adding a job to build documentation from .rst files
    This job will use shell to build documentation on Spirent's lab slave
    that has docutils installed
    
    Change-Id: I3a049b3e4a70e77f3449f5f9b38b8a52a4b55fac
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

---
+++ jjb/opnfvdocs/build-docu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for file in $(find . -type f -iname '*.rst'); do
+	file_cut="${file%.*}"
+        html_file=$file_cut".html"
+        pdf_file=$file_cut".pdf"
+        rst2html $file > $html_file
+        rst2pdf $file -o $pdf_file
+done
