c42455af4f0cf3502f16b2d017cd98df038f83e8
MatthewLi <matthew.lijun@huawei.com>
2015-04-14 19:22:56 +0800

    JIRA: DOCS-6 - Add a build shell and a JJB for Octopus project This job will be used to build documents
    Change-Id: If4f1e2853b8bce48145a842488bed01ac9b93ab0
    Signed-off-by: MatthewLi <matthew.lijun@huawei.com>

---
+++ jjb/octopus/build-docu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for file in $(find . -type f -iname '*.rst'); do
+         file_cut="${{file%.*}}"
+         html_file=$file_cut".html"
+         pdf_file=$file_cut".pdf"
+         rst2html $file > $html_file
+         rst2pdf $file -o $pdf_file
+done
+v
+v
+
