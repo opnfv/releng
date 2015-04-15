818c51ca2d7d5c98b2da7722064b22c35c7c920e
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-15 17:46:54 +0300

    Changed build-docu.sh for genesis project
    JIRA: genesis is special in nature and it might contain various documentations
    that should go under specific <installer>/docs/ after it gets built
    
    Change-Id: I1e6fc9307086fd3c332addb61266992f1fb25ddb
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

--- jjb/genesis/build-docu.sh
+++ jjb/genesis/build-docu.sh
@@ -6,4 +6,7 @@ for file in $(find . -type f -iname '*.rst'); do
         pdf_file=$file_cut".pdf"
         rst2html $file > $html_file
         rst2pdf $file -o $pdf_file
+	gs_cp_folder=$(echo $file| cut -d "/" -f2,3)
+	/usr/local/bin/gsutil cp $html_file gs://artifacts.opnfv.org/genesis/$gs_cp_folder/
+	/usr/local/bin/gsutil cp $pdf_file gs://artifacts.opnfv.org/genesis/$gs_cp_folder/
 done
