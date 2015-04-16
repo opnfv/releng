38cb2c967ec8bd8a3d2817cce076fad607bb5040
Aric Gardner <agardner@linuxfoundation.org>
2015-04-16 11:58:52 -0400

    Update build-docu.sh to set metadata
    Other changes:
    set project variable (in this case project="genesis")
    Creates a null delimited array
    iterates the array
    no longer writes pfd and html files to disk (uses sdtout)
    logs and echos results of gsutil cp
    turned off -vx and added some verbosity with echo and cat
    
    Change-Id: Ia9f329a9b7d45d458ecd5f7398f968d556bda053
    JIRA: DOCS-14
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

--- jjb/genesis/build-docu.sh
+++ jjb/genesis/build-docu.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
-set -xv
-for file in $(find . -type f -iname '*.rst'); do
-        file_cut="${{file%.*}}"
-        html_file=$file_cut".html"
-        pdf_file=$file_cut".pdf"
-        rst2html $file > $html_file
-        rst2pdf $file -o $pdf_file
-	gs_cp_folder=$(echo $file| cut -d "/" -f2,3)
-	/usr/local/bin/gsutil cp $html_file gs://artifacts.opnfv.org/genesis/$gs_cp_folder/
-	/usr/local/bin/gsutil cp $pdf_file gs://artifacts.opnfv.org/genesis/$gs_cp_folder/
+project="genesis"
+export PATH=$PATH:/usr/local/bin/
+
+files=()
+while read -r -d ''; do
+      files+=("$REPLY")
+done < <(find . -type f -iname '*.rst' -print0)
+
+for file in "${{files[@]}}"; do
+
+  file_cut="${{file%.*}}"
+  gs_cp_folder="$(echo "$file"| cut -d "/" -f2,3)"
+
+  html_file=$file_cut".html"
+
+    echo "rst2html $file"
+    rst2html $file | gsutil cp -L gsoutput.txt - \
+    gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$html_file")
+    gsutil setmeta -h "Content-Type:text/html" \
+                   -h "Cache-Control:private, max-age=0, no-transform" \
+                   gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$html_file")
+    cat gsoutput.txt
+
+  pdf_file="$file_cut"".pdf"
+
+    echo "rst2pdf $file"
+    rst2pdf "$file" -o - | gsutil cp -L gsoutput.txt - \
+    gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$pdf_file")
+    gsutil setmeta -h "Content-Type:application/pdf" \
+                   -h "Cache-Control:private, max-age=0, no-transform" \
+                   gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$pdf_file")
+    cat gsoutput.txt
+
 done
+
+rm -f gsoutput.txt
