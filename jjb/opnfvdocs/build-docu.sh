285302e7a3b366f56fada09682dd483c160e2ef9
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-17 18:33:58 +0300

    Update [3] on build-docu.sh for opnfvdocs project, added sha & date
    JIRA: Added sha1 & date injection &
    eliminating "./" in the path of files
    
    Change-Id: I153ff2f4efe7226887283923896202a59bfe5d3d
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

--- jjb/opnfvdocs/build-docu.sh
+++ jjb/opnfvdocs/build-docu.sh
@@ -2,16 +2,24 @@
 project="opnfvdocs"
 export PATH=$PATH:/usr/local/bin/
 
+git_sha1=${{git rev-parse HEAD}}
+git_date=${{date}}
+
 files=()
 while read -r -d ''; do
       files+=("$REPLY")
 done < <(find . -type f -iname '*.rst' -print0)
 
-for file in "${{files[@]}}"; do
+for file in "${{files[@]//"./"/}}"; do
 
   file_cut="${{file%.*}}"
-  gs_cp_folder="${{file_cut#/}}"
+  gs_cp_folder="${{file_cut}}"
 
+    # sed part
+    sed -i "s/_sha1_/$git_sha1/g" $full_file_clean
+    sed -i "s/_date_/$git_date/g" $full_file_clean
+
+    # rst2html part
     echo "rst2html $file"
     rst2html $file | gsutil cp -L gsoutput.txt - \
     gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".html
@@ -21,7 +29,7 @@ for file in "${{files[@]}}"; do
     cat gsoutput.txt
 
     echo "rst2pdf $file"
-    rst2pdf "$file" -o - | gsutil cp -L gsoutput.txt - \
+    rst2pdf $fle -o - | gsutil cp -L gsoutput.txt - \
     gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".html
     gsutil setmeta -h "Content-Type:application/pdf" \
                    -h "Cache-Control:private, max-age=0, no-transform" \
