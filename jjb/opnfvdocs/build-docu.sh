eb98d2c38bfe8d2461429a457cf984e3d05bcaa1
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-17 13:01:36 +0300

    Fix setmeta cache to 0 and exclude empty folders at document conversion
    JIRA: DOCS-14, DOCS-13 - Fix setmeta cache to 0 and exclude empty folders at document conversion
    by changing how gsutils copies files, sets metadata and excludes empty folders
    
    Change-Id: I92626c7a56a572d38196f9c074023d91fdbed331
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

--- jjb/opnfvdocs/build-docu.sh
+++ jjb/opnfvdocs/build-docu.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 set -xv
-for file in $(find . -type f -iname '*.rst'); do
+project="opnfvdocs"
+export PATH=$PATH:/usr/local/bin/
+
+files=()
+while read -r -d ''; do
+      files+=("$REPLY")
+done < <(find . -type f -iname '*.rst' -print0)
+
+for file in "${{files[@]}}"; do
 	file_cut="${{file%.*}}"
-        html_file=$file_cut".html"
-        pdf_file=$file_cut".pdf"
-        rst2html $file > $html_file
-        rst2pdf $file -o $pdf_file
+	gs_cp_folder=$(dirname "${file}")
+
+	html_file=$file_cut".html"
+		echo "rst2html $file"
+		rst2html $file | gsutil cp -L gsoutput.txt - \
+    		gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$html_file")
+		# The 3 lines below are used just to set medatada in order to reduce cache age to 0
+    		gsutil setmeta -h "Content-Type:text/html" \
+				-h "Cache-Control:private, max-age=0, no-transform" \
+                     	       gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$html_file")
+	cat gsoutput.txt
+
+	pdf_file="$file_cut"".pdf"
+
+	        echo "rst2pdf $file"
+		rst2pdf "$file" -o - | gsutil cp -L gsoutput.txt - \
+		gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$pdf_file")
+                # The 3 lines below are used just to set medatada in order to reduce cache age to 0
+		gsutil setmeta -h "Content-Type:application/pdf" \
+	        		-h "Cache-Control:private, max-age=0, no-transform" \
+		gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder"/$(basename "$pdf_file")
+	cat gsoutput.txt
+
 done
+
+rm -f gsoutput.txt
