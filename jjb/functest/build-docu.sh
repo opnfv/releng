f66afcfea0b88f07f44ff3d68620ebc1b398cb02
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-20 09:54:51 +0300

    Expanding artifacts management for R1 inside build-docu.sh
    JIRA: DOCU-20 - Expanding artifacts management for R1 inside build-docu.sh
    
    Change-Id: I4e8ac6bce3f6c47aff56e89e8133d2b4e547ff87
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

--- jjb/functest/build-docu.sh
+++ jjb/functest/build-docu.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
-set -xv
-for file in $(find . -type f -iname '*.rst'); do
-        file_cut="${{file%.*}}"
-        html_file=$file_cut".html"
-        pdf_file=$file_cut".pdf"
-        rst2html $file > $html_file
-        rst2pdf $file -o $pdf_file
+project="functest"
+export PATH=$PATH:/usr/local/bin/
+
+git_sha1="git rev-parse HEAD"
+git_date="date"
+
+files=()
+while read -r -d ''; do
+      files+=("$REPLY")
+done < <(find * -type f -iname '*.rst' -print0)
+
+for file in "${{files[@]}}"; do
+
+  file_cut="${{file%.*}}"
+  gs_cp_folder="${{file_cut}}"
+
+    # sed part
+    sed -i "s/_sha1_/$git_sha1/g" $file
+    sed -i "s/_date_/$git_date/g" $file
+
+    # rst2html part
+    echo "rst2html $file"
+    rst2html $file | gsutil cp -L gsoutput.txt - \
+    gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".html
+    gsutil setmeta -h "Content-Type:text/html" \
+                   -h "Cache-Control:private, max-age=0, no-transform" \
+                   gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".html
+    cat gsoutput.txt
+    rm -f gsoutput.txt
+
+    echo "rst2pdf $file"
+    rst2pdf $file -o - | gsutil cp -L gsoutput.txt - \
+    gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".pdf
+    gsutil setmeta -h "Content-Type:application/pdf" \
+                   -h "Cache-Control:private, max-age=0, no-transform" \
+                   gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".pdf
+    cat gsoutput.txt
+    rm -f gsoutput.txt
+
 done
+
