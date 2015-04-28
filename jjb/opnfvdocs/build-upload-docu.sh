7582497a68eeee1149e38bc306b12655cd092128
Victor Laza <vlaza@cloudbasesolutions.com>
2015-04-28 11:19:43 +0300

    Content-Type:image/jpeg in build-upload-docu.sh for images
    JIRA: stdin with cat "$img" for image upload in build-upload-docu.sh
    and Content-Type:image/jpeg for images
    
    Change-Id: I81a8d97d6ad784890eabe4d4ebb549cfb3bec541
    Signed-off-by: Victor Laza <vlaza@cloudbasesolutions.com>

--- jjb/opnfvdocs/build-upload-docu.sh
+++ jjb/opnfvdocs/build-upload-docu.sh
@@ -49,9 +49,9 @@ for img in "${{images[@]}}"; do
 
 	# uploading found images
 	echo "uploading $img"
-        echo "$img" | gsutil cp -L gsoutput.txt - \
+        cat "$img" | gsutil cp -L gsoutput.txt - \
         gs://artifacts.opnfv.org/"$project"/"$img"
-        gsutil setmeta -h "Content-Type:text/html" \
+        gsutil setmeta -h "Content-Type:image/jpeg" \
                         -h "Cache-Control:private, max-age=0, no-transform" \
                         gs://artifacts.opnfv.org/"$project"/"$img"
         cat gsoutput.txt
