f3e806a2d5841c67897a4e11c7b7b0da8a93ea13
Aric Gardner <agardner@linuxfoundation.org>
2015-05-27 12:09:04 -0400

    Missed adding $patchset in two places
    JIRA:DOCS-36
    Change-Id: I8baa764cfa4c668a71c9bea018e90315942a683e
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

--- jjb/opnfvdocs/docu-build.sh
+++ jjb/opnfvdocs/docu-build.sh
@@ -30,7 +30,7 @@ for file in "${{files[@]}}"; do
 	# rst2html part
 	echo "rst2html $file"
 	rst2html $file | gsutil cp -L gsoutput.txt - \
-	gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".html
+	gs://artifacts.opnfv.org/"$project""$patchset"/"$gs_cp_folder".html
 	gsutil setmeta -h "Content-Type:text/html" \
 			-h "Cache-Control:private, max-age=0, no-transform" \
 			gs://artifacts.opnfv.org/"$project""$patchset"/"$gs_cp_folder".html
@@ -39,7 +39,7 @@ for file in "${{files[@]}}"; do
 
 	echo "rst2pdf $file"
 	rst2pdf $file -o - | gsutil cp -L gsoutput.txt - \
-	gs://artifacts.opnfv.org/"$project"/"$gs_cp_folder".pdf
+	gs://artifacts.opnfv.org/"$project""$patchset"/"$gs_cp_folder".pdf
 	gsutil setmeta -h "Content-Type:application/pdf" \
 			-h "Cache-Control:private, max-age=0, no-transform" \
 			gs://artifacts.opnfv.org/"$project""$patchset"/"$gs_cp_folder".pdf
