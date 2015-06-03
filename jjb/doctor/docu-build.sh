c1193966000f6265c62976ace2e74037b2f171aa
Ryota MIBU <r-mibu@cq.jp.nec.com>
2015-06-04 00:43:54 +0900

    Fix target file path for 'gsutil setmeta'
    JIRA: OCTO-67
    
    Change-Id: I2dd39cb9733e1bb79b3c7fafc18f6c9989d98eb0
    
    Signed-off-by: Ryota MIBU <r-mibu@cq.jp.nec.com>

--- jjb/doctor/docu-build.sh
+++ jjb/doctor/docu-build.sh
@@ -2,7 +2,6 @@
 set -e
 set -o pipefail
 
-design_docs_dir="design_docs"
 build_dir="build"
 project="$(git remote -v | head -n1 | awk '{{print $2}}' | sed -e 's,.*:\(.*/\)\?,,' -e 's/\.git$//')"
 export PATH=$PATH:/usr/local/bin/
@@ -11,13 +10,13 @@ make
 
 # upload all built files
 files=(
-    $build_dir/$design_docs_dir
-    $build_dir/requirements/html
-    $build_dir/requirements/latex/*.pdf
+    design_docs
+    requirements/html
+    requirements/latex/*.pdf
 )
 
 for file in "${{files[@]}}"; do
-    gsutil cp -r -L gsoutput.txt $file gs://artifacts.opnfv.org/$project/
+    gsutil cp -r -L gsoutput.txt $build_dir/$file gs://artifacts.opnfv.org/$project/
     gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
 	gs://artifacts.opnfv.org/$project/$file
     cat gsoutput.txt
