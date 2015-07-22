6456c62de32eed33713d16ef322209a3c2257874
grkoffi <koffirodrigue@gmail.com>
2015-07-22 12:05:00 +0200

    Update result_collection_api
    Fix lines length regarding pep8.
    Add new handlers for cases and results and
    HTTP methods to projects.
    
    JIRA : RELENG-7
    
    Change-Id: If42218b92967ab3afd3f0c6353f6a8f230430b53
    Signed-off-by: Guy Rodrigue Koffi <koffirodrigue@gmail.com>

---
+++ utils/test/result_collection_api/common/constants.py
@@ -0,0 +1,18 @@
+##############################################################################
+# Copyright (c) 2015 Orange
+# guyrodrigue.koffi@orange.com / koffirodrigue@gmail.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+API_LISTENING_PORT = 8000
+
+MONGO_URL = "mongodb://MONGO_IP:27017/"
+
+DEFAULT_REPRESENTATION = "application/json"
+HTTP_BAD_REQUEST = 400
+HTTP_FORBIDDEN = 403
+HTTP_NOT_FOUND = 404
