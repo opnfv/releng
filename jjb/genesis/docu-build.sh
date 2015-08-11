6e205f8f9abb8f32a99c4178f0eba37432887561
Aric Gardner <agardner@linuxfoundation.org>
2015-08-11 16:53:42 -0400

    Disable documentation generation
    documentation generation is currently broken for this  and olding back code.
    
    Change-Id: I116b6c773fbe2e465be6249a5b6149abd44b64d0
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

--- jjb/genesis/docu-build.sh
+++ jjb/genesis/docu-build.sh
@@ -9,39 +9,41 @@ echo "Build"
 echo "-----"
 echo
 
-make
+#make
 
 echo
 echo "Upload"
 echo "------"
 echo
 
-# NOTE: make sure source parameters for GS paths are not empty.
-[[ $GERRIT_CHANGE_NUMBER =~ .+ ]]
-[[ $GERRIT_PROJECT =~ .+ ]]
-[[ $GERRIT_BRANCH =~ .+ ]]
+echo "document generation disabled"
 
-gs_path_review="artifacts.opnfv.org/review/$GERRIT_CHANGE_NUMBER"
-if [[ $GERRIT_BRANCH = "master" ]] ; then
-    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT"
-else
-    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT/${{GERRIT_BRANCH##*/}}"
-fi
-
-if [[ $JOB_NAME =~ "verify" ]] ; then
-    gsutil cp -r build/* "gs://$gs_path_review/"
-    echo
-    echo "Document is available at http://$gs_path_review"
-else
-    gsutil cp -r build/design_docs "gs://$gs_path_branch/"
-    gsutil cp -r build/requirements/html "gs://$gs_path_branch/"
-    gsutil cp -r build/requirements/latex/*.pdf "gs://$gs_path_branch/"
-    echo
-    echo "Latest document is available at http://$gs_path_branch"
-
-    if gsutil ls "gs://$gs_path_review" > /dev/null 2>&1 ; then
-        echo
-        echo "Deleting Out-of-dated Documents..."
-        gsutil rm -r "gs://$gs_path_review"
-    fi
-fi
+## NOTE: make sure source parameters for GS paths are not empty.
+#[[ $GERRIT_CHANGE_NUMBER =~ .+ ]]
+#[[ $GERRIT_PROJECT =~ .+ ]]
+#[[ $GERRIT_BRANCH =~ .+ ]]
+#
+#gs_path_review="artifacts.opnfv.org/review/$GERRIT_CHANGE_NUMBER"
+#if [[ $GERRIT_BRANCH = "master" ]] ; then
+#    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT"
+#else
+#    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT/${{GERRIT_BRANCH##*/}}"
+#fi
+#
+#if [[ $JOB_NAME =~ "verify" ]] ; then
+#    gsutil cp -r build/* "gs://$gs_path_review/"
+#    echo
+#    echo "Document is available at http://$gs_path_review"
+#else
+#    gsutil cp -r build/design_docs "gs://$gs_path_branch/"
+#    gsutil cp -r build/requirements/html "gs://$gs_path_branch/"
+#    gsutil cp -r build/requirements/latex/*.pdf "gs://$gs_path_branch/"
+#    echo
+#    echo "Latest document is available at http://$gs_path_branch"
+#
+#    if gsutil ls "gs://$gs_path_review" > /dev/null 2>&1 ; then
+#        echo
+#        echo "Deleting Out-of-dated Documents..."
+#        gsutil rm -r "gs://$gs_path_review"
+#    fi
+#fi
