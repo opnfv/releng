25ecf373287b9226d409282aeb8f7062ae0c0cad
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2015-08-22 01:07:26 +0200

    Post links of documents generated for review to Gerrit
    JIRA: RELENG-18
    
    Change-Id: Ib0f3b4dcb31844b3dc96357ca1e4f000a633e493
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/copper/docu-build.sh
+++ jjb/copper/docu-build.sh
@@ -33,6 +33,10 @@ if [[ $JOB_NAME =~ "verify" ]] ; then
     gsutil cp -r build/* "gs://$gs_path_review/"
     echo
     echo "Document is available at http://$gs_path_review/index.html"
+    # post link to gerrit as comment
+    gerrit_comment=$(echo '"Document is available at 'http://$gs_path_review/index.html' for review"')
+    ssh -p 29418 gerrit.opnfv.org gerrit review -p $GERRIT_PROJECT -m \
+    $gerrit_comment $GERRIT_PATCHSET_REVISION
 else
     gsutil cp -r build/* "gs://$gs_path_branch/design_docs/"
     #gsutil cp -r build/design_docs "gs://$gs_path_branch/"
