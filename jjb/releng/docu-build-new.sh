ed50d36b59cda7b6bc65775e9211eafc7aa67aaf
Aric Gardner <agardner@linuxfoundation.org>
2015-08-26 16:21:09 -0400

    Testing single dir documentation upload
    Change-Id: Iddc23760107a7a6a6bbeca61d9011a78727e2a3a
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

--- jjb/releng/docu-build-new.sh
+++ jjb/releng/docu-build-new.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
+set -e
+set -o pipefail
+
+export PATH=$PATH:/usr/local/bin/
 
 clean() {{
 if [[ -d docs/output ]]; then
@@ -27,5 +31,42 @@ fi
 
 sphinx-build -b html -E -c docs/etc/ ""$dir"/" docs/output/"${{dir##*/}}/"
 
-done
+echo
+echo "Upload"
+echo "------"
+echo
 
+# NOTE: make sure source parameters for GS paths are not empty.
+[[ $GERRIT_CHANGE_NUMBER =~ .+ ]]
+[[ $GERRIT_PROJECT =~ .+ ]]
+[[ $GERRIT_BRANCH =~ .+ ]]
+
+gs_path_review="artifacts.opnfv.org/review/$GERRIT_CHANGE_NUMBER"
+if [[ $GERRIT_BRANCH = "master" ]] ; then
+    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT"
+else
+    gs_path_branch="artifacts.opnfv.org/$GERRIT_PROJECT/${{GERRIT_BRANCH##*/}}"
+fi
+
+if [[ $JOB_NAME =~ "verify" ]] ; then
+    gsutil cp -r docs/output/"${{dir##*/}}/" "gs://$gs_path_review/"
+    echo
+    echo "Document is available at http://$gs_path_review/index.html"
+    # post link to gerrit as comment
+    gerrit_comment=$(echo '"Document is available at 'http://$gs_path_review/index.html' for review"')
+    ssh -p 29418 gerrit.opnfv.org gerrit review -p $GERRIT_PROJECT -m \
+    $gerrit_comment $GERRIT_PATCHSET_REVISION
+else
+    gsutil cp -r docs/output/"${{dir##*/}}/" "gs://$gs_path_branch/design_docs/"
+
+    echo "Latest document is available at http://$gs_path_branch/design_docs/index.html"
+
+    if gsutil ls "gs://$gs_path_review" > /dev/null 2>&1 ; then
+        echo
+        echo "Deleting Out-of-dated Documents..."
+        gsutil rm -r "gs://$gs_path_review"
+    fi
+fi
+
+
+done
