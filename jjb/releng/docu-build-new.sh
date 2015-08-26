c44a804c968f6893bf413e72a57ad2adc0e87a64
Aric Gardner <agardner@linuxfoundation.org>
2015-08-26 16:00:02 -0400

    Adding initial docs, script and logic for releng project
    Change-Id: Ie5bebf61b5f539d31c4c270b3f86b68b7a3ff2c9
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ jjb/releng/docu-build-new.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+clean() {
+if [[ -d docs/output ]]; then
+rm -rf docs/output
+echo "cleaning up output directory"
+fi
+} 
+
+trap clean EXIT TERM INT SIGTERM SIGHUP
+
+directories=()
+while read -d $'\n'; do
+        directories+=("$REPLY")
+done < <(find docs/ -name 'index.rst' -printf '%h\n' | sort -u )
+
+for dir in "${directories[@]}"; do
+echo
+echo "#############################"
+echo "Building DOCS in ${dir##*/}"
+echo "#############################"
+echo
+
+if [[ ! -d docs/output/"${dir##*/}/" ]]; then
+  mkdir -p docs/output/"${dir##*/}/"
+fi
+
+sphinx-build -b html -E -c docs/etc/ ""$dir"/" docs/output/"${dir##*/}/"
+#find docs/output/"${dir##*/}/"
+
+done
+
