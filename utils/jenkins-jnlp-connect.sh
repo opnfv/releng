7803377bbbc7b1edd8cd828c50dd7210f1949584
Aric Gardner <agardner@linuxfoundation.org>
2015-10-13 07:48:30 -0400

    Script to configure monit for jnlp slave connections
    Updates slave.jar.
    Restarts slave connection if it drops.
    detects distro
    
    JIRA: RELENG-42
    
    Change-Id: I237eda32ac59a57202811bc1d0c7ce29010ac71d
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ utils/jenkins-jnlp-connect.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#This script will help keep your jnlp slave connection up and update the slave.jar before connection.
+#Config consists of these 4 lines. Run as root to install and configure monit, then start monit
+jenkinshome="/home/jenkins-ci"
+jenkinsuser="jenkins-ci"
+slave_name=""
+slave_secret=""
+connectionstring="java -jar slave.jar -jnlpUrl https://build.opnfv.org/ci/computer/"$slave_name"/slave-agent.jnlp -secret "$slave_secret""
+
+
+if [[ -x "/home/$jenkinsuser/jenkins-jnlp-connect.sh" ]]; then
+  echo "jenkins-jnlp-connect.sh is in the correct place and is executable"
+else
+  echo "please make sure this script is called jenkins-jnlp-connect.sh and is executable"
+  exit 1
+fi
+
+if [[ -z $jenkinsuser || -z $jenkinshome ]]; then
+  echo "jenkinsuser or home not defined, please edit this file to define it"
+  exit 1
+fi
+
+if [[ -z $slave_name || -z $slave_secret ]]; then
+  echo "slave name or secret not defined, please edit this file to define it"
+  exit 1
+fi
+
+#detect distro
+distro="$(tr -s ' \011' '\012' < /etc/issue | head -n 1)"
+if [[ $distro == Debian || $distro == Ubuntu ]]; then
+monitconfdir="/etc/monit/conf.d/"
+elif [[ $distro == Fedora || $distro == CentOS || $distro == Redhat ]]; then
+monitconfdir="/etc/monit.d"
+fi
+
+#make pid dir
+if ! [ -d /var/run/$jenkinsuser/ ]; then
+  mkdir   /var/run/$jenkinsuser/
+  chown $jenkinsuser:$jenkinsuser /var/run/$jenkinsuser/
+fi
+
+#create pid file
+pidfile="/var/run/$jenkinsuser/jenkins_jnlp_pid"
+echo $$ > /var/run/$jenkinsuser/jenkins_jnlp_pid
+trap 'rm -f "$pidfile"; exit' EXIT SIGQUIT SIGINT SIGSTOP SIGTERM
+
+#test firewall
+echo "testing that the firewall is open for us at build.opnfv.org"
+if [[ "$(nc -w 4 build.opnfv.org 57387; echo $?) != 0" ]]; then
+  :
+else
+  echo "LF firewall not open, please send a report to helpdesk"
+  exit 1
+fi
+
+#intall monit if needed
+if [ $(which monit) ]; then
+  echo "monit installed"
+else
+
+#apt-get install monit or yum install monit
+  if [ -n "$(command -v yum)" ]; then
+    echo "please install monit; eg: yum -y install monit"
+    exit 1
+  elif [ -n "$(command -v apt-get)" ]; then
+    echo "please install monit  eg: apt-get install -y monit"
+    exit 1
+  else
+    echo "system not supported"
+    exit 1
+  fi
+fi
+
+#create monit conf file if needed
+if [[ -f $monitconfdir/jenkins ]]; then
+  :
+else
+cat << EOF > $monitconfdir/jenkins
+check process bash with pidfile /var/run/$jenkinsuser/pid_file
+start program = "/bin/bash -c 'cd /home/$jenkinsuser/; export started_monit=true; /home/$jenkinsuser/jenkins-jnlp-connect.sh' as uid $jenkinsuser and gid $jenkinsuser
+stop program = "/bin/kill $(/bin/cat /var/run/"$jenkinsuser"/pid_file)"
+EOF
+fi
+
+if [[ $started_monit == "true" ]]; then
+
+  if [[ $(whoami) == "$jenkinsuser" ]]; then
+    #grab new slave.jar if avaliable
+    wget --timestamping https://build.opnfv.org/ci/jnlpJars/slave.jar && true
+    #Start slave
+    $connectionstring
+  else
+     echo "please run this program as "$jenkinsuser""
+  exit 1
+  fi
+
+else
+  echo "you are ready to start monit"
+  echo "example debug mode:  /usr/bin/monit -Ivv -c /etc/monit.conf "
+  exit 0
+fi
