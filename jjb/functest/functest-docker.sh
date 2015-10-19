2a2dd79f0f7f4de755c5514863d2e38ef1ddce86
jose.lausuch <jose.lausuch@ericsson.com>
2015-10-19 16:01:31 +0200

    Add daily jjob to build and push functest docker
    JIRA: FUNCTEST-78
    
    Change-Id: I4b50f3ddfc7f97d5d56de08afd065ae88e3eec4c
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ jjb/functest/functest-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# log info to console
+echo "Starting the build of Functest Docker."
+echo "--------------------------------------------------------"
+echo
+
+# Remove previous running containers
+docker ps | grep opnfv/functest | awk '{print $1}' | xargs docker stop &>/dev/null
+docker ps -a | grep opnfv/functest | awk '{print $1}' | xargs docker rm &>/dev/null
+
+
+# Remove existing images
+docker images | grep opnfv/functest | awk '{print $3}' | xargs docker rmi &>/dev/null
+
+# start the build
+cd $WORKSPACE/docker/
+docker build -t opnfv/functest .
+
+# list the images
+docker images
+
+
+
+if [ ${PUSH_IMAGE} == true ]; then
+    echo "Starting the push of Functest Docker image."
+    echo "--------------------------------------------------------"
+    echo
+    # Push to the Dockerhub repository
+    docker push opnfv/functest
+fi
