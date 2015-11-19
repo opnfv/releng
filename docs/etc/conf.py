b0ca3be669429dfa9f4a92a9876467748d6d495a
Ryota MIBU <r-mibu@cq.jp.nec.com>
2015-11-19 17:59:55 +0900

    doc: fix logo and filename in sphinx-build [WIP]
    DO NOT MERGE THIS!
    
    Change-Id: I53be7e1ec8e8c3b9134de40e99084e5ea623430c
    Signed-off-by: Ryota MIBU <r-mibu@cq.jp.nec.com>

--- docs/etc/conf.py
+++ docs/etc/conf.py
@@ -10,8 +10,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 pygments_style = 'sphinx'
 html_use_index = False
+html_logo = 'opnfv-logo.png'
 
-pdf_documents = [('index', u'Copper', u'Copper Project', u'OPNFV')]
+#pdf_documents = [('index', u'Copper', u'Copper Project', u'OPNFV')]
 pdf_fit_mode = "shrink"
 pdf_stylesheets = ['sphinx','kerning','a4']
 #latex_domain_indices = False
@@ -20,8 +21,9 @@ pdf_stylesheets = ['sphinx','kerning','a4']
 latex_elements = {
     'printindex': '',
 }
+latex_logo = 'opnfv-logo.png'
 
-project = u'Copper: Virtual Infrastructure Deployment Policies'
+project = u'Document Sample (in releng)'
 copyright = u'%s, OPNFV' % datetime.date.today().year
 version = u'1.0.0'
 release = u'1.0.0'
