bdbb134df09e24bc247074b7e8eb7e44c4a13d3a
Ryota MIBU <r-mibu@cq.jp.nec.com>
2015-11-20 21:41:21 +0900

    doc: fix rst files to have consistent view in html and pdf
    Change-Id: I6e36f16be6e1c9160820d137a78ac1e7674153f0
    Signed-off-by: Ryota MIBU <r-mibu@cq.jp.nec.com>

--- docs/etc/conf.py
+++ docs/etc/conf.py
@@ -1,6 +1,4 @@
 import datetime
-import sys
-import os
 
 needs_sphinx = '1.3'
 
@@ -12,18 +10,14 @@ pygments_style = 'sphinx'
 html_use_index = False
 html_logo = 'opnfv-logo.png'
 
-pdf_documents = [('index', u'Copper', u'Copper Project', u'OPNFV')]
 pdf_fit_mode = "shrink"
 pdf_stylesheets = ['sphinx','kerning','a4']
-#latex_domain_indices = False
-#latex_use_modindex = False
 
 latex_elements = {
     'printindex': '',
 }
 latex_logo = 'opnfv-logo.png'
 
-project = u'Copper: Virtual Infrastructure Deployment Policies'
 copyright = u'%s, OPNFV' % datetime.date.today().year
 version = u'1.0.0'
 release = u'1.0.0'
