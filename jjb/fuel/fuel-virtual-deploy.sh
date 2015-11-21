6abac26c49ea658823d4f5110e0a45758971d9db
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2015-11-21 21:57:14 +0100

    Enable virtual deployments for merged fuel commits
    JIRA: RELENG-56
    
    Change-Id: Ie1c179b67e003e51fcd58e7d0c6d09c0d1382171
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/fuel/fuel-virtual-deploy.sh
+++ jjb/fuel/fuel-virtual-deploy.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
 
-echo "Placeholder for Fuel virtual deploy"
+# source the file so we get OPNFV vars
+source latest.properties
+
+# echo the info about artifact that is used during the deployment
+echo "Using $(echo $OPNFV_ARTIFACT_URL | cut -d'/' -f3) for deployment"
+
+# create TMPDIR if it doesn't exist
+export TMPDIR=$HOME/tmpdir
+[[ -d $TMPDIR ]] || mkdir -p $TMPDIR
+
+# change permissions down to TMPDIR
+chmod a+x $HOME
+chmod a+x $TMPDIR
+
+# set CONFDIR, BRIDGE
+CONFDIR=$WORKSPACE/fuel/deploy/templates/virtual_environment/conf
+BRIDGE=pxebr
+
+# log info to console
+echo "Starting the deployment for a merged change using $INSTALLER. This could take some time..."
+echo "--------------------------------------------------------"
+echo
+
+# start the deployment
+echo "Issuing command"
+echo "sudo $WORKSPACE/fuel/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $CONFDIR/dea.yaml -dha $CONFDIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh"
+
+sudo $WORKSPACE/fuel/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $CONFDIR/dea.yaml -dha $CONFDIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh
+
+echo
+echo "--------------------------------------------------------"
+echo "Virtual deployment is done! Removing the intermediate files from artifact repo"
+
+PROPERTIES_FILE=$(echo $OPNFV_ARTIFACT_URL | sed 's/iso/properties/')
+gsutil rm gs://$OPNFV_ARTIFACT_URL
+gsutil rm gs://$PROPERTIES_FILE
