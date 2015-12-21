be9bcfaafe183e62b86779ed08ebb2bbd81fd6a5
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2015-12-21 13:05:29 +0100

    Refactor fuel JJB to prepare for Milestone E
    Replace default stream definitions with stream anchors in order
    to be able to specify which job on which POD runs against which
    branch.
    
    Move project specific verify job to its own file to ease the stream
    handling.
    
    Enable CI on Ericsson POD2.
    
    Change-Id: I3732b43d8858473535ef5666882ee85124c6d652
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/fuel/fuel-build.sh
+++ jjb/fuel/fuel-build.sh
@@ -3,13 +3,40 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+cd $WORKSPACE
+
+# check to see if we already have an artifact on artifacts.opnfv.org
+# for this commit
+echo "Checking to see if we already built and stored Fuel ISO for this commit"
+
+LATEST_ISO_PROPERTIES=$WORKSPACE/latest.iso.properties
+curl -s -o $LATEST_ISO_PROPERTIES http://$GS_URL/latest.properties 2>/dev/null
+
+# get metadata of latest ISO
+LATEST_ISO_SHA1=$(grep OPNFV_GIT_SHA1 $LATEST_ISO_PROPERTIES | cut -d'=' -f2)
+LATEST_ISO_URL=$(grep OPNFV_ARTIFACT_URL $LATEST_ISO_PROPERTIES | cut -d'=' -f2)
+
+# get current SHA1
+CURRENT_SHA1=$(git rev-parse HEAD)
+
+if [[ "$CURRENT_SHA1" == "$LATEST_ISO_SHA1" ]]; then
+    echo "An ISO has already been built for this commit"
+    echo "    $LATEST_ISO_URL"
+    echo "Nothing new to build. Exiting."
+    exit 0
+else
+    echo "This commit has not been built yet. Proceeding with the build."
+    /bin/rm -f $LATEST_ISO_PROPERTIES
+    echo
+fi
+
 # log info to console
 echo "Starting the build of $INSTALLER. This could take some time..."
 echo "--------------------------------------------------------"
 echo
 
 # create the cache directory if it doesn't exist
-[[ -d $CACHE_DIRECTORY ]] || mkdir -p $CACHE_DIRECTORY
+mkdir -p $CACHE_DIRECTORY
 
 # set OPNFV_ARTIFACT_VERSION
 if [[ "$JOB_NAME" =~ "merge" ]]; then
