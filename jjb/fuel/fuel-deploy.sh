23583a68f520cb3a8f68c091cf33c752ac302ee2
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2015-12-21 23:03:50 +0100

    Add location of pod config files into slave-config & disable lab-reconfig
    Change-Id: I7e71a1bd3f909fac49c74ab0c8ef336a6b5fa711
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/fuel/fuel-deploy.sh
+++ jjb/fuel/fuel-deploy.sh
@@ -11,28 +11,28 @@ echo "Using $(echo $OPNFV_ARTIFACT_URL | cut -d'/' -f3) for deployment"
 
 # create TMPDIR if it doesn't exist
 export TMPDIR=$HOME/tmpdir
-[[ -d $TMPDIR ]] || mkdir -p $TMPDIR
+mkdir -p $TMPDIR
 
 # change permissions down to TMPDIR
 chmod a+x $HOME
 chmod a+x $TMPDIR
 
 # set CONFDIR, BRIDGE
-CONFDIR=$WORKSPACE/deploy/templates/hardware_environment/conf/linux_foundation_lab/pod2
+POD_CONF_DIR=$WORKSPACE/deploy/templates/hardware_environment/conf/linux_foundation_lab/pod2
 BRIDGE=pxebr
 
 # clone genesis repo and checkout the SR1 tag
-echo "Cloning genesis repo"
-cd $WORKSPACE
-GIT_SSL_NO_VERIFY=true git clone https://gerrit.opnfv.org/gerrit/genesis genesis
-cd genesis
-git checkout arno.2015.2.0
+#echo "Cloning genesis repo"
+#cd $WORKSPACE
+#GIT_SSL_NO_VERIFY=true git clone https://gerrit.opnfv.org/gerrit/genesis genesis
+#cd genesis
+#git checkout arno.2015.2.0
 
 # cleanup first
-sudo $WORKSPACE/genesis/common/ci/clean.sh -base_config $WORKSPACE/genesis/foreman/ci/inventory/lf_pod2_ksgen_settings.yml
+#sudo $WORKSPACE/genesis/common/ci/clean.sh -base_config $WORKSPACE/genesis/foreman/ci/inventory/lf_pod2_ksgen_settings.yml
 
 # prepare for Fuel Deployment
-sudo $WORKSPACE/genesis/common/ci/setup.sh
+#sudo $WORKSPACE/genesis/common/ci/setup.sh
 
 # log info to console
 echo "Starting the deployment using $INSTALLER_TYPE. This could take some time..."
@@ -41,9 +41,9 @@ echo
 
 # start the deployment
 echo "Issuing command"
-echo "sudo $WORKSPACE/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $CONFDIR/dea.yaml -dha $CONFDIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh"
+echo "sudo $WORKSPACE/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $POD_CONF_DIR/dea.yaml -dha $POD_CONF_DIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh"
 
-sudo $WORKSPACE/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $CONFDIR/dea.yaml -dha $CONFDIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh
+sudo $WORKSPACE/ci/deploy.sh -iso $WORKSPACE/opnfv.iso -dea $POD_CONF_DIR/dea.yaml -dha $POD_CONF_DIR/dha.yaml -s $TMPDIR -b $BRIDGE -nh
 
 echo
 echo "--------------------------------------------------------"
