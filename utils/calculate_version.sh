f2b14373dc46b31eddd121b6a5b17c10c4d1b140
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2016-01-27 09:53:39 +0100

    Add license header
    License headers are added.
    - Set company name based on the author who created the file first time
    - Add SPDX header for Apache 2.0
    
    Change-Id: I8f357a2d6f75179171c681f4b53e0f4249eca5a2
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- utils/calculate_version.sh
+++ utils/calculate_version.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2016 Ericsson AB and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 
-#
-# Authors:
-#      Jose Lausuch <jose.lausuch@ericsson.com>
-#      Fatih Degirmenci <fatih.degirmenci@ericsson.com>
-#
 # Calculates and generates the version tag for the OPNFV objects:
 #     - Docker images
 #     - ISOs
-#     - Artifcats
-#
+#     - Artifacts
 
 info ()  {
     logger -s -t "Calculate_version.info" "$*"
