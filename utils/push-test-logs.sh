e7ed5d8b804b3b400ec85f3c8d34aa645c9b4dfb
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2016-01-28 17:44:32 +0100

    test: Store test logs on artifacts.opnfv.org
    Change-Id: I6e6c1befa9d867632c9cbd5ac1995aeb8560d0eb
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

---
+++ utils/push-test-logs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2016 Orange and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -e
+set -o pipefail
+
+export PATH=$PATH:/usr/local/bin/
+
+git_sha1="$(git rev-parse HEAD)"
+res_build_date=${1:-$(date -u +"%Y-%m-%d_%H-%M-%S")}
+project=$PROJECT
+branch=${GIT_BRANCH##*/}
+testbed=$NODE_NAME
+dir_result="${HOME}/opnfv/$project/results/$branch"
+
+if [[ "$branch" == "master" ]]; then
+    project_artifact=logs/$project/$testbed/$res_build_date
+else
+    project_artifact=logs/$project/$testbed/$branch/$res_build_date
+fi
+
+# create the folder to store the results
+mkdir -p $dir_result
+
+# copy folder to artifact
+if [ -d "$dir_result" ]; then
+    if [ "$(ls -A $dir_result)" ]; then
+          echo "copy result files to artifact $project_artifact"
+          gsutil -m cp -r "$dir_result" gs://artifacts.opnfv.org/"$project_artifact"/
+
+          # delete local results
+          # should not be useful as the container is about to die...just in case
+          rm -Rf $dir_result/*
+    else
+          echo "Result folder is empty"
+    fi
+fi
