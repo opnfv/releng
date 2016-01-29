842bf841ead18ea4fa0474671d370fc142d0f1d2
meimei <meimei@huawei.com>
2016-01-29 17:16:27 +0800

    [compass] Prevent gsutil setmeta warnings from failing the build
    Change-Id: If629d4f345931e31053a0a95fc266a168c62fb86
    Signed-off-by: meimei <meimei@huawei.com>

--- jjb/compass4nfv/compass-upload-artifact.sh
+++ jjb/compass4nfv/compass-upload-artifact.sh
@@ -16,6 +16,26 @@ gsutil cp $BUILD_DIRECTORY/compass.iso gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSIO
 gsutil cp $BUILD_DIRECTORY/opnfv.properties gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.properties > gsutil.properties.log 2>&1
 gsutil cp $BUILD_DIRECTORY/opnfv.properties gs://$GS_URL/latest.properties > gsutil.latest.log 2>&1
 
+gsutil -m setmeta \
+    -h "Content-Type:text/html" \
+    -h "Cache-Control:private, max-age=0, no-transform" \
+    gs://$GS_URL/latest.properties \
+    gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.properties > /dev/null 2>&1
+
+gsutil -m setmeta \
+    -h "Cache-Control:private, max-age=0, no-transform" \
+    gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.iso > /dev/null 2>&1
+
+# disabled errexit due to gsutil setmeta complaints
+#   BadRequestException: 400 Invalid argument
+# check if we uploaded the file successfully to see if things are fine
+gsutil ls gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.iso > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "Problem while uploading artifact!"
+    echo "Check log $WORKSPACE/gsutil.iso.log on the machine where this build is done."
+    exit 1
+fi
+
 echo
 echo "--------------------------------------------------------"
 echo "Done!"
