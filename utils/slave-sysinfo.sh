34a1842eac819344e1dd7a36093641e8555e7e3b
Aric Gardner <agardner@linuxfoundation.org>
2016-03-23 15:59:19 -0400

    Script to be run on all pods on a daily
    Tied to label opnfv-sysinfo
    I have given all slaves this lable
    Runs a simple bash script, input on what we shoud display appreciated
    I will use this to track uptime (not sure how yet)
    Started in disabled state casue I want to be present the first time
    it runs
    
    Change-Id: Ia698f9dd25feb49a8bec8006a8f3de3ea0d5f4c4
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ utils/slave-sysinfo.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo "" 
+echo ": "
+$(printf "\n %s %s" )
+
+echo "" 
+echo "Date: "
+$(printf "\n %s %s" date)
+
+echo "" 
+echo "Uptime: "
+$(printf "\n %s %s" uptime)
+
+echo "" 
+echo "Current user is : "
+$(printf "\n %s %s" whoami)
+
+echo "" 
+echo "Checking for passwordless sudo: "
+$(printf "\n %s %s" sudo -n true)
+
+echo "" 
+echo "Current system is : "
+$(printf "\n %s %s" uname -a)
+
+echo "" 
+echo "Number of cores: "
+$(printf "\n %s %s" getconf _NPROCESSORS_ONLN)
+
+echo "" 
+echo "Proccessor Speed: "
+$(printf "\n %s %s" cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq)
+
+echo "" 
+echo "Memory: "
+$(printf "\n %s %s" free -m)
+
+
+echo ""
+echo "Current connected devices are : "
+$(printf "\n %s %s" arp -a)
+
+
+echo ""
+echo "Current IP configuration is : "
+$(printf "\n %s %s" ifconfig)
+  
+echo ""
+echo "Done."
+
+
+
