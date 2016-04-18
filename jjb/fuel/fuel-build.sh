93f113c11d7d064444a924e67b333cc312ef30b8
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2016-04-19 01:00:29 +0200

    fuel: Disable build avoidance in CI
    Change-Id: I086b47b956faaa6c57c532a99debd06c05b148a2
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/fuel/fuel-build.sh
+++ jjb/fuel/fuel-build.sh
@@ -35,11 +35,13 @@ CURRENT_SHA1=$(git rev-parse HEAD)
 FORCE_BUILD=${FORCE_BUILD:-false}
 
 if [[ "$CURRENT_SHA1" == "$LATEST_ISO_SHA1" && "$FORCE_BUILD" == "false" ]]; then
-    echo "An ISO has already been built for this commit"
-    echo "    $LATEST_ISO_URL"
-    echo "Nothing new to build. Exiting."
-    touch $WORKSPACE/.noupload
-    exit 0
+    echo "***************************************************"
+    echo "   An ISO has already been built for this commit"
+    echo "   $LATEST_ISO_URL"
+    echo "***************************************************"
+#    echo "Nothing new to build. Exiting."
+#    touch $WORKSPACE/.noupload
+#    exit 0
 else
     echo "This commit has not been built yet or forced build! Proceeding with the build."
     /bin/rm -f $LATEST_ISO_PROPERTIES
