a2e6a3b876f6c1b67d0c493d215209b69f0c52ba
Morgan Richomme <morgan.richomme@orange.com>
2016-05-19 15:15:29 +0200

    Forget conf file...
    Change-Id: I85261ec5fcbe59db2e67f8347d2babb082454e14
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

---
+++ utils/test/reporting/functest/reportingConf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Reporting: Declaration of the variables
+#
+# ****************************************************
+installers = ["apex", "compass", "fuel", "joid"]
+# installers = ["compass"]
+versions = ["brahmaputra", "master"]
+# versions = ["master"]
+PERIOD = 10
+MAX_SCENARIO_CRITERIA = 18
