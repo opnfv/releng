4ffdf0493d91898edd86329b731752a31d6dcddd
Tim Rozet <trozet@redhat.com>
2016-05-27 13:21:59 -0400

    Apex: Fixes artifact upload
    Fixes include:
     - SRPM extension was wrong, causing SRPMs to not be uploaded correctly
     - No more redirecting stderr to stdout
     - More print statements to Jenkins
    
    Change-Id: Ib96bb1efd967d9510d14a2e63bdf7d97b5d07cb2
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/apex/apex-upload-artifact.sh
+++ jjb/apex/apex-upload-artifact.sh
@@ -12,7 +12,8 @@ echo
 source $WORKSPACE/opnfv.properties
 
 # upload artifact and additional files to google storage
-gsutil cp $BUILD_DIRECTORY/release/OPNFV-CentOS-7-x86_64-$OPNFV_ARTIFACT_VERSION.iso gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.iso > gsutil.iso.log 2>&1
+gsutil cp $BUILD_DIRECTORY/release/OPNFV-CentOS-7-x86_64-$OPNFV_ARTIFACT_VERSION.iso gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.iso > gsutil.iso.log
+echo "ISO Upload Complete!"
 RPM_INSTALL_PATH=$BUILD_DIRECTORY/noarch
 RPM_LIST=$RPM_INSTALL_PATH/$(basename $OPNFV_RPM_URL)
 VERSION_EXTENSION=$(echo $(basename $OPNFV_RPM_URL) | sed 's/opnfv-apex-//')
@@ -21,14 +22,18 @@ for pkg in common undercloud opendaylight-sfc onos; do
 done
 SRPM_INSTALL_PATH=$BUILD_DIRECTORY
 SRPM_LIST=$SRPM_INSTALL_PATH/$(basename $OPNFV_SRPM_URL)
+VERSION_EXTENSION=$(echo $(basename $OPNFV_SRPM_URL) | sed 's/opnfv-apex-//')
 for pkg in common undercloud opendaylight-sfc onos; do
     SRPM_LIST+=" ${SRPM_INSTALL_PATH}/opnfv-apex-${pkg}-${VERSION_EXTENSION}"
 done
+
 for artifact in $RPM_LIST $SRPM_LIST; do
-  gsutil cp $artifact gs://$GS_URL/$(basename $artifact) > gsutil.iso.log 2>&1
+  echo "Uploading artifact: ${artifact}"
+  gsutil cp $artifact gs://$GS_URL/$(basename $artifact) > gsutil.iso.log
+  echo "Upload complete for ${artifact}"
 done
-gsutil cp $WORKSPACE/opnfv.properties gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.properties > gsutil.properties.log 2>&1
-gsutil cp $WORKSPACE/opnfv.properties gs://$GS_URL/latest.properties > gsutil.latest.log 2>&1
+gsutil cp $WORKSPACE/opnfv.properties gs://$GS_URL/opnfv-$OPNFV_ARTIFACT_VERSION.properties > gsutil.properties.log
+gsutil cp $WORKSPACE/opnfv.properties gs://$GS_URL/latest.properties > gsutil.latest.log
 
 echo
 echo "--------------------------------------------------------"
