dda17ce0119293fa3ffb0f152421bd761f138f72
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-06-01 15:36:17 +0800

    project-ize testAPI
    JIRA: FUNCTEST-284
    
    Change-Id: I219e934bb11f50de84df2aa0345ecc7885223491
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ utils/test/result_collection_api/opnfv_testapi/common/config.py
@@ -0,0 +1,91 @@
+##############################################################################
+# Copyright (c) 2015 Orange
+# guyrodrigue.koffi@orange.com / koffirodrigue@gmail.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+# feng.xiaowei@zte.com.cn remove prepare_put_request            5-30-2016
+##############################################################################
+
+
+from ConfigParser import SafeConfigParser, NoOptionError
+
+
+class ParseError(Exception):
+    """
+    Custom exception class for config file
+    """
+
+    def __init__(self, message):
+        self.msg = message
+
+    def __str__(self):
+        return 'error parsing config file : %s' % self.msg
+
+
+class APIConfig:
+    """
+    The purpose of this class is to load values correctly from the config file.
+    Each key is declared as an attribute in __init__() and linked in parse()
+    """
+
+    def __init__(self):
+        self._default_config_location = "/etc/opnfv_testapi/config.ini"
+        self.mongo_url = None
+        self.mongo_dbname = None
+        self.api_port = None
+        self.api_debug_on = None
+        self._parser = None
+
+    def _get_parameter(self, section, param):
+        try:
+            return self._parser.get(section, param)
+        except NoOptionError:
+            raise ParseError("[%s.%s] parameter not found" % (section, param))
+
+    def _get_int_parameter(self, section, param):
+        try:
+            return int(self._get_parameter(section, param))
+        except ValueError:
+            raise ParseError("[%s.%s] not an int" % (section, param))
+
+    def _get_bool_parameter(self, section, param):
+        result = self._get_parameter(section, param)
+        if str(result).lower() == 'true':
+            return True
+        if str(result).lower() == 'false':
+            return False
+
+        raise ParseError(
+            "[%s.%s : %s] not a boolean" % (section, param, result))
+
+    @staticmethod
+    def parse(config_location=None):
+        obj = APIConfig()
+
+        if config_location is None:
+            config_location = obj._default_config_location
+
+        obj._parser = SafeConfigParser()
+        obj._parser.read(config_location)
+        if not obj._parser:
+            raise ParseError("%s not found" % config_location)
+
+        # Linking attributes to keys from file with their sections
+        obj.mongo_url = obj._get_parameter("mongo", "url")
+        obj.mongo_dbname = obj._get_parameter("mongo", "dbname")
+
+        obj.api_port = obj._get_int_parameter("api", "port")
+        obj.api_debug_on = obj._get_bool_parameter("api", "debug")
+
+        return obj
+
+    def __str__(self):
+        return "mongo_url = %s \n" \
+               "mongo_dbname = %s \n" \
+               "api_port = %s \n" \
+               "api_debug_on = %s \n" % (self.mongo_url,
+                                         self.mongo_dbname,
+                                         self.api_port,
+                                         self.api_debug_on)
---
+++ utils/test/result_collection_api/opnfv_testapi/resources/pod_handlers.py
@@ -0,0 +1,79 @@
+from opnfv_testapi.tornado_swagger_ui.tornado_swagger import swagger
+from handlers import GenericApiHandler
+from pod_models import Pod
+from opnfv_testapi.common.constants import HTTP_FORBIDDEN
+
+
+class GenericPodHandler(GenericApiHandler):
+    def __init__(self, application, request, **kwargs):
+        super(GenericPodHandler, self).__init__(application, request, **kwargs)
+        self.table = 'pods'
+        self.table_cls = Pod
+
+
+class PodCLHandler(GenericPodHandler):
+    @swagger.operation(nickname='list-all')
+    def get(self):
+        """
+            @description: list all pods
+            @return 200: list all pods, empty list is no pod exist
+            @rtype: L{Pods}
+        """
+        self._list()
+
+    @swagger.operation(nickname='create')
+    def post(self):
+        """
+            @description: create a pod
+            @param body: pod to be created
+            @type body: L{PodCreateRequest}
+            @in body: body
+            @rtype: L{Pod}
+            @return 200: pod is created.
+            @raise 403: pod already exists
+            @raise 400: body or name not provided
+        """
+        def query(data):
+            return {'name': data.name}
+
+        def error(data):
+            message = '{} already exists as a pod'.format(data.name)
+            return HTTP_FORBIDDEN, message
+
+        miss_checks = ['name']
+        db_checks = [(self.table, False, query, error)]
+        self._create(miss_checks, db_checks)
+
+
+class PodGURHandler(GenericPodHandler):
+    @swagger.operation(nickname='get-one')
+    def get(self, pod_name):
+        """
+            @description: get a single pod by pod_name
+            @rtype: L{Pod}
+            @return 200: pod exist
+            @raise 404: pod not exist
+        """
+        query = dict()
+        query['name'] = pod_name
+        self._get_one(query)
+
+    def delete(self, pod_name):
+        """ Remove a POD
+
+        # check for an existing pod to be deleted
+        mongo_dict = yield self.db.pods.find_one(
+            {'name': pod_name})
+        pod = TestProject.pod(mongo_dict)
+        if pod is None:
+            raise HTTPError(HTTP_NOT_FOUND,
+                            "{} could not be found as a pod to be deleted"
+                            .format(pod_name))
+
+        # just delete it, or maybe save it elsewhere in a future
+        res = yield self.db.projects.remove(
+            {'name': pod_name})
+
+        self.finish_request(answer)
+        """
+        pass
