673e88e1675da011cfe62dd3da9f90e20465a13a
Tim Rozet <trozet@redhat.com>
2016-06-03 16:15:36 -0400

    Apex: Fixes build caching
    Changes include:
     - Removing deprecated Brahmaputra support
     - Properly passing CACHE_DIRECTORY without file:// prefix
    
    JIRA: APEX-167
    
    Change-Id: Ic52e394d682d59662006a74ec2d32eef7bc3b6c2
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/apex/apex-build.sh
+++ jjb/apex/apex-build.sh
@@ -10,41 +10,29 @@ echo
 [[ -d $CACHE_DIRECTORY ]] || mkdir -p $CACHE_DIRECTORY
 # set OPNFV_ARTIFACT_VERSION
 if echo $BUILD_TAG | grep "apex-verify" 1> /dev/null; then
-  if echo $GERRIT_BRANCH | grep "brahmaputra" 1> /dev/null; then
-    export OPNFV_ARTIFACT_VERSION=brahmaputra-dev${BUILD_NUMBER}
-    export BUILD_ARGS="-v $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY $BUILD_DIRECTORY"
-  else
-    export OPNFV_ARTIFACT_VERSION=dev${BUILD_NUMBER}
-    export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY"
-  fi
+  export OPNFV_ARTIFACT_VERSION=dev${BUILD_NUMBER}
+  export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c $CACHE_DIRECTORY"
 elif [ "$ARTIFACT_VERSION" == "daily" ]; then
-  if echo $GERRIT_BRANCH | grep "brahmaputra" 1> /dev/null; then
-    export OPNFV_ARTIFACT_VERSION=brahmaputra-$(date -u +"%Y-%m-%d")
-    export BUILD_ARGS="-v $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY $BUILD_DIRECTORY"
-  else
-    export OPNFV_ARTIFACT_VERSION=$(date -u +"%Y-%m-%d")
-    export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY --iso"
-  fi
+  export OPNFV_ARTIFACT_VERSION=$(date -u +"%Y-%m-%d")
+  export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c $CACHE_DIRECTORY --iso"
 else
   export OPNFV_ARTIFACT_VERSION=${ARTIFACT_VERSION}
-  if echo $GERRIT_BRANCH | grep "brahmaputra" 1> /dev/null; then
-    export BUILD_ARGS="-v $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY $BUILD_DIRECTORY"
-  else
-    export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c file://$CACHE_DIRECTORY --iso"
-  fi
+  export BUILD_ARGS="-r $OPNFV_ARTIFACT_VERSION -c $CACHE_DIRECTORY --iso"
 fi
-# clean for stable but doesn't matter for master
-if echo $GERRIT_BRANCH | grep "brahmaputra" 1> /dev/null; then
-  sudo opnfv-clean
-fi
+
 # start the build
 cd $WORKSPACE/ci
 ./build.sh $BUILD_ARGS
 RPM_VERSION=$(grep Version: $BUILD_DIRECTORY/opnfv-apex.spec | awk '{ print $2 }')-$(echo $OPNFV_ARTIFACT_VERSION | tr -d '_-')
 # list the contents of BUILD_OUTPUT directory
+echo "Build Directory Contents:"
+echo "-------------------------"
 ls -al $BUILD_DIRECTORY
-# save information regarding artifact into file
-(
+
+if ! echo $BUILD_TAG | grep "apex-verify" 1> /dev/null; then
+  echo "Writing opnfv.properties file"
+  # save information regarding artifact into file
+  (
     echo "OPNFV_ARTIFACT_VERSION=$OPNFV_ARTIFACT_VERSION"
     echo "OPNFV_GIT_URL=$(git config --get remote.origin.url)"
     echo "OPNFV_GIT_SHA1=$(git rev-parse HEAD)"
@@ -54,6 +42,7 @@ ls -al $BUILD_DIRECTORY
     echo "OPNFV_RPM_URL=$GS_URL/opnfv-apex-$RPM_VERSION.noarch.rpm"
     echo "OPNFV_RPM_MD5SUM=$(md5sum $BUILD_DIRECTORY/noarch/opnfv-apex-$RPM_VERSION.noarch.rpm | cut -d' ' -f1)"
     echo "OPNFV_BUILD_URL=$BUILD_URL"
-) > $WORKSPACE/opnfv.properties
+  ) > $WORKSPACE/opnfv.properties
+fi
 echo "--------------------------------------------------------"
 echo "Done!"
