c11ac26bbc6c789087578d1ac3da013bfafd9832
Dan Radez <dradez@redhat.com>
2016-06-09 14:58:09 -0400

    adding testing builder
    Change-Id: I6d93dbad6ac7d25ace691ca0991179ef8cf8645a
    Signed-off-by: Dan Radez <dradez@redhat.com>

---
+++ jjb/apex/apex-unit-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# log info to console
+echo "Starting unit tests for Apex..."
+echo "---------------------------------------------------------------------------------------"
+echo
+
+
+pushd ci/ > /dev/null
+./test.sh
+popd
+
+echo "--------------------------------------------------------"
+echo "Unit Tests Done!"
