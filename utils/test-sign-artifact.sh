1ddaecd83202b1bf0a9d8b88dd108413ece3b0e6
Aric Gardner <agardner@linuxfoundation.org>
2016-07-06 15:18:30 -0400

    Testing signing artifacts
    gpg agent does not work in non interactive mode.
    merging this so that I can reproduce and debug
    
    Change-Id: I641e8988885aa2d0acf794c9ec7592cb6c90a3e9
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

---
+++ utils/test-sign-artifact.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# clone releng repository
+echo "Cloning releng repository..."
+[ -d releng ] && rm -rf releng
+git clone https://gerrit.opnfv.org/gerrit/releng $WORKSPACE/releng/ &> /dev/null
+#this is where we import the siging key
+if [ -f $WORKSPACE/releng/utils/gpg_import_key.sh ]; then 
+  source $WORKSPACE/releng/utils/gpg_import_key.sh
+fi
+
+artifact="foo"
+echo foo > foo
+
+testsign () {
+  echo "Signing artifact: ${artifact}"
+  gpg2 -vvv --batch \
+    --default-key opnfv-helpdesk@rt.linuxfoundation.org  \
+    --passphrase besteffort \
+    --detach-sig $artifact
+done
+}
+
+testsign
+
