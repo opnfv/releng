44e3920eef5f5a619835da961fbfdc40ed798189
WuKong <rebirthmonkey@gmail.com>
2016-07-08 14:53:33 +0200

    add unit tests for Moon
    Change-Id: If1a0e02b8eaba52b68501e6463b02b3a0547c2c9
    Signed-off-by: WuKong <rebirthmonkey@gmail.com>

--- jjb/moon/moon-verify.sh
+++ jjb/moon/moon-verify.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Hello World"
+echo "launch Moon unit tests"
+
+sudo apt-get install -y git python-nose
+
+cd /tmp
+git clone https://git.opnfv.org/moon
+
+nosetest /tmp/moon/keystone-moon/keystone/tests/moon/unit
+
+rm -rf /tmp/moon
