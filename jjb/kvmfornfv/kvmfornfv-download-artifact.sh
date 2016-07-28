48e9188db830c825c5212969e3cf85c055002f3c
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2016-07-28 09:55:24 +0200

    kvmfornfv: Create daily job and enable artifact upload
    Change-Id: I75b9bb58b3413514b1e3fea63185e210f56de95a
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/kvmfornfv/kvmfornfv-download-artifact.sh
+++ jjb/kvmfornfv/kvmfornfv-download-artifact.sh
@@ -7,16 +7,20 @@ else
     exit 1
 fi
 
-if [[ "$JOB_TYPE" == "verify" ]]; then
-    echo "Downloading artifacts for the change $GERRIT_CHANGE_NUMBER. This could take some time..."
-    GS_URL="gs://artifacts.opnfv.org/$PROJECT/review/$GERRIT_CHANGE_NUMBER"
-else
-    echo "Artifact download is not enabled for $JOB_TYPE jobs"
-    exit 1
-fi
+# do stuff differently based on the job type
+case "$JOB_TYPE" in
+    verify)
+        echo "Downloading artifacts for the change $GERRIT_CHANGE_NUMBER. This could take some time..."
+        GS_UPLOAD_LOCATION="gs://artifacts.opnfv.org/$PROJECT/review/$GERRIT_CHANGE_NUMBER"
+        ;;
+    *)
+        echo "Artifact download is not enabled for $JOB_TYPE jobs"
+        exit 1
+esac
 
 /bin/mkdir -p $WORKSPACE/build_output
-gsutil cp -r $GS_URL/* $WORKSPACE/build_output > $WORKSPACE/gsutil.log 2>&1
+gsutil cp -r $GS_UPLOAD_LOCATION/* $WORKSPACE/build_output > $WORKSPACE/gsutil.log 2>&1
+
 echo "--------------------------------------------------------"
 ls -al $WORKSPACE/build_output
 echo "--------------------------------------------------------"
