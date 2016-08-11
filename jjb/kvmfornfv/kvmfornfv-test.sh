75ca57900f4fb7680acbd05ffc722c0e0df015a3
swatisharma <swatix.sharma@intel.com>
2016-08-11 12:33:54 +0530

    CI Integration for KVMforNFV
    This patch contains the script for triggering kvmfornfv
    cyclictest scripts.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

--- jjb/kvmfornfv/kvmfornfv-test.sh
+++ jjb/kvmfornfv/kvmfornfv-test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+##########################################################
+##This script includes executing cyclictest scripts.
+##########################################################
+#The latest build packages are stored in build_output
 ls -al $WORKSPACE/build_output
+
+#start the test
+cd $WORKSPACE
+./ci/test_kvmfornfv.sh
