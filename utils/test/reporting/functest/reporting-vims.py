9c8143c688588a4dd832162b3d8dfb76ddfd30a1
Morgan Richomme <morgan.richomme@orange.com>
2016-08-25 14:32:08 +0200

    Add colorado reporting for tempest and vims
    Change-Id: I978799cd1cd777b5033e55a0146c41865e9c2bf5
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- utils/test/reporting/functest/reporting-vims.py
+++ utils/test/reporting/functest/reporting-vims.py
@@ -33,81 +33,87 @@ logger.info("****************************************")
 installers = conf.installers
 step_order = ["initialisation", "orchestrator", "vIMS", "sig_test"]
 logger.info("Start processing....")
-for installer in installers:
-    logger.info("Search vIMS results for installer %s" % installer)
-    request = Request(conf.URL_BASE + '?case=vims&installer=' + installer)
 
-    try:
-        response = urlopen(request)
-        k = response.read()
-        results = json.loads(k)
-    except URLError, e:
-        logger.error("Error code: %s" % e)
+# For all the versions
+for version in conf.versions:
+    for installer in installers:
+        logger.info("Search vIMS results for installer: %s, version: %s"
+                    % (installer, version))
+        request = Request(conf.URL_BASE + '?case=vims&installer=' +
+                          installer + "&version=" + version)
 
-    test_results = results['results']
+        try:
+            response = urlopen(request)
+            k = response.read()
+            results = json.loads(k)
+        except URLError, e:
+            logger.error("Error code: %s" % e)
 
-    logger.debug("Results found: %s" % test_results)
+        test_results = results['results']
 
-    scenario_results = {}
-    for r in test_results:
-        if not r['scenario'] in scenario_results.keys():
-            scenario_results[r['scenario']] = []
-        scenario_results[r['scenario']].append(r)
+        logger.debug("Results found: %s" % test_results)
 
-    for s, s_result in scenario_results.items():
-        scenario_results[s] = s_result[0:5]
-        logger.debug("Search for success criteria")
-        for result in scenario_results[s]:
-            result["start_date"] = result["start_date"].split(".")[0]
-            sig_test = result['details']['sig_test']['result']
-            if not sig_test == "" and isinstance(sig_test, list):
-                format_result = sig_test_format(sig_test)
-                if format_result['failures'] > format_result['passed']:
-                    result['details']['sig_test']['duration'] = 0
-                result['details']['sig_test']['result'] = format_result
-            nb_step_ok = 0
-            nb_step = len(result['details'])
+        scenario_results = {}
+        for r in test_results:
+            if not r['scenario'] in scenario_results.keys():
+                scenario_results[r['scenario']] = []
+            scenario_results[r['scenario']].append(r)
 
-            for step_name, step_result in result['details'].items():
-                if step_result['duration'] != 0:
-                    nb_step_ok += 1
-                m, s = divmod(step_result['duration'], 60)
-                m_display = ""
-                if int(m) != 0:
-                    m_display += str(int(m)) + "m "
-                step_result['duration_display'] = m_display + str(int(s)) + "s"
+        for s, s_result in scenario_results.items():
+            scenario_results[s] = s_result[0:5]
+            logger.debug("Search for success criteria")
+            for result in scenario_results[s]:
+                result["start_date"] = result["start_date"].split(".")[0]
+                sig_test = result['details']['sig_test']['result']
+                if not sig_test == "" and isinstance(sig_test, list):
+                    format_result = sig_test_format(sig_test)
+                    if format_result['failures'] > format_result['passed']:
+                        result['details']['sig_test']['duration'] = 0
+                    result['details']['sig_test']['result'] = format_result
+                nb_step_ok = 0
+                nb_step = len(result['details'])
 
-            result['pr_step_ok'] = 0
-            if nb_step != 0:
-                result['pr_step_ok'] = (float(nb_step_ok)/nb_step)*100
-            try:
-                logger.debug("Scenario %s, Installer %s"
-                             % (s_result[1]['scenario'], installer))
-                logger.debug("Orchestrator deployment: %s s"
-                             % result['details']['orchestrator']['duration'])
-                logger.debug("vIMS deployment: %s s"
-                             % result['details']['vIMS']['duration'])
-                logger.debug("Signaling testing: %s s"
-                             % result['details']['sig_test']['duration'])
-                logger.debug("Signaling testing results: %s"
-                             % format_result)
-            except:
-                logger.error("Data badly formatted")
-            logger.debug("------------------------------------------------")
+                for step_name, step_result in result['details'].items():
+                    if step_result['duration'] != 0:
+                        nb_step_ok += 1
+                    m, s = divmod(step_result['duration'], 60)
+                    m_display = ""
+                    if int(m) != 0:
+                        m_display += str(int(m)) + "m "
 
-    templateLoader = jinja2.FileSystemLoader(conf.REPORTING_PATH)
-    templateEnv = jinja2.Environment(loader=templateLoader)
+                    step_result['duration_display'] = m_display + str(int(s)) + "s"
 
-    TEMPLATE_FILE = "/template/index-vims-tmpl.html"
-    template = templateEnv.get_template(TEMPLATE_FILE)
+                result['pr_step_ok'] = 0
+                if nb_step != 0:
+                    result['pr_step_ok'] = (float(nb_step_ok)/nb_step)*100
+                try:
+                    logger.debug("Scenario %s, Installer %s"
+                                 % (s_result[1]['scenario'], installer))
+                    logger.debug("Orchestrator deployment: %s s"
+                                 % result['details']['orchestrator']['duration'])
+                    logger.debug("vIMS deployment: %s s"
+                                 % result['details']['vIMS']['duration'])
+                    logger.debug("Signaling testing: %s s"
+                                 % result['details']['sig_test']['duration'])
+                    logger.debug("Signaling testing results: %s"
+                                 % format_result)
+                except:
+                    logger.error("Data badly formatted")
+                logger.debug("----------------------------------------")
 
-    outputText = template.render(scenario_results=scenario_results,
-                                 step_order=step_order,
-                                 installer=installer)
+        templateLoader = jinja2.FileSystemLoader(conf.REPORTING_PATH)
+        templateEnv = jinja2.Environment(loader=templateLoader)
 
-    with open(conf.REPORTING_PATH +
-              "/release/master/index-vims-" +
-              installer + ".html", "wb") as fh:
-        fh.write(outputText)
+        TEMPLATE_FILE = "/template/index-vims-tmpl.html"
+        template = templateEnv.get_template(TEMPLATE_FILE)
+
+        outputText = template.render(scenario_results=scenario_results,
+                                     step_order=step_order,
+                                     installer=installer)
+
+        with open(conf.REPORTING_PATH +
+                  "/release/" + version + "/index-vims-" +
+                  installer + ".html", "wb") as fh:
+            fh.write(outputText)
 
 logger.info("vIMS report succesfully generated")
