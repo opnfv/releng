e179ec5cdff82921b07dbb297fe51b82f19fd9f5
wu.zhihui <wu.zhihui1@zte.com.cn>
2016-08-26 11:18:04 +0800

    qtip: move bash scripts out of qtip-ci-jobs.yml
    Change-Id: Ib48a1800b5d6d253c8bd8d383cf36c7119f6e9a4
    Signed-off-by: wu.zhihui <wu.zhihui1@zte.com.cn>

---
+++ jjb/qtip/qtip-cleanup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 ZTE and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+echo "Cleaning up QTIP  docker containers/images..."
+
+# Remove previous running containers if exist
+if [[ ! -z $(docker ps -a | grep opnfv/qtip) ]]; then
+    echo "Removing existing opnfv/qtip containers..."
+    running_containers=$(docker ps | grep opnfv/qtip | awk '{print $1}')
+    docker stop ${running_containers}
+    all_containers=$(docker ps -a | grep opnfv/qtip | awk '{print $1}')
+    docker rm ${all_containers}
+fi
+
+# Remove existing images if exist
+if [[ ! -z $(docker images | grep opnfv/qtip) ]]; then
+    echo "Docker images to remove:"
+    docker images | head -1 && docker images | grep opnfv/qtip
+    image_tags=($(docker images | grep opnfv/qtip | awk '{print $2}'))
+    for tag in "${image_tags[@]}"; do
+        echo "Removing docker image opnfv/qtip:$tag..."
+        docker rmi opnfv/qtip:$tag
+    done
+fi
+
