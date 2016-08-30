1a68455d59c4161997f706729cd70f31c9ab679d
jose.lausuch <jose.lausuch@ericsson.com>
2016-08-30 16:49:36 +0200

    [Functest] Push logs to artifacts regardless if loop fails
    This prevents from exiting the job without pushing the logs
    to artifacts if the functest loop fails.
    
    JIRA: FUNCTEST-443
    
    Change-Id: I6925b5855326df809dbce9f389b10be8a00cf8de
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ jjb/functest/functest-exit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+branch=${GIT_BRANCH##*/}
+ret_val_file="${HOME}/opnfv/functest/results/${branch}/return_value"
+if [ ! -f ${ret_val_file} ]; then
+    echo "Return value not found!"
+    exit -1
+fi
+
+ret_val=`cat ${ret_val_file}`
+
+exit ret_val
