ccba126c6130661e4ffdfa113b97dcf2512a3155
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-09-02 16:09:19 +0800

    correct backup-db.sh in kibana repo to keep consistent with testapi
    We change some interfaces in testapi, but this file doesn't change
    with that
    
    JIRA: FUNCTEST-455
    
    Change-Id: I105a82557244481bbdf9885fc166ebe975b050ea
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/scripts/backup-db.sh
+++ utils/test/scripts/backup-db.sh
@@ -18,16 +18,16 @@ echo "Create Directory for backup"
 mkdir -p $TARGET_DIR
 
 echo "Export results"
-mongoexport -db test_results_collection -c test_results --out $TARGET_DIR/results.json
+mongoexport --db test_results_collection -c results --out $TARGET_DIR/backup-results.json
 echo "Export test cases"
-mongoexport --db test_results_collection -c test_cases --out $TARGET_DIR/backup-cases.json
+mongoexport --db test_results_collection -c testcases --out $TARGET_DIR/backup-cases.json
 echo "Export projects"
-mongoexport --db test_results_collection -c test_projects --out $TARGET_DIR/backup-projects.json
+mongoexport --db test_results_collection -c projects --out $TARGET_DIR/backup-projects.json
 echo "Export pods"
-mongoexport --db test_results_collection -c pod --out $TARGET_DIR/backup-pod.json
+mongoexport --db test_results_collection -c pods --out $TARGET_DIR/backup-pod.json
 
 echo "Create tar.gz"
-tar -cvzf $TEST_RESULT_DB_BACKUP $TARGET_DIR
+#tar -cvzf $TEST_RESULT_DB_BACKUP $TARGET_DIR
 
 echo "Delete temp directory"
-rm -Rf $TARGET_DIR
+#rm -Rf $TARGET_DIR
