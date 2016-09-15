b0bf6cf3fb4316b689416682db4b7a2a82bfd49f
kalyanreddy <reddyx.gundarapu@intel.com>
2016-09-15 14:54:55 +0530

    kvmfornfv: Downloading sha512sum file of the guest image.
    This patch contains the script to download sha512sum file
    of the kvmfornfv guest image along with other artifacts.
    
    Change-Id: I2aa2e76124a12fb0d11975cda758f76e91f00390
    Signed-off-by: kalyanreddy <reddyx.gundarapu@intel.com>

--- jjb/kvmfornfv/kvmfornfv-download-artifact.sh
+++ jjb/kvmfornfv/kvmfornfv-download-artifact.sh
@@ -27,8 +27,10 @@ case "$JOB_TYPE" in
         exit 1
 esac
 
+GS_GUESTIMAGE_LOCATION="gs://artifacts.opnfv.org/$PROJECT/guest-image"
 /bin/mkdir -p $WORKSPACE/build_output
 gsutil cp -r $GS_UPLOAD_LOCATION/* $WORKSPACE/build_output > $WORKSPACE/gsutil.log 2>&1
+gsutil cp $GS_GUESTIMAGE_LOCATION/guest1.sha512 $WORKSPACE/build_output > $WORKSPACE/gsutil.log 2>&1
 
 echo "--------------------------------------------------------"
 ls -al $WORKSPACE/build_output
