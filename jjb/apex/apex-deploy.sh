ed2441e3ef3100c1a1adb91332672951803a8717
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-09-19 12:08:52 -0700

    Allow Asynchronous Apex Builds
    - Remove any build blockers on 'apex-build-*' jobs.
    - Moves manual deployment 'runner' jobs to own file
    - Adds script to download latest build artifacts before deployment.
    - Removes 'apex-gs-cleanup-{stream}' template since it was never
      generated as a job, and the same work is handled by the
      'artifact-cleanup-daily-{stream}' job.
    - Add 'disabled' macro to jobs so that streams, or individual scenarios
      can be disabled.
    - Merges 'apex-verify-unit-tests-{stream}' into 'apex-verify-{stream}'
      Unit Test verification was being duplicated by the unit-tests
      job.
    - Replace 'BUILD_DIRECTORY' wildcard checks with 'JOB_NAME' regex checks
    
    Change-Id: I4a51565686bd2011a310c3926d8260cd4641dca5
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/apex/apex-deploy.sh
+++ jjb/apex/apex-deploy.sh
@@ -11,70 +11,49 @@ echo "Starting the Apex virtual deployment."
 echo "--------------------------------------------------------"
 echo
 
-if ! rpm -q wget > /dev/null; then
-  sudo yum -y install wget
-fi
-
-if [[ $BUILD_DIRECTORY == *verify* ]]; then
-    # Build is from a verify, use local build artifacts (not RPMs)
-    cd $WORKSPACE/../${BUILD_DIRECTORY}
-    WORKSPACE=$(pwd)
-    echo "WORKSPACE modified to $WORKSPACE"
-    cd $WORKSPACE/ci
-elif [[ ! "$ARTIFACT_NAME" == "latest" ]]; then
-    # if artifact name is passed the pull a
-    # specific artifact from artifacts.opnfv.org
-    # artifact specified should be opnfv-apex-<version>.noarch.rpm
-    RPM_INSTALL_PATH=$GS_URL
-    RPM_LIST=$RPM_INSTALL_PATH/$ARTIFACT_NAME
-else
-    # Use latest RPMS
-    if [[ $BUILD_DIRECTORY == *apex-build* ]]; then
-      # Triggered from a daily so RPMS should be in local directory
-      BUILD_DIRECTORY=$WORKSPACE/../$BUILD_DIRECTORY
-      echo "BUILD DIRECTORY modified to $BUILD_DIRECTORY"
-
-      if [[ -f ${BUILD_DIRECTORY}/../opnfv.properties ]]; then
-        # if opnfv.properties exists then use the
-        # local build. Source the file so we get local OPNFV vars
-        source ${BUILD_DIRECTORY}/../opnfv.properties
-        RPM_INSTALL_PATH=${BUILD_DIRECTORY}/noarch
-        RPM_LIST=$RPM_INSTALL_PATH/$(basename $OPNFV_RPM_URL)
-      else
-        echo "BUILD_DIRECTORY is from a daily job, so will not use latest from URL"
-        echo "Check that the slave has opnfv.properties in $BUILD_DIRECTORY"
-        exit 1
-      fi
-    else
-      # use the latest from artifacts.opnfv.org
-      # get the latest.properties to get the link to the latest artifact
-      if ! wget -O $WORKSPACE/opnfv.properties http://$GS_URL/latest.properties; then
-        echo "ERROR: Unable to find latest.properties at ${GS_URL}...exiting"
-        exit 1
-      fi
-      # source the file so we get OPNFV vars
-      source opnfv.properties
-      RPM_INSTALL_PATH=$(echo "http://"$OPNFV_RPM_URL | sed 's/\/'"$(basename $OPNFV_RPM_URL)"'//')
-      RPM_LIST=${RPM_INSTALL_PATH}/$(basename $OPNFV_RPM_URL)
-    fi
-fi
-
 if [ -z "$DEPLOY_SCENARIO" ]; then
   echo "Deploy scenario not set!"
   exit 1
 fi
 
-# use local build for verify
-if [[ "$BUILD_DIRECTORY" == *verify* ]]; then
-    if [ ! -e "${WORKSPACE}/build/lib" ]; then
-      ln -s ${WORKSPACE}/lib ${WORKSPACE}/build/lib
-    fi
+
+# Setup Build Variables
+if [[ "$JOB_NAME" =~ "verify" ]]; then
     DEPLOY_SETTINGS_DIR="${WORKSPACE}/config/deploy"
     NETWORK_SETTINGS_DIR="${WORKSPACE}/config/network"
-    DEPLOY_CMD="$(pwd)/deploy.sh"
+    DEPLOY_CMD="${WORKSPACE}/ci/deploy.sh"
     RESOURCES="${WORKSPACE}/build/images/"
     CONFIG="${WORKSPACE}/build"
     LIB="${WORKSPACE}/lib"
+
+    RPM_INSTALL_PATH=$(echo "http://"$OPNFV_RPM_URL | sed 's/\/'"$(basename $OPNFV_RPM_URL)"'//')
+    RPM_LIST=${RPM_INSTALL_PATH}/$(basename $OPNFV_RPM_URL)
+
+    if [ ! -e "${WORKSPACE}/build/lib" ]; then
+      ln -s ${WORKSPACE}/lib ${WORKSPACE}/build/lib
+    fi
+else
+    DEPLOY_SETTINGS_DIR="/etc/opnfv-apex/"
+    NETWORK_SETTINGS_DIR="/etc/opnfv-apex/"
+    DEPLOY_CMD="opnfv-deploy"
+    RESOURCES="/var/opt/opnfv/images"
+    CONFIG="/var/opt/opnfv"
+    LIB="/var/opt/opnfv/lib"
+
+    if [[ "$ARTIFACT_NAME" == "latest" ]]; then
+        RPM_INSTALL_PATH=${BUILD_DIRECTORY}
+        RPM_LIST=$RPM_INSTALL_PATH/$(basename $OPNFV_RPM_URL)
+    else
+        # if artifact name is passed the pull a
+        # specific artifact from artifacts.opnfv.org
+        # artifact specified should be opnfv-apex-<version>.noarch.rpm
+        RPM_INSTALL_PATH=$GS_URL
+        RPM_LIST=$RPM_INSTALL_PATH/$ARTIFACT_NAME
+    fi
+fi
+
+# Install Dependencies
+if [[ "$JOB_NAME" =~ "verify" ]]; then
     # Make sure python34 deps are installed
     for dep_pkg in epel-release python34 python34-PyYAML python34-setuptools; do
       if ! rpm -q ${dep_pkg} > /dev/null; then
@@ -105,10 +84,10 @@ if [[ "$BUILD_DIRECTORY" == *verify* ]]; then
     else
         export PYTHONPATH=$PYTHONPATH:${WORKSPACE}/lib/python
     fi
-# use RPMs
 else
     # find version of RPM
     VERSION_EXTENSION=$(echo $(basename $RPM_LIST) | grep -Eo '[0-9]+\.[0-9]+-[0-9]{8}')
+
     # build RPM List which already includes base Apex RPM
     for pkg in ${APEX_PKGS}; do
         RPM_LIST+=" ${RPM_INSTALL_PATH}/opnfv-apex-${pkg}-${VERSION_EXTENSION}.noarch.rpm"
@@ -126,13 +105,6 @@ else
       echo "Unable to install new RPMs: $RPM_LIST"
       exit 1
     fi
-
-    DEPLOY_CMD=opnfv-deploy
-    DEPLOY_SETTINGS_DIR="/etc/opnfv-apex/"
-    NETWORK_SETTINGS_DIR="/etc/opnfv-apex/"
-    RESOURCES="/var/opt/opnfv/images"
-    CONFIG="/var/opt/opnfv"
-    LIB="/var/opt/opnfv/lib"
 fi
 
 # set env vars to deploy cmd
@@ -144,7 +116,7 @@ if [ "$OPNFV_CLEAN" == 'yes' ]; then
   else
     clean_opts=''
   fi
-  if [[ "$BUILD_DIRECTORY" == *verify* ]]; then
+  if [[ "$JOB_NAME" =~ "verify" ]]; then
     sudo CONFIG=${CONFIG} LIB=${LIB} ./clean.sh ${clean_opts}
   else
     sudo CONFIG=${CONFIG} LIB=${LIB} opnfv-clean ${clean_opts}
@@ -164,7 +136,7 @@ if [ ! -e "$DEPLOY_FILE" ]; then
   echo "ERROR: Required settings file missing: Deploy settings file ${DEPLOY_FILE}"
 fi
 
-if [[ "$JOB_NAME" == *virtual* ]]; then
+if [[ "$JOB_NAME" =~ "virtual" ]]; then
   # settings for virtual deployment
   if [ "$IPV6_FLAG" == "True" ]; then
     NETWORK_FILE="${NETWORK_SETTINGS_DIR}/network_settings_v6.yaml"
