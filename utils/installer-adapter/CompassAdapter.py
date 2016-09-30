3e0722db092f2a80a7c9d113f71d6fd39286d6c6
jose.lausuch <jose.lausuch@ericsson.com>
2016-09-30 17:24:31 +0200

    Installer adapters
    This tool might be used by any project in OPNFV
    to retrieve information about the deployed
    OpenStack nodes.
    It is python based and using paramiko.
    
    It can:
      - get info about deployment
      - get the info about the nodes (ip, mac, ...)
      - stablish ssh connection even with ProxyCommand option
      - run remote commands
      - scp to/from nodes (i.e. fetch credentials from controller)
    
    Added FuelAdapter as an example.
    
    To be completed:
    - ApexAdapter.py
    - JoidAdapter.py
    - CompassAdapter.py
    
    Change-Id: I49d8be96d754e0950e337aa2f88172341446fdd4
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ utils/installer-adapter/CompassAdapter.py
@@ -0,0 +1,29 @@
+##############################################################################
+# Copyright (c) 2016 Ericsson AB and others.
+# Author: Jose Lausuch (jose.lausuch@ericsson.com)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+from ssh_utils import SSH_Connection
+
+
+class CompassAdapter:
+
+    def __init__(self, installer_ip):
+        self.installer_ip = installer_ip
+
+    def get_deployment_info(self):
+        pass
+
+    def get_nodes(self):
+        pass
+
+    def get_controller_ips(self):
+        pass
+
+    def get_compute_ips(self):
+        pass
