cf35f1d7eeeced90eec28d13022df53c2ee629df
jose.lausuch <jose.lausuch@ericsson.com>
2016-09-30 17:24:31 +0200

    Installer adapters
    This tool might be used by any project in OPNFV
    to retrieve information about the deployed
    OpenStack nodes.
    It is python based and using paramiko.
    
    It can:
      - get info about deployment
      - get the info about the nodes (ip, mac, ...)
      - stablish ssh connection even with ProxyCommand option
      - run remote commands
      - scp to/from nodes (i.e. fetch credentials from controller)
    
    Added FuelAdapter as an example.
    
    To be completed:
    - ApexAdapter.py
    - JoidAdapter.py
    - CompassAdapter.py
    
    Change-Id: I49d8be96d754e0950e337aa2f88172341446fdd4
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ utils/installer-adapter/InstallerHandler.py
@@ -0,0 +1,62 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# Author: Jose Lausuch (jose.lausuch@ericsson.com)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from FuelAdapter import FuelAdapter
+from ApexAdapter import ApexAdapter
+from CompassAdapter import CompassAdapter
+from JoidAdapter import JoidAdapter
+
+
+INSTALLERS = ["fuel", "apex", "compass", "joid"]
+
+
+class InstallerHandler:
+
+    def __init__(self,
+                 installer,
+                 installer_ip,
+                 scenario,
+                 installer_user,
+                 installer_pwd=None):
+        self.installer = installer.lower()
+        self.installer_ip = installer_ip
+        self.installer_user = installer_user
+        self.installer_pwd = installer_pwd
+        self.scenario = scenario
+
+        if self.installer == INSTALLERS[0]:
+            self.InstallerAdapter = FuelAdapter(self.installer_ip,
+                                                self.installer_user,
+                                                self.installer_pwd)
+        elif self.installer == INSTALLERS[1]:
+            self.InstallerAdapter = ApexAdapter(self.installer_ip)
+        elif self.installer == INSTALLERS[2]:
+            self.InstallerAdapter = CompassAdapter(self.installer_ip)
+        elif self.installer == INSTALLERS[3]:
+            self.InstallerAdapter = JoidAdapter(self.installer_ip)
+        else:
+            print("Installer %s is  not valid. "
+                  "Please use one of the followings: %s"
+                  % (self.installer, INSTALLERS))
+            exit(1)
+
+    def get_deployment_info(self):
+        return self.InstallerAdapter.get_deployment_info()
+
+    def get_nodes(self):
+        return self.InstallerAdapter.get_nodes()
+
+    def get_controller_ips(self):
+        return self.InstallerAdapter.get_controller_ips()
+
+    def get_compute_ips(self):
+        return self.InstallerAdapter.get_compute_ips()
+
+    def get_all(self):
+        pass
