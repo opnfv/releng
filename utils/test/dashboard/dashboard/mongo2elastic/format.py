d31c9b16f1c9552d0376ce9ca5de94d84e39a897
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-10-11 15:41:45 +0800

    visualize qpi using kibana_dashboard
    JIRA: QTIP-122
    
    Change-Id: Ice29c0a841e911b23959c91322331c5a66281be5
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/dashboard/dashboard/mongo2elastic/format.py
+++ utils/test/dashboard/dashboard/mongo2elastic/format.py
@@ -184,3 +184,21 @@ def format_vims(testcase):
         }
     }
     return True
+
+
+def format_qpi(testcase):
+    """
+    Look for these and leave any of those:
+        details.index
+
+    If none are present, then return False
+    """
+    details = testcase['details']
+    if 'index' not in details:
+        return False
+
+    for key, value in details.items():
+        if key != 'index':
+            del details[key]
+
+    return True
