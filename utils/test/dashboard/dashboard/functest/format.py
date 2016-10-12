5ecc84446fd0e3dccd77f42dcc211c42b1adc0f9
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-10-12 11:15:39 +0800

    projectize
    Change-Id: I7dd0e5c495db8866ccab4304499bd2d3f73c0102
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/dashboard/dashboard/functest/format.py
+++ utils/test/dashboard/dashboard/functest/format.py
@@ -184,21 +184,3 @@ def format_vims(testcase):
         }
     }
     return True
-
-
-def format_qpi(testcase):
-    """
-    Look for these and leave any of those:
-        details.index
-
-    If none are present, then return False
-    """
-    details = testcase['details']
-    if 'index' not in details:
-        return False
-
-    for key, value in details.items():
-        if key != 'index':
-            del details[key]
-
-    return True
