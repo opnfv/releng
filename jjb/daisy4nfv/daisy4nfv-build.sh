63955af4b3606d5cfc0008faf0feb7de6c9c5c10
Jing Sun <sun.jing22@zte.com.cn>
2016-10-13 11:54:02 -0400

    implement build.sh of daisy4nfv
    Change-Id: Iea673c27d17e10e53f0d6b7ef04ccb9742d51739
    Signed-off-by: Jing Sun <sun.jing22@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-build.sh
+++ jjb/daisy4nfv/daisy4nfv-build.sh
@@ -4,3 +4,11 @@ echo "--------------------------------------------------------"
 echo "This is diasy4nfv build job!"
 echo "--------------------------------------------------------"
 
+# build output directory
+OUTPUT_DIR=$WORKSPACE/build_output
+mkdir -p $OUTPUT_DIR
+
+# start the build
+cd $WORKSPACE
+./ci/build.sh $OUTPUT_DIR
+
