131ecb05e00ffe28b559667a18d662b92395520c
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2016-11-08 05:04:24 -0500

    DO NOT MERGE, try to cleanup workspace
    Change-Id: I5e6d71ce6180dde47a2cff6164007ed903a082fb
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-basic.sh
+++ jjb/daisy4nfv/daisy4nfv-basic.sh
@@ -4,3 +4,4 @@ echo "--------------------------------------------------------"
 echo "This is diasy4nfv basic job!"
 echo "--------------------------------------------------------"
 
+sudo ls -la /home/jenkins-ci/opnfv/slave_root/workspace/daisy4nfv-verify-build-master
