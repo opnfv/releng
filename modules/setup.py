e310e1a4ba195579bd2114dfe25f0c242eb25abb
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-16 12:53:12 +0100

    Add setup.py for common OPNFV modules
    JIRA: RELENG-161
    
    Change-Id: I2423493d0c9c9c4ce78fe8c2669b9ed24e697b58
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ modules/setup.py
@@ -0,0 +1,21 @@
+##############################################################################
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="opnfv",
+    version="danube",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    url="https://www.opnfv.org",
+    install_requires=["paramiko>=2.0.1"]
+)
