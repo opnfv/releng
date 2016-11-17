900c31487a2160e8f4e07dc46d1ef8c9417f65bb
zeyu zhu <zhu.zeyu@zte.com.cn>
2016-11-17 11:18:46 +0800

    Implement virtual deployment in merge job of Daisy4nfv CI
    Change-Id: I2f0edea6a2f527e800c953458b4bb31741e970ab
    Signed-off-by: zeyu zhu <zhu.zeyu@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-virtual-deploy.sh
+++ jjb/daisy4nfv/daisy4nfv-virtual-deploy.sh
@@ -4,3 +4,24 @@ echo "--------------------------------------------------------"
 echo "This is diasy4nfv virtual deploy job!"
 echo "--------------------------------------------------------"
 
+cd $WORKSPACE
+
+if [[ "$NODE_NAME" =~ "-virtual" ]]; then
+    export NETWORK_CONF=./deploy/config/vm_environment/$NODE_NAME/network.yml
+    export DHA_CONF=./deploy/config/vm_environment/$NODE_NAME/deploy.yml
+else
+    exit 1
+fi
+
+./ci/deploy/deploy.sh ${DHA_CONF} ${NETWORK_CONF}
+
+if [ $? -ne 0 ]; then
+    echo "depolyment failed!"
+    deploy_ret=1
+fi
+
+echo
+echo "--------------------------------------------------------"
+echo "Done!"
+
+exit $deploy_ret
