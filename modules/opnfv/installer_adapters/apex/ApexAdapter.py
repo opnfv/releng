6dfc8659015790243bd7af53d8469d99a72c8833
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-17 23:03:44 +0100

    Create Apex Adapter
    JIRA: RELENG-152
    
    Change-Id: I293837695a4b82e57b9fac6027fe8197d68b1f14
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- modules/opnfv/installer_adapters/apex/ApexAdapter.py
+++ modules/opnfv/installer_adapters/apex/ApexAdapter.py
@@ -8,13 +8,38 @@
 ##############################################################################
 
 
+import opnfv.utils.SSHUtils as ssh_utils
+import opnfv.utils.OPNFVLogger as logger
+
+
 class ApexAdapter:
 
-    def __init__(self, installer_ip):
+    def __init__(self, installer_ip, pkey_file, user="stack"):
         self.installer_ip = installer_ip
+        self.installer_user = user
+        self.pkey_file = pkey_file
+        self.installer_connection = ssh_utils.get_ssh_client(
+            self.installer_ip,
+            self.installer_user,
+            pkey_file=self.pkey_file)
+        self.logger = logger.Logger("ApexHandler").getLogger()
+
+    def runcmd_apex_installer(self, cmd):
+        _, stdout, stderr = (self.installer_connection.exec_command(cmd))
+        error = stderr.readlines()
+        if len(error) > 0:
+            self.logger.error("error %s" % ''.join(error))
+            return error
+        output = ''.join(stdout.readlines())
+        return output
 
     def get_deployment_info(self):
-        pass
+        output = self.runcmd_apex_installer(
+            "source /home/stack/stackrc;nova list")
+        # self.logger(output)
+        dir(output)
+        type(output)
+        print output
 
     def get_nodes(self):
         pass
@@ -25,8 +50,17 @@ class ApexAdapter:
     def get_compute_ips(self):
         pass
 
-    def get_file_from_installer(self, origin, target, options=None):
-        pass
+    def get_file_from_installer(self, remote_path, local_path, options=None):
+        self.logger.debug("Fetching %s from Undercloud %s" %
+                          (remote_path, self.installer_ip))
+        get_file_result = ssh_utils.get_file(self.installer_connection,
+                                             remote_path,
+                                             local_path)
+        if get_file_result is None:
+            self.logger.error("SFTP failed to retrieve the file.")
+            return 1
+        self.logger.info("%s successfully copied from Undercloud to %s" %
+                         (remote_path, local_path))
 
     def get_file_from_controller(self, origin, target, ip=None, options=None):
         pass
