79febf5281630fe9532260757d9926c347c2e151
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-17 23:03:44 +0100

    Create Apex Adapter
    JIRA: RELENG-152
    
    Change-Id: I293837695a4b82e57b9fac6027fe8197d68b1f14
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- modules/opnfv/installer_adapters/apex/ApexAdapter.py
+++ modules/opnfv/installer_adapters/apex/ApexAdapter.py
@@ -8,10 +8,57 @@
 ##############################################################################
 
 
+import libvirt
+import re
+import subprocess
+import xml.etree.ElementTree as ET
+
+import opnfv.modules.utils.SSHUtils as ssh_utils
+import opnfv.modules.utils.OPNFVLogger as logger
+
+
 class ApexAdapter:
 
-    def __init__(self, installer_ip):
-        self.installer_ip = installer_ip
+    def __init__(self, user="root"):
+        self.logger = logger.Logger("FuelHandler").getLogger()
+        self.installer_ip = self.get_ip_from_installer()
+        self.installer_connection = ssh_utils.get_ssh_client(
+            self.installer_ip,
+            self.installer_user)
+
+    def get_ip_from_installer(self):
+        conn = libvirt.openReadOnly(None)
+        if conn == None:
+            raise Exception('Failed to open connection to the hypervisor')
+
+        try:
+            undercloud_vm = conn.lookupByName("undercloud")
+        except:
+            try:
+                undercloud_vm = conn.lookupByName("instack")
+            except:
+                raise Exception('Failed to find undercloud')
+
+        xml = vm.XMLDesc(0)
+        root = ET.fromstring(xml)
+        interface = root.findall("./devices/interface")[0]
+        mac = interface.find("mac")
+        mac_address = mac.get("address")
+        print mac_address
+
+        process = subprocess.Popen(['/usr/sbin/arp', '-a'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        process.wait()
+        for line in process.stdout:
+            if mac_address in line:
+                ip_address = re.search(
+                    r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', line)
+                print 'VM {0} with MAC Address {1} is using IP {2}'.format(
+                    vm.name(), mac_address, ip_address.groups(0)[0]
+                )
+            else:
+                print("Cannot find IP of undercloud in the ARP table")
 
     def get_deployment_info(self):
         pass
