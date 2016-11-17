7c95718fb909c5c2e1ce3feb5a5fab8301411d67
zeyu zhu <zhu.zeyu@zte.com.cn>
2016-11-17 11:18:46 +0800

    Implement virtual deployment in merge job of Daisy4nfv CI
    Change-Id: I2f0edea6a2f527e800c953458b4bb31741e970ab
    Signed-off-by: zeyu zhu <zhu.zeyu@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-virtual-deploy.sh
+++ jjb/daisy4nfv/daisy4nfv-virtual-deploy.sh
@@ -4,3 +4,6 @@ echo "--------------------------------------------------------"
 echo "This is diasy4nfv virtual deploy job!"
 echo "--------------------------------------------------------"
 
+# start the deploy
+cd $WORKSPACE
+./ci/deploy/deploy.sh
