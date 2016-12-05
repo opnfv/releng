6d6257f318de69f0698a7cdbc0f084bba37844e2
Tim Rozet <trozet@redhat.com>
2016-12-05 17:12:46 -0500

    Apex: Create promotion and verify job for Apex CSIT snapshots
    Changes Include:
     - Adds a daily job to create Apex snapshot tarball if functest passes
     - snapshot artifact uploaded to artifacts.opnfv.org
     - snapshot artifcat info is stored in snapshot.properties
     - 3rd party job updated to deploy from snapshot
     - 3rd party install netvirt job updated to use updated sdnvpn tools
    
    JIRA: APEX-364
    
    Depends-On: I792db0fac3f4e81969fe85c05fc298fe5af02537
    
    Change-Id: I8a875c081d756d098173c0baf11c0b4e4956b9fd
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/3rd_party_ci/install-netvirt.sh
+++ jjb/3rd_party_ci/install-netvirt.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SNAP_CACHE=$HOME/snap_cache
 # clone opnfv sdnvpn repo
 git clone https://gerrit.opnfv.org/gerrit/p/sdnvpn.git $WORKSPACE/sdnvpn
-. $WORKSPACE/sdnvpn/odl-pipeline/odl-pipeline-common.sh
-pushd $LIB
-./odl_reinstaller.sh --cloner-info $CLONER_INFO --odl-artifact $NETVIRT_ARTIFACT
-popd
+
+if [ ! -f "$NETVIRT_ARTIFACT" ]; then
+  echo "ERROR: ${NETVIRT_ARTIFACT} specified as NetVirt Artifact, but file does not exist"
+  exit 1
+fi
+
+# TODO (trozet) snapshot should have already been unpacked into cache folder
+# but we really should check the cache here, and not use a single cache folder
+# for when we support multiple jobs on a single slave
+pushd sdnvpn/odl-pipeline/lib > /dev/null
+./tripleo_manager.sh --update-opendaylight --pod-config ${SNAP_CACHE}/node.yaml \
+  --artifact ${NETVIRT_ARTIFACT} --ssh-key-file ${SNAP_CACHE}/id_rsa
+popd > /dev/null
