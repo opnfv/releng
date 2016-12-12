122f0f35d8b0a3b189647d30737d6cd81c3b5154
Julien <zhang.jun3g@zte.com.cn>
2016-12-12 22:41:38 +0800

    Add a seperate docker build job for daisy
    1. Daisy can not share opnfv-docker yaml for it is only affected by
    Dockerfile not including files in the repo.
    2. It depends on FSTrigger plugin in Jenkins.
    3. Suggest to use daisy instead of daisy4nfv as the job name according other
    installers.
    
    Change-Id: Ib2c9f0aab74e8b226a1b5c6cb4cf9c6a213b4186
    Signed-off-by: Julien-zte <zhang.jun3g@zte.com.cn>

---
+++ jjb/daisy4nfv/daisy-docker.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2016 Ericsson AB and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+echo "Starting daisy-docker for $DOCKER_REPO_NAME ..."
+echo "--------------------------------------------------------"
+echo
+
+
+if [[ -n $(ps -ef|grep 'docker build'|grep -v grep) ]]; then
+    echo "There is already another build process in progress:"
+    echo $(ps -ef|grep 'docker build'|grep -v grep)
+    # Abort this job since it will collide and might mess up the current one.
+    echo "Aborting..."
+    exit 1
+fi
+
+# Remove previous running containers if exist
+if [[ -n "$(docker ps -a | grep $DOCKER_REPO_NAME)" ]]; then
+    echo "Removing existing $DOCKER_REPO_NAME containers..."
+    docker ps -a | grep $DOCKER_REPO_NAME | awk '{print $1}' | xargs docker rm -f
+    t=60
+    # Wait max 60 sec for containers to be removed
+    while [[ $t -gt 0 ]] && [[ -n "$(docker ps| grep $DOCKER_REPO_NAME)" ]]; do
+        sleep 1
+        let t=t-1
+    done
+fi
+
+
+# Remove existing images if exist
+if [[ -n "$(docker images | grep $DOCKER_REPO_NAME)" ]]; then
+    echo "Docker images to remove:"
+    docker images | head -1 && docker images | grep $DOCKER_REPO_NAME
+    image_tags=($(docker images | grep $DOCKER_REPO_NAME | awk '{print $2}'))
+    for tag in "${image_tags[@]}"; do
+        if [[ -n "$(docker images|grep $DOCKER_REPO_NAME|grep $tag)" ]]; then
+            echo "Removing docker image $DOCKER_REPO_NAME:$tag..."
+            docker rmi -f $DOCKER_REPO_NAME:$tag
+        fi
+    done
+fi
+
+
+# cd to directory where Dockerfile is located
+cd $WORKSPACE/docker
+if [ ! -f ./Dockerfile ]; then
+    echo "ERROR: Dockerfile not found."
+    exit 1
+fi
+
+# Get tag version
+branch="${GIT_BRANCH##origin/}"
+echo "Current branch: $branch"
+
+if [[ "$branch" == "master" ]]; then
+    DOCKER_TAG="latest"
+else
+    if [[ "$RELEASE_VERSION" != "" ]]; then
+        release=$(echo $branch|sed 's/.*\///')
+        DOCKER_TAG=${release}.${RELEASE_VERSION}
+        # e.g. colorado.1.0, colorado.2.0, colorado.3.0
+    else
+        DOCKER_TAG="stable"
+    fi
+fi
+
+# Start the build
+echo "Building docker image: $DOCKER_REPO_NAME:$DOCKER_TAG"
+echo "--------------------------------------------------------"
+echo
+cmd="docker build --no-cache -t $DOCKER_REPO_NAME:$DOCKER_TAG --build-arg BRANCH=$branch ."
+
+echo ${cmd}
+${cmd}
+
+
+# list the images
+echo "Available images are:"
+docker images
+
+# Push image to Dockerhub
+if [[ "$PUSH_IMAGE" == "true" ]]; then
+    echo "Pushing $DOCKER_REPO_NAME:$DOCKER_TAG to the docker registry..."
+    echo "--------------------------------------------------------"
+    echo
+    docker push $DOCKER_REPO_NAME:$DOCKER_TAG
+fi
