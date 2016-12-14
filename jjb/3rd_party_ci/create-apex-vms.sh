867f03df972868297bf98cfdc0813f136b8ed87b
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-12-14 15:18:16 +0100

    Avoid removing the whole file system
    If scripts are used by hand and
    WORKSPACE is not set then rm -rf /*
    will remove the filesystem.
    
    Change-Id: I9bb9ee6643d3916fedc7d3848ecf9b03bde10fae
    Signed-off-by: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

--- jjb/3rd_party_ci/create-apex-vms.sh
+++ jjb/3rd_party_ci/create-apex-vms.sh
@@ -2,6 +2,8 @@
 set -e
 
 # wipe the WORKSPACE
+if [ -z ${WORKSPACE} ]; then echo "WORKSPACE is unset"; else echo "WORKSPACE is set to \"$WORKSPACE\""; fi
+WORKSPACE=${WORKSPACE:-$PWD}
 /bin/rm -rf $WORKSPACE/*
 
 # clone opnfv sdnvpn repo
--- jjb/3rd_party_ci/install-netvirt.sh
+++ jjb/3rd_party_ci/install-netvirt.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# wipe the WORKSPACE
+if [ -z ${WORKSPACE} ]; then echo "WORKSPACE is unset"; else echo "WORKSPACE is set to \"$WORKSPACE\""; fi
+WORKSPACE=${WORKSPACE:-$PWD}
+/bin/rm -rf $WORKSPACE/*
 set -e
 # clone opnfv sdnvpn repo
 git clone https://gerrit.opnfv.org/gerrit/p/sdnvpn.git $WORKSPACE/sdnvpn
