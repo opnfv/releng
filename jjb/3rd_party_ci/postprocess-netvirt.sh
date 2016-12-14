85875bf1074764aea8979d5500bdc699dad006e7
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-12-14 15:18:16 +0100

    Avoid removing the whole file system
    If scripts are used by hand and
    WORKSPACE is not set then rm -rf /*
    will remove the filesystem.
    
    Change-Id: I9bb9ee6643d3916fedc7d3848ecf9b03bde10fae
    Signed-off-by: Nikolas Hermanns <nikolas.hermanns@ericsson.com>

--- jjb/3rd_party_ci/postprocess-netvirt.sh
+++ jjb/3rd_party_ci/postprocess-netvirt.sh
@@ -2,6 +2,13 @@
 set -e
 
 # wipe the WORKSPACE
+if [ -z ${WORKSPACE} ]; then echo "WORKSPACE is unset"; else echo "WORKSPACE is set to \"$WORKSPACE\""; fi
+WORKSPACE=${WORKSPACE:-$PWD}
 /bin/rm -rf $WORKSPACE/*
 
-echo "Hello World"
+# clone opnfv sdnvpn repo
+git clone https://gerrit.opnfv.org/gerrit/p/sdnvpn.git $WORKSPACE/sdnvpn
+. $WORKSPACE/sdnvpn/odl-pipeline/odl-pipeline-common.sh
+pushd $LIB
+./postprocess.sh
+popd
