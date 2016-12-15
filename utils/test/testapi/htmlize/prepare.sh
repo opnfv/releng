0412f19936bb0f3452c6b1f8e399da2e59517a1f
rohitsakala <rohitsakala@gmail.com>
2016-12-16 00:38:41 +0530

    Create Jenkins Job for testapi automation
    Right now, only builder for creating testapi doc is included
    
    JIRA: FUNCTEST-664
    
    Change-Id: If0a34154084c1d01ed6b997d2226779da43bcb14
    Signed-off-by: rohitsakala <rohitsakala@gmail.com>

---
+++ utils/test/testapi/htmlize/prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Install Pre-requisites
+pip install requests
+
+# Swgger Codegen Tool
+url="http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar"
+
+#Check for jar file locally and in the repo
+if [ ! -f swagger-codegen-cli.jar ];
+then
+    wget http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar -O swagger-codegen-cli.jar
+fi
+
+# Start OPNFV Test API Server
+cd ../
+pip install -r requirements.txt
+./install.sh
+opnfv-testapi &
