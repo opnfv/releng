7aaa30f9b79393b4dd26bea9495964284ee4771d
rohitsakala <rohitsakala@gmail.com>
2016-12-16 00:38:41 +0530

    Create Jenkins Job for testapi automation
    Right now, only builder for creating testapi doc is included
    
    Change-Id: If0a34154084c1d01ed6b997d2226779da43bcb14
    Signed-off-by: rohitsakala <rohitsakala@gmail.com>

---
+++ utils/test/testapi/htmlize/start-server.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Install Pre-requisites
+pip install requests
+
+# Start OPNFV Test API Server
+cd ../
+pip install -r requirements.txt
+./install.sh
+opnfv-testapi &
