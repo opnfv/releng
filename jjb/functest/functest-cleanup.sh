488416437604b77e9f4bcff7cfc627f567fbf53c
helenyao <yaohelan@huawei.com>
2016-12-18 21:48:21 -0500

    opnfv/functest:<none> and its containers are removed
    JIRA: FUNCTEST-653
    
    opnfv/functest images with <none> tag are generated after functest images are pull many times.
    These images can be queried with filter "dangling".
    Logics are added to remove dangling images and their containers.
    
    Change-Id: I271f4d95c5fb62fdebe690d82330f6b25ac1e2f8
    Signed-off-by: helenyao <yaohelan@huawei.com>

--- jjb/functest/functest-cleanup.sh
+++ jjb/functest/functest-cleanup.sh
@@ -3,6 +3,17 @@
 [[ $CI_DEBUG == true ]] && redirect="/dev/stdout" || redirect="/dev/null"
 
 echo "Cleaning up docker containers/images..."
+
+# Remove containers along with image opnfv/functest:<none>
+if [[ ! -z $(docker images -q -f "dangling=true") ]]; then
+    images_dangling=($(docker images -q -f "dangling=true"))
+    echo "Removing image opnfv:functest:<none> and its containers..."
+    for image_id in "${images_dangling[@]}"; do
+        docker ps -a | grep $image_id | awk '{print $1}'| xargs docker rm -f >${redirect}
+        docker rmi $image_id >${redirect}
+    done
+fi
+
 # Remove previous running containers if exist
 if [[ ! -z $(docker ps -a | grep opnfv/functest) ]]; then
     echo "Removing existing opnfv/functest containers..."
@@ -12,10 +23,10 @@ fi
 # Remove existing images if exist
 if [[ $CLEAN_DOCKER_IMAGES == true ]] && [[ ! -z $(docker images | grep opnfv/functest) ]]; then
     echo "Docker images to remove:"
-    docker images | head -1 && docker images | grep opnfv/functest >${redirect}
+    (docker images | head -1 && docker images | grep opnfv/functest) >${redirect}
     image_tags=($(docker images | grep opnfv/functest | awk '{print $2}'))
     for tag in "${image_tags[@]}"; do
         echo "Removing docker image opnfv/functest:$tag..."
-        docker rmi opnfv/functest:$tag >/dev/null
+        docker rmi opnfv/functest:$tag >${redirect}
     done
 fi
