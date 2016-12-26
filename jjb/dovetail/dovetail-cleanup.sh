fdc045d4a2964c610b3f3c4f5846532f7c5a02d9
MatthewLi <matthew.lijun@huawei.com>
2016-12-26 01:47:17 -0500

    dovetail: rewrite cleanup images process
    Change-Id: Id4f9f1ddb7d84e5780479a7ca2bc979132dde8d4
    Signed-off-by: MatthewLi <matthew.lijun@huawei.com>

--- jjb/dovetail/dovetail-cleanup.sh
+++ jjb/dovetail/dovetail-cleanup.sh
@@ -2,14 +2,28 @@
 
 [[ $CI_DEBUG == true ]] && redirect="/dev/stdout" || redirect="/dev/null"
 
-echo "Cleaning up docker containers/images..."
-# Remove previous running containers if exist
+#clean up dovetail and its dependent project docker images with tag None
+clean_images=(opnfv/dovetail opnfv/functest opnfv/yardstick)
+for clean_image in "${clean_images[@]}"; do
+    echo "Remove containers with image ${clean_image}:<None>..."
+    dangling_images=($(docker images -qf "dangling=true" | grep ${clean_image} | awk '{print $3}'))
+    if [[ -n ${dangling_images} ]]; then
+        echo "Removing ${clean_image}:<none> images and their containers..."
+        for image_id in "${dangling_images[@]}"; do
+            echo "Removing image_id: $image_id and its containers"
+            docker ps -a | grep $image_id | awk '{print $1}'| xargs docker rm -f >${redirect}
+            docker rmi $image_id >${redirect}
+        done
+    fi
+done
+
+echo "Cleaning up dovetail docker containers/images..."
 if [[ ! -z $(docker ps -a | grep opnfv/dovetail) ]]; then
     echo "Removing existing opnfv/dovetail containers..."
     docker ps -a | grep opnfv/dovetail | awk '{print $1}' | xargs docker rm -f >${redirect}
 fi
 
-# Remove existing images if exist
+echo "Remove dovetail existing images if exist..."
 if [[ ! -z $(docker images | grep opnfv/dovetail) ]]; then
     echo "Docker images to remove:"
     docker images | head -1 && docker images | grep opnfv/dovetail >${redirect}
