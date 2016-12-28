1e87534588f30256b8fc127423fb0f8e553630ed
rexlee8776 <limingjiang@huawei.com>
2016-12-28 09:25:10 +0000

    improve yardstick logger to output Debug info to artifacts
    JIRA: RELENG-158
    
    Change-Id: I3bd91c671fa3dcac5cd47ca12610457fd1ab5a5b
    Signed-off-by: rexlee8776 <limingjiang@huawei.com>

--- utils/push-test-logs.sh
+++ utils/push-test-logs.sh
@@ -18,6 +18,7 @@ project=$PROJECT
 branch=${GIT_BRANCH##*/}
 testbed=$NODE_NAME
 dir_result="${HOME}/opnfv/$project/results/${branch}"
+
 # src: https://wiki.opnfv.org/display/INF/Hardware+Infrastructure
 # + intel-pod3 (vsperf)
 node_list=(\
