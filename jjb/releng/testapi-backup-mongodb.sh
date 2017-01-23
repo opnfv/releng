dbbc5302c256356ae646afa7b4a13e128e48b30f
rohitsakala <rohitsakala@gmail.com>
2017-01-24 02:26:53 +0530

    Add jenkins job for testapi mongodb backup
    Mongodb backup will run every week and will get pushed into
    artifacts.opnfv.org/testing/
    
    JIRA: FUNCTEST-701
    
    Change-Id: I3a6fee59a1b8b5bdc46f07f6f6e9f5a60b77f24f
    Signed-off-by: rohitsakala <rohitsakala@gmail.com>

---
+++ jjb/releng/testapi-backup-mongodb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Directory for results
+mkdir $WORKSPACE/results/
+
+# Run MongoDB backup
+python $WORKSPACE/utils/test/testapi/update/templates/backup_mongodb.py -o $WORKSPACE/results/
+
+workspace=$WORKSPACE
+artifact_dir="testapibackup"
+
+set +e
+/usr/local/bin/gsutil &>/dev/null
+if [ $? != 0 ]; then
+    echo "Not possible to push results to artifact: gsutil not installed"
+    exit 1
+else
+    echo "Uploading mongodump to artifact $artifact_dir"
+    /usr/local/bin/gsutil cp -r "$workspace"/results/test_results_collection* gs://artifacts.opnfv.org/"$artifact_dir"/
+    echo "MongoDump can be found at http://artifacts.opnfv.org/$artifact_dir"
+fi
