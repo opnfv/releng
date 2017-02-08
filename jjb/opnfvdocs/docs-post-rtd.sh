8ff6aae05b5a2e676f1fb68508f9356857f79d4a
Aric Gardner <agardner@linuxfoundation.org>
2017-02-08 23:37:48 +0000

    Revert "Add new jobs for doc merge and verify"
    This reverts commit fd8d915b212fb8adc943a74b537b5b5efffeaaad.
    
    Change-Id: I9e56196f2ac84b72af2f9b51bbcd663fdc4e02c3

--- jjb/opnfvdocs/docs-post-rtd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $GERRIT_BRANCH == "master" ]; then
-    RTD_BUILD_VERSION=latest
-else
-    RTD_BUILD_VERSION=${{GERRIT_BRANCH/\//-}}
-fi
-curl -X POST --data "version_slug=$RTD_BUILD_VERSION" https://readthedocs.org/build/{rtdproject}
