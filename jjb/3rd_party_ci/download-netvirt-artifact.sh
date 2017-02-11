d752c2cdf6cb76ad8bd37958a6289f305fd102dc
Tim Rozet <trozet@redhat.com>
2017-02-11 09:38:46 -0500

    CSIT: Fixes zip2tar conversion for ODL artifact
    File modes were not being preserved using the python tool, hence
    manually unpacking and re-packing now into tar.gz
    
    Change-Id: I40289117ec74ea2717a2428e9542da3dc98f590e
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/3rd_party_ci/download-netvirt-artifact.sh
+++ jjb/3rd_party_ci/download-netvirt-artifact.sh
@@ -3,16 +3,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+ODL_ZIP=distribution-karaf-0.6.0-SNAPSHOT.zip
+
 echo "Attempting to fetch the artifact location from ODL Jenkins"
 CHANGE_DETAILS_URL="https://git.opendaylight.org/gerrit/changes/netvirt~master~$GERRIT_CHANGE_ID/detail"
 # due to limitation with the Jenkins Gerrit Trigger, we need to use Gerrit REST API to get the change details
 ODL_BUILD_JOB_NUM=$(curl -s $CHANGE_DETAILS_URL | grep -Eo 'netvirt-distribution-check-carbon/[0-9]+' | tail -1 | grep -Eo [0-9]+)
 
-NETVIRT_ARTIFACT_URL="https://jenkins.opendaylight.org/releng/job/netvirt-distribution-check-carbon/${ODL_BUILD_JOB_NUM}/artifact/distribution-karaf-0.6.0-SNAPSHOT.zip"
+NETVIRT_ARTIFACT_URL="https://jenkins.opendaylight.org/releng/job/netvirt-distribution-check-carbon/${ODL_BUILD_JOB_NUM}/artifact/${ODL_ZIP}"
 echo -e "URL to artifact is\n\t$NETVIRT_ARTIFACT_URL"
 
 echo "Downloading the artifact. This could take time..."
-wget -q -O distribution-karaf-0.6.0-SNAPSHOT.zip $NETVIRT_ARTIFACT_URL
+wget -q -O $ODL_ZIP $NETVIRT_ARTIFACT_URL
 if [[ $? -ne 0 ]]; then
     echo "The artifact does not exist! Probably removed due to ODL Jenkins artifact retention policy."
     echo "Rerun netvirt-patch-test-current-carbon to get artifact rebuilt."
@@ -21,8 +23,8 @@ fi
 
 #TODO(trozet) remove this once odl-pipeline accepts zip files
 echo "Converting artifact zip to tar.gz"
-sudo pip install ruamel.zip2tar
-zip2tar distribution-karaf-0.6.0-SNAPSHOT.zip --gz --tar-file-name /tmp/${NETVIRT_ARTIFACT}
+unzip $ODL_ZIP
+tar czf /tmp/${NETVIRT_ARTIFACT} $(echo $ODL_ZIP | sed -n 's/\.zip//p')
 
 echo "Download complete"
 ls -al /tmp/${NETVIRT_ARTIFACT}
