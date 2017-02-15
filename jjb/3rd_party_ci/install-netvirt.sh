3e958fe5298a3e7ff7be8c8c7610ff150f44e8f3
Tim Rozet <trozet@redhat.com>
2017-02-15 16:16:27 -0500

    Apex,CSIT: Updates install netvirt phase
    Updates the odl_reinstaller script call to be able to reconfigure
    external neutron networks during netvirt artifact upgrade.
    
    Depends-On: I083da7e8aae2891d36dc2fde756af32778b04b52
    
    Change-Id: I1839371a78dca160c3bb23ead94882af18c3ef9f
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/3rd_party_ci/install-netvirt.sh
+++ jjb/3rd_party_ci/install-netvirt.sh
@@ -12,22 +12,19 @@ if [ ! -f "/tmp/${NETVIRT_ARTIFACT}" ]; then
   exit 1
 fi
 
-if [ ! -f "${SNAP_CACHE}/node.yaml" ]; then
-  echo "ERROR: node.yaml pod config missing in ${SNAP_CACHE}"
-  exit 1
-fi
-
-if [ ! -f "${SNAP_CACHE}/id_rsa" ]; then
-  echo "ERROR: id_rsa ssh creds missing in ${SNAP_CACHE}"
-  exit 1
-fi
+for file in node.yaml id_rsa overcloudrc; do
+  if [ ! -f "${SNAP_CACHE}/${file}" ]; then
+    echo "ERROR: ${file} missing in ${SNAP_CACHE}"
+    exit 1
+  fi
+done
 
 # TODO (trozet) snapshot should have already been unpacked into cache folder
 # but we really should check the cache here, and not use a single cache folder
 # for when we support multiple jobs on a single slave
 pushd sdnvpn/odl-pipeline/lib > /dev/null
-# FIXME (trozet) remove this once permissions are fixed in sdnvpn repo
-chmod +x odl_reinstaller.sh
+
 ./odl_reinstaller.sh --pod-config ${SNAP_CACHE}/node.yaml \
-  --odl-artifact /tmp/${NETVIRT_ARTIFACT} --ssh-key-file ${SNAP_CACHE}/id_rsa
+  --odl-artifact /tmp/${NETVIRT_ARTIFACT} --ssh-key-file ${SNAP_CACHE}/id_rsa \
+  --credentials ${SNAP_CACHE}/overcloudrc
 popd > /dev/null
