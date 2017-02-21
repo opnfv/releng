0f90de81b3529166be21860c126d5e7e700845db
jose.lausuch <jose.lausuch@ericsson.com>
2017-02-21 21:48:52 +0100

    [Functest] Refactor Functest-suite job
    - Remove if clauses for old releases (not used any more)
    - Use python call to run_tests.py instead of CLI
       This will allow us to return the proper value to
       the job. The CLI command returns always 0.
    - Remove push logs to artifacts
        Functest-suite is not meant for daily CI runs
    
    Change-Id: I34d99cbe5af07a1fcf583a9a0b6ac6ab286c9adc
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- jjb/functest/functest-suite.sh
+++ jjb/functest/functest-suite.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-set -e
 
-echo "Functest: run $FUNCTEST_SUITE_NAME on branch $BRANCH"
-if [[ "$BRANCH" =~ 'brahmaputra' ]]; then
-    cmd="${FUNCTEST_REPO_DIR}/docker/run_tests.sh --test $FUNCTEST_SUITE_NAME"
-elif [[ "$BRANCH" =~ 'colorado' ]]; then
-    cmd="python ${FUNCTEST_REPO_DIR}/ci/run_tests.py -t $FUNCTEST_SUITE_NAME"
-else
-    cmd="functest testcase run $FUNCTEST_SUITE_NAME"
-fi
+FUNCTEST_SUITE_NAME=barometer,vping_userdata
 container_id=$(docker ps -a | grep opnfv/functest | awk '{print $1}' | head -1)
-docker exec $container_id $cmd
+if [ -z $container_id ]; then
+    echo "Functest container not found"
+    exit 1
+fi
 
-ret_value=$?
-ret_val_file="${HOME}/opnfv/functest/results/${BRANCH##*/}/return_value"
-echo ${ret_value}>${ret_val_file}
+global_ret_val=0
 
-exit 0
+tests=($(echo $FUNCTEST_SUITE_NAME | tr "," "\n"))
+for test in ${tests[@]}; do
+    cmd="python /home/opnfv/repos/functest/functest/ci/run_tests.py -t $test"
+    docker exec $container_id $cmd
+    ret_val=$?
+    if [[ $ret_val -ne 0 ]]; then
+        global_ret_val=1
+    fi
+done
+
+exit $global_ret_val
