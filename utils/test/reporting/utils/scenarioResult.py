88245fe59ee27b1facf37455d59f0245f6503c85
Morgan Richomme <morgan.richomme@orange.com>
2017-03-06 14:44:47 +0100

    bug fix: last 4 run reporting for storperf
    - bug fix:
    copy/paste of yardstick code but list of results
    is not sorted by default, taking the last 4 array
    items lead to random results on the 4 last run
    sort results before taking the last 4 results
    to establish last 4 run criteria
    - enhancement: add link to last CI run in the
    reporting page
    
    Change-Id: Ice16832a81cb65503a63b16321a2be92a427d279
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- utils/test/reporting/utils/scenarioResult.py
+++ utils/test/reporting/utils/scenarioResult.py
@@ -10,11 +10,12 @@
 
 class ScenarioResult(object):
     def __init__(self, status, four_days_score='', ten_days_score='',
-                 score_percent=0.0):
+                 score_percent=0.0, last_url=''):
         self.status = status
         self.four_days_score = four_days_score
         self.ten_days_score = ten_days_score
         self.score_percent = score_percent
+        self.last_url = last_url
 
     def getStatus(self):
         return self.status
@@ -27,3 +28,6 @@ class ScenarioResult(object):
 
     def getScorePercent(self):
         return self.score_percent
+
+    def getLastUrl(self):
+        return self.last_url
