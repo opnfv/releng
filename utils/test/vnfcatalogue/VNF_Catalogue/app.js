4cfd9c4c7273ce27a9e4b399900bd98c492e3e57
Kumar Rishabh <shailrishabh@gmail.com>
2017-03-15 01:02:11 +0530

    Add vnf, tag create & Associate Plugins
    Adds support for accessing database, plugin to create tags and
    vnfs and make association between them. Also adds autocomplete feature
    to vnf-tag association.
    
    Change-Id: Id55f998df68ae5e6e6fd298c6393b3500777468a
    Signed-off-by: Kumar Rishabh <shailrishabh@gmail.com>

--- utils/test/vnfcatalogue/VNF_Catalogue/app.js
+++ utils/test/vnfcatalogue/VNF_Catalogue/app.js
@@ -13,9 +13,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 var routes = require('./routes/index');
 var search_projects = require('./routes/search_projects');
+var project_profile = require('./routes/project_profile');
+var add_project = require('./routes/add_project');
+var add_tag = require('./routes/add_tag');
+var search_tag = require('./routes/search_tag');
+var search_vnf = require('./routes/search_vnf');
+var vnf_tag_association = require('./routes/vnf_tag_association');
+
 
 var app = express();
 
@@ -23,27 +31,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+db_pool = require('./database').pool;
+
 // Database
-var db = require('mysql2');
+//var db = require('mysql2');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
-    req.db = db;
+    //db_pool size 50 default
+    req.db_pool = db_pool;
     next();
 });
 
 app.use('/', routes);
 app.use('/search_projects', search_projects);
-
-
+app.use('/project_profile', project_profile);
+app.use('/add_project', add_project);
+app.use('/add_tag', add_tag);
+app.use('/search_tag', search_tag);
+app.use('/search_vnf', search_vnf);
+app.use('/vnf_tag_association', vnf_tag_association);
 // Some Error handling for now #TODO Remove
 
 /// catch 404 and forwarding to error handler
