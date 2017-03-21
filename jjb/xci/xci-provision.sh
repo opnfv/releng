8c79c9ce995f2ef6dd3184c40848b89ac9c14e9e
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2017-03-21 13:41:43 +0100

    xci: Enable OpenStack installation using openstack-ansible for daily runs
    Change-Id: I6ae53f22147adbe1c3ad79ad6d3ed6f716f21892
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- jjb/xci/xci-provision.sh
+++ jjb/xci/xci-provision.sh
@@ -44,8 +44,8 @@ sudo /bin/rm -rf /opt/bifrost /opt/openstack-ansible /opt/stack /opt/releng /opt
 fix_ownership
 
 # clone all the repos first and checkout the patch afterwards
-OPENSTACK_BRANCH=${OPENSTACK_BRANCH:-master}
-OPNFV_BRANCH=${OPNFV_BRANCH:-master}
+export OPENSTACK_BRANCH=${OPENSTACK_BRANCH:-master}
+export OPNFV_BRANCH=${OPNFV_BRANCH:-master}
 sudo git clone -b $OPENSTACK_BRANCH https://git.openstack.org/openstack/bifrost /opt/bifrost
 sudo git clone -b $OPNFV_BRANCH https://gerrit.opnfv.org/gerrit/releng /opt/releng
 
