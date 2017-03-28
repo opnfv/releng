a354d385b2b22d6c675dde60ce473f7b54b69ab6
Fatih Degirmenci <fatih.degirmenci@ericsson.com>
2017-03-28 19:02:19 +0200

    xci: Rename the deployment host to opnfv
    Reflect the changes to other impacted files as well.
    
    Change-Id: I106f4e47fe5c75d288f4878fe6ec9f8ff39c652e
    Signed-off-by: Fatih Degirmenci <fatih.degirmenci@ericsson.com>

--- prototypes/xci/xci-deploy.sh
+++ prototypes/xci/xci-deploy.sh
@@ -13,11 +13,11 @@ fi
 # find where are we
 XCI_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# source pinned versions
-source $XCI_PATH/config/pinned-versions
-
 # source user vars
 source $XCI_PATH/config/user-vars
+
+# source pinned versions
+source $XCI_PATH/config/pinned-versions
 
 # source flavor configuration
 source "$XCI_PATH/flavors/${XCI_FLAVOR}-vars"
