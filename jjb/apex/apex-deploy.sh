65b1aa43850a1154a9153a2bf7b3271f46ca0d5d
Feng Pan <fpan@redhat.com>
2017-04-05 20:54:49 -0400

    APEX: Reduce virtual ram on compute nodes for fdio/ovs scenarios
    Change-Id: I7df2c6a6a940f49268146334cb7f77d3fffeb665
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- jjb/apex/apex-deploy.sh
+++ jjb/apex/apex-deploy.sh
@@ -189,7 +189,7 @@ if [[ "$JOB_NAME" == *virtual* ]]; then
   # settings for virtual deployment
   DEPLOY_CMD="${DEPLOY_CMD} -v"
   if [[ "${DEPLOY_SCENARIO}" =~ fdio|ovs ]]; then
-    DEPLOY_CMD="${DEPLOY_CMD} --virtual-default-ram 14 --virtual-compute-ram 8"
+    DEPLOY_CMD="${DEPLOY_CMD} --virtual-default-ram 14 --virtual-compute-ram 6"
   fi
   if [[ "$JOB_NAME" == *csit* ]]; then
     DEPLOY_CMD="${DEPLOY_CMD} -e csit-environment.yaml"
