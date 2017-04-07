ebfcb06123168dd9cfaa1de5b382a19d978c1c2d
Feng Pan <fpan@redhat.com>
2017-04-07 16:52:12 -0400

    APEX: Remove setting of hugepages
    Change-Id: Ic482f75bb905082f503958c1be01c9cd7e68566a
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- jjb/apex/apex-deploy.sh
+++ jjb/apex/apex-deploy.sh
@@ -189,11 +189,7 @@ if [[ "$JOB_NAME" == *virtual* ]]; then
   # settings for virtual deployment
   DEPLOY_CMD="${DEPLOY_CMD} -v"
   if [[ "${DEPLOY_SCENARIO}" =~ fdio|ovs ]]; then
-    DEPLOY_CMD="${DEPLOY_CMD} --virtual-default-ram 12 --virtual-compute-ram 4"
-    TMP_DEPLOY_FILE="${WORKSPACE}/${DEPLOY_SCENARIO}.yaml"
-    cp -f ${DEPLOY_FILE} ${TMP_DEPLOY_FILE}
-    sed -i 's/^\(\s*hugepages:\).*$/\1 1024/g' ${TMP_DEPLOY_FILE}
-    DEPLOY_FILE=${TMP_DEPLOY_FILE}
+    DEPLOY_CMD="${DEPLOY_CMD} --virtual-default-ram 12 --virtual-compute-ram 6"
   fi
   if [[ "$JOB_NAME" == *csit* ]]; then
     DEPLOY_CMD="${DEPLOY_CMD} -e csit-environment.yaml"
