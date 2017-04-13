72e42c093439ebb723e615d1a3d58b02d118d92e
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-04-13 16:04:09 +0800

    leverage executor to test_testcase.py in TestAPI
    Change-Id: Id03524c81ccbc4dd4dd1f795e5b4edef8e6bfc7e
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/testapi/opnfv_testapi/tests/unit/executor.py
+++ utils/test/testapi/opnfv_testapi/tests/unit/executor.py
@@ -10,70 +10,73 @@ import functools
 import httplib
 
 
-def create(excepted_status, excepted_response, *args):
+def create(excepted_status, excepted_response):
     def _create(create_request):
         @functools.wraps(create_request)
         def wrap(self):
             request = create_request(self)
-            status, body = self.create(request, *args)
+            status, body = self.create(request)
             if excepted_status == httplib.OK:
-                getattr(self, excepted_response)(body, *args)
+                getattr(self, excepted_response)(body)
             else:
                 self.assertIn(excepted_response, body)
         return wrap
     return _create
 
 
-def get(excepted_status, excepted_response, *args):
+def get(excepted_status, excepted_response):
     def _get(get_request):
         @functools.wraps(get_request)
         def wrap(self):
             request = get_request(self)
-            status, body = self.get(request, *args)
+            status, body = self.get(request)
             if excepted_status == httplib.OK:
-                getattr(self, excepted_response)(body, *args)
+                getattr(self, excepted_response)(body)
             else:
                 self.assertIn(excepted_response, body)
         return wrap
     return _get
 
 
-def update(excepted_status, excepted_response, *args):
+def update(excepted_status, excepted_response):
     def _update(update_request):
         @functools.wraps(update_request)
         def wrap(self):
             request, resource = update_request(self)
-            status, body = self.update(request, resource, *args)
+            status, body = self.update(request, resource)
             if excepted_status == httplib.OK:
-                getattr(self, excepted_response)(request, body, *args)
+                getattr(self, excepted_response)(request, body)
             else:
                 self.assertIn(excepted_response, body)
         return wrap
     return _update
 
 
-def delete(excepted_status, excepted_response, *args):
+def delete(excepted_status, excepted_response):
     def _delete(delete_request):
         @functools.wraps(delete_request)
         def wrap(self):
             request = delete_request(self)
-            status, body = self.delete(request, *args)
+            if isinstance(request, tuple):
+                status, body = self.delete(request[0], *(request[1]))
+            else:
+                status, body = self.delete(request)
             if excepted_status == httplib.OK:
-                getattr(self, excepted_response)(body, *args)
+                getattr(self, excepted_response)(body)
             else:
                 self.assertIn(excepted_response, body)
         return wrap
     return _delete
 
 
-def query(excepted_status, excepted_response, number=0, *args):
+def query(excepted_status, excepted_response, number=0):
     def _query(get_request):
         @functools.wraps(get_request)
         def wrap(self):
             request = get_request(self)
-            status, body = self.query(request, *args)
+            status, body = self.query(request)
             if excepted_status == httplib.OK:
-                getattr(self, excepted_response)(body, number, *args)
+                getattr(self, excepted_response)(body, number)
             else:
                 self.assertIn(excepted_response, body)
         return wrap
