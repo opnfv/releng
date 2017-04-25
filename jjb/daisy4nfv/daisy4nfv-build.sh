a9e9b56798a7478774270863775e0fee6c80e25b
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-04-25 05:23:19 -0400

    Add set -o to daisy4nfv-build.sh
    By using set -o, this can prevent CI from uploading *.properties
    files without *.bin to artifact.opnfv.org if build was failed.
    
    This once happened to [1].
    
    [1] http://artifacts.opnfv.org/daisy/opnfv-2017-04-25_06-57-05.properties
    
    Change-Id: I26c3112af1d73a84df05c3852038c99d7070ab59
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-build.sh
+++ jjb/daisy4nfv/daisy4nfv-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 echo "--------------------------------------------------------"
 echo "This is diasy4nfv build job!"
 echo "--------------------------------------------------------"
