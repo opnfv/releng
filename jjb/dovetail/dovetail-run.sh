44df09b5fbf564c014020ea98ce7650b46bfd771
xudan <xudan16@huawei.com>
2017-04-27 06:53:32 +0000

    dovetail: create file pod.yaml for running HA test cases
    JIRA: DOVETAIL-407
    
    1. HA test cases need a file pod.yaml giving the info of the pod
       (node's ip, username, password/key_file).
    2. Dovetail client has finished the adaption JIRA: DOVETAIL-405.
    3. CI uses create_pod_file.py to generate /home/opnfv/dovetail/userconfig/pod.yaml
    4. If INSTALLER_TYPE is fuel, fetch it's jumpserver key to
       /home/opnfv/dovetail/userconfig/id_rsa
    
    Change-Id: Iaf6afbdfb8e4331ae9a10ea3df060c37e9010a0a
    Signed-off-by: xudan <xudan16@huawei.com>

--- jjb/dovetail/dovetail-run.sh
+++ jjb/dovetail/dovetail-run.sh
@@ -32,10 +32,11 @@ if ! sudo iptables -C FORWARD -j RETURN 2> ${redirect} || ! sudo iptables -L FOR
     sudo iptables -I FORWARD -j RETURN
 fi
 
+releng_repo=${WORKSPACE}/releng
+[ -d ${releng_repo} ] && sudo rm -rf ${releng_repo}
+git clone https://gerrit.opnfv.org/gerrit/releng ${releng_repo} >/dev/null
+
 if [[ ${INSTALLER_TYPE} != 'joid' ]]; then
-    releng_repo=${WORKSPACE}/releng
-    [ -d ${releng_repo} ] && sudo rm -rf ${releng_repo}
-    git clone https://gerrit.opnfv.org/gerrit/releng ${releng_repo} >/dev/null
     ${releng_repo}/utils/fetch_os_creds.sh -d ${OPENRC} -i ${INSTALLER_TYPE} -a ${INSTALLER_IP} >${redirect}
 fi
 
@@ -47,16 +48,55 @@ else
     exit 1
 fi
 
+pip install virtualenv
+
+cd ${releng_repo}/modules
+virtualenv venv
+source venv/bin/activate
+sudo pip install -e ./ >/dev/null
+
+if [[ ${INSTALLER_TYPE} == compass ]]; then
+    options="-u root -p root"
+elif [[ ${INSTALLER_TYPE} == fuel ]]; then
+    options="-u root -p r00tme"
+else
+    echo "Don't support to generate pod.yaml on ${INSTALLER_TYPE} currently."
+    echo "HA test cases may not run properly."
+fi
+
+pod_file_dir="/home/opnfv/dovetail/userconfig"
+cmd="sudo python ${releng_repo}/utils/create_pod_file.py -t ${INSTALLER_TYPE} -i ${INSTALLER_IP} ${options} -f ${pod_file_dir}/pod.yaml"
+echo ${cmd}
+${cmd}
+
+deactivate
+
+if [ -f ${pod_file_dir}/pod.yaml ]; then
+    echo "file ${pod_file_dir}/pod.yaml:"
+    cat ${pod_file_dir}/pod.yaml
+else
+    echo "Error: There doesn't exist file ${pod_file_dir}/pod.yaml."
+    echo "HA test cases may not run properly."
+fi
+
+ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+if [ "$INSTALLER_TYPE" == "fuel" ]; then
+    echo "Fetching id_rsa file from jump_server $INSTALLER_IP..."
+    sshpass -p r00tme sudo scp $ssh_options root@${INSTALLER_IP}:~/.ssh/id_rsa ${pod_file_dir}/id_rsa
+fi
+
 opts="--privileged=true -id"
 results_envs="-v /var/run/docker.sock:/var/run/docker.sock \
               -v /home/opnfv/dovetail/results:/home/opnfv/dovetail/results"
 openrc_volume="-v ${OPENRC}:${OPENRC}"
+userconfig_volume="-v ${pod_file_dir}:${pod_file_dir}"
 
 # Pull the image with correct tag
 echo "Dovetail: Pulling image opnfv/dovetail:${DOCKER_TAG}"
 docker pull opnfv/dovetail:$DOCKER_TAG >$redirect
 
-cmd="docker run ${opts} ${results_envs} ${openrc_volume} \
+cmd="docker run ${opts} ${results_envs} ${openrc_volume} ${userconfig_volume} \
      ${sshkey} opnfv/dovetail:${DOCKER_TAG} /bin/bash"
 echo "Dovetail: running docker run command: ${cmd}"
 ${cmd} >${redirect}
