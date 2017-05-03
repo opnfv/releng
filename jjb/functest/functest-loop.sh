d958103210087fae3935099e6789fc4aab8384b4
jose.lausuch <jose.lausuch@ericsson.com>
2017-05-03 15:20:40 +0200

    [Functest] Remove Colorado and Brahmaputra support
    CI no longer runs brahmaputra and colorado jobs.
    To keep the jobs as simple as possible, remove
    that support.
    
    Change-Id: I29661f414923807dc6c0d22526dc852a45dbe7bb
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- jjb/functest/functest-loop.sh
+++ jjb/functest/functest-loop.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set +e
 
-branch=${GIT_BRANCH##*/}
-[[ "$PUSH_RESULTS_TO_DB" == "true" ]] && flags+="-r"
-if [[ "$BRANCH" =~ 'brahmaputra' ]]; then
-    cmd="${FUNCTEST_REPO_DIR}/docker/run_tests.sh -s ${flags}"
-elif [[ "$BRANCH" =~ 'colorado' ]]; then
-    cmd="python ${FUNCTEST_REPO_DIR}/ci/run_tests.py -t all ${flags}"
-else
-    cmd="python ${FUNCTEST_REPO_DIR}/functest/ci/run_tests.py -t all ${flags}"
-fi
+
+cmd="python ${FUNCTEST_REPO_DIR}/functest/ci/run_tests.py -t all ${flags}"
+
 container_id=$(docker ps -a | grep opnfv/functest | awk '{print $1}' | head -1)
 docker exec $container_id $cmd
 
