9377677a9b06f01a6f4b3c4298d2424997b4070c
agardner <agardner@linuxfoundation.org>
2017-05-04 11:54:36 -0400

    fixed syntax errors in zte pods, change labconfig name
    Change-Id: I2d0e6c1199fe3ac2841923abdabd19c239b2b5f8
    Signed-off-by: agardner <agardner@linuxfoundation.org>

--- jjb/securedlab/check-jinja2.sh
+++ jjb/securedlab/check-jinja2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set +x
 set -o errexit
-for lab_configs in $(find labs/ -name 'pod.yaml' | grep -v zte); do
+for lab_configs in $(find labs/ -name 'pod.yaml'); do
         while IFS= read -r jinja_templates; do
           echo "./utils/generate_config.py -y $lab_configs -j $jinja_templates"
           ./utils/generate_config.py -y $lab_configs -j $jinja_templates
-        done < <(find installers/ -name '*.jinja2')
+        done < <(find installers/ -name 'labconfig.yaml')
 done
