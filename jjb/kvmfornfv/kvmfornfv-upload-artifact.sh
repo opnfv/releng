1c5a39426bba0bb1f029b89e963ff6dd8af88506
Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>
2017-05-15 11:38:42 -0700

    KVMFORNFV: Common build packages for daily test cases
    This patch is used to upload the metadata file only for the artifacts
    built by daily build job. Also used to add seperate checks for the
    artifacts uploaded by build and test phases.
    
    Change-Id: I4c760e2269f3251fd0de91a803123167fb2ad225
    Signed-off-by: Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>

--- jjb/kvmfornfv/kvmfornfv-upload-artifact.sh
+++ jjb/kvmfornfv/kvmfornfv-upload-artifact.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -o errexit
 set -o nounset
 
 if [[ "$JOB_NAME" =~ (verify|merge|daily|weekly) ]]; then
@@ -51,6 +50,13 @@ else
             #Uploading logs to artifacts
             echo "Uploading artifacts for future debugging needs...."
             gsutil cp -r $WORKSPACE/build_output/log-*.tar.gz $GS_LOG_LOCATION > $WORKSPACE/gsutil.log 2>&1
+            # verifying the logs uploaded by cyclictest daily test job
+            gsutil ls $GS_LOG_LOCATION > /dev/null 2>&1
+            if [[ $? -ne 0 ]]; then
+                echo "Problem while uploading logs to artifacts!"
+                echo "Check log $WORKSPACE/gsutil.log on $NODE_NAME"
+                exit 1
+            fi
         else
             echo "No test logs/artifacts available for uploading"
         fi
@@ -67,10 +73,13 @@ if [[ "$JOB_TYPE" == "daily" && "$PHASE" == "build" ]]; then
         gs://$GS_URL/latest.properties > /dev/null 2>&1
 fi
 
-gsutil ls $GS_UPLOAD_LOCATION > /dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    echo "Problem while uploading artifacts!"
-    echo "Check log $WORKSPACE/gsutil.log on $NODE_NAME"
-    exit 1
+# verifying the artifacts uploading by verify/daily build job
+if [[ "$PHASE" == "build" ]]; then
+    gsutil ls $GS_UPLOAD_LOCATION > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "Problem while uploading artifacts!"
+        echo "Check log $WORKSPACE/gsutil.log on $NODE_NAME"
+        exit 1
+    fi
 fi
 echo "Uploaded artifacts!"
