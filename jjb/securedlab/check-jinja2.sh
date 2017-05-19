4eb3f92c41b9d586ea388ff49197c3b536fb9465
agardner <agardner@linuxfoundation.org>
2017-05-19 14:29:22 -0400

    pod configs are not going to live in directories
    They will be called pod[#].yaml
    so we look for that instead
    
    Change-Id: If36d347cf7bef3108a2d50d74a84de1a07b5bdd2
    Signed-off-by: agardner <agardner@linuxfoundation.org>

--- jjb/securedlab/check-jinja2.sh
+++ jjb/securedlab/check-jinja2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +x
 set -o errexit
-for lab_configs in $(find labs/ -name 'pod.yaml'); do
+for lab_configs in $(find labs/ -name 'pod*.yaml'); do
         while IFS= read -r jinja_templates; do
           echo "./utils/generate_config.py -y $lab_configs -j $jinja_templates"
           ./utils/generate_config.py -y $lab_configs -j $jinja_templates
