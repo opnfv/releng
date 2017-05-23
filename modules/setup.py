439d45336be7291c29d427fab6a2f6ce44a4ce98
Cédric Ollivier <cedric.ollivier@orange.com>
2017-05-23 09:31:19 +0200

    Fix name in modules/setup.py
    It fixes duplicated names between setup.py and modules/setup.py.
    
    We could also have considered listing releng utils as scripts in
    modules/setup.py which is mandatory if another OPNFV project needs
    them (eg Functest before [1] was merged).
    They could be required in case of functional or unit testing too.
    
    [1] https://gerrit.opnfv.org/gerrit/#/c/35199/
    
    Change-Id: Ic77f4e46d6d3396f9891b73105a0eb7aa037754f
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- modules/setup.py
+++ modules/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="opnfv",
+    name="opnfv_modules",
     version="danube",
     packages=find_packages(),
     include_package_data=True,
