a16f810777b5c65296ff69b3558c122e86027356
Aric Gardner <agardner@linuxfoundation.org>
2017-06-14 12:03:12 +0800

    Add weekly job for security scan
    Added license headers
    remove errexit from report to gerrit
    run as --user nobody
    
    Change-Id: I4b65dbae1f255015877766a0afa44e9b9898651c
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/ci_gate_security/anteater-security-audit-weekly.sh
+++ jjb/ci_gate_security/anteater-security-audit-weekly.sh
@@ -1,54 +1,33 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 
-#WORKSPACE=/tmp/test/
-
-cd $WORKSPACE
-mkdir $WORKSPACE/allrepos
-cd $WORKSPACE/allrepos
-
-#Clone all repos for scan
-for x in $(ssh gerrit.opnfv.org -p 29418 'gerrit ls-projects');
-do git clone ssh://gerrit.opnfv.org:29418/$x
-done
-
-
-#Set docker mounts
 echo "--------------------------------------------------------"
 vols="-v $WORKSPACE/allrepos/:/home/opnfv/anteater/allrepos/"
 echo "Pulling releng-anteater docker image"
 echo "--------------------------------------------------------"
 docker pull opnfv/releng-anteater
 echo "--------------------------------------------------------"
-
-#Starts a shell inside docker
-cmd="docker run --privileged=true -id $vols opnfv/releng-anteater /bin/bash"
+cmd="docker run --user nobody -id $vols opnfv/releng-anteater /bin/bash"
 echo "Running docker command $cmd"
 container_id=$($cmd)
 echo "Container ID is $container_id"
+source $WORKSPACE/opnfv-projects.sh
+for project in "${PROJECT_LIST[@]}"
 
-
-for project in $(ssh gerrit.opnfv.org -p 29418 'gerrit ls-projects');
 do
-        cmd="anteater --project testproj --path /home/opnfv/anteater/allrepos/$project"
-        echo "Executing command inside container"
-        echo "$cmd"
-        echo "--------------------------------------------------------"
-        docker exec $container_id $cmd
+  cmd="anteater --project testproj --path /home/opnfv/anteater/allrepos/$project"
+  echo "Executing command inside container"
+  echo "$cmd"
+  echo "--------------------------------------------------------"
+  docker exec $container_id $cmd > $WORKSPACE/"$project".securityaudit.log 2>&1
 done
 
-
 exit_code=$?
-
-
 echo "--------------------------------------------------------"
 echo "Stopping docker container with ID $container_id"
 docker stop $container_id
 
-#TODO get reports
-# /home/opnfv/reports/*-$project.log
-# copy to gce (ls on host at least)
-#
+
 #gsutil cp $WORKSPACE/securityaudit.log \
 #    gs://$GS_URL/$PROJECT-securityaudit-weekly.log 2>&1
 #
