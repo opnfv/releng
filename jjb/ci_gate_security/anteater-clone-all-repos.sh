b35837bf42b4fae32351e2f1acfba2123ff32d26
Aric Gardner <agardner@linuxfoundation.org>
2017-06-14 12:03:12 +0800

    Add weekly job for security scan
    How do we iterate over a list of projects with this?
    
    just releng is set with now, and its not weekly so we can test
    
    Change-Id: I4b65dbae1f255015877766a0afa44e9b9898651c
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/ci_gate_security/anteater-clone-all-repos.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+export PATH=$PATH:/usr/local/bin/
+
+
+#WORKSPACE="$(pwd)"
+
+cd $WORKSPACE
+if [ ! -d "$WORKSPACE/allrepos" ]; then
+  mkdir $WORKSPACE/allrepos
+fi
+
+cd $WORKSPACE/allrepos
+
+declare -a PROJECT_LIST
+EXCLUDE_PROJECTS="All-Projects|All-Users|securedlab"
+
+PROJECT_LIST=($(ssh gerrit.opnfv.org -p 29418 gerrit ls-projects | egrep -v $EXCLUDE_PROJECTS))
+echo "PROJECT_LIST=(${PROJECT_LIST[*]})" > $WORKSPACE/opnfv-projects.sh
+
+for PROJECT in ${PROJECT_LIST[@]}; do
+  echo "> Cloning $PROJECT"
+#  if [ ! -d "$PROJECT" ]; then
+#    git clone "https://gerrit.opnfv.org/gerrit/$PROJECT.git"
+#  else
+#    pushd "$PROJECT" > /dev/null
+#    git pull -f
+#    popd > /dev/null
+#  fi
+done
