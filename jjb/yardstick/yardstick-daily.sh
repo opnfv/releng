eec4effc7f71c89549cbf90d9c140ce0b473555d
JingLu5 <lvjing5@huawei.com>
2017-06-26 02:25:23 +0000

    [yardstick] Pass "BRANCH" variable to yardstick docekr container
    Recently update of utils/fetch_os_creds.sh in commit 8359219bae93a028f6205cdc9460654f9e45c217
    use a "BRANCH" varibale in fuel environmet
    Currently Yardstick didn't pass such variable to yardstick docker, which causes the following error:
    /home/opnfv/repos/releng/utils/fetch_os_creds.sh: line 75: BRANCH: unbound variable
    
    This patch fix this error by Pass "BRANCH" variable to yardstick docekr container
    
    Change-Id: Ifcf3df7107b2f4b493b37c7ef5473f170a8edb94
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- jjb/yardstick/yardstick-daily.sh
+++ jjb/yardstick/yardstick-daily.sh
@@ -31,7 +31,8 @@ fi
 opts="--privileged=true --rm"
 envs="-e INSTALLER_TYPE=${INSTALLER_TYPE} -e INSTALLER_IP=${INSTALLER_IP} \
     -e NODE_NAME=${NODE_NAME} -e EXTERNAL_NETWORK=${EXTERNAL_NETWORK} \
-    -e YARDSTICK_BRANCH=${BRANCH} -e DEPLOY_SCENARIO=${DEPLOY_SCENARIO}"
+    -e YARDSTICK_BRANCH=${BRANCH} -e BRANCH=${BRANCH} \
+    -e DEPLOY_SCENARIO=${DEPLOY_SCENARIO}"
 
 # Pull the image with correct tag
 echo "Yardstick: Pulling image opnfv/yardstick:${DOCKER_TAG}"
