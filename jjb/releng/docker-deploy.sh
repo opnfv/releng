6595e7baa6265aa326dc58e9e961bd7c6f6628df
rohitsakala <rohitsakala@gmail.com>
2017-06-29 22:08:05 +0530

    Add jenkins jobs for reporting dashbaord deployment
    Jenkins Jobs have been added for automating the deployment of the reporting
    dashboard. Also for automating the update of docker in the docker hub.
    Modified unit tests of reporting to make it run from anywhere.
    
    Change-Id: Id13a55416e17199dae1dd6ce64f7c167d117ba37
    Signed-off-by: Sakala Venkata Krishna Rohit <rohitsakala@gmail.com>

--- jjb/releng/docker-deploy.sh
+++ jjb/releng/docker-deploy.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+#  Licensed to the Apache Software Foundation (ASF) under one   *
+#  or more contributor license agreements.  See the NOTICE file *
+#  distributed with this work for additional information        *
+#  regarding copyright ownership.  The ASF licenses this file   *
+#  to you under the Apache License, Version 2.0 (the            *
+#  "License"); you may not use this file except in compliance   *
+#  with the License.  You may obtain a copy of the License at   *
+#                                                               *
+#    http://www.apache.org/licenses/LICENSE-2.0                 *
+#                                                               *
+#  Unless required by applicable law or agreed to in writing,   *
+#  software distributed under the License is distributed on an  *
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+#  KIND, either express or implied.  See the License for the    *
+#  specific language governing permissions and limitations      *
+#  under the License.                                           *
+
+# Assigning Variables
+command=$1
+url=$2
 
 function check() {
 
     # Verify hosted
     sleep 5
-    cmd=`curl -s --head  --request GET http://testresults.opnfv.org/test/swagger/APIs | grep '200 OK' > /dev/null`
+    cmd=`curl -s --head  --request GET ${url} | grep '200 OK' > /dev/null`
     rc=$?
     echo $rc
 
@@ -63,7 +83,7 @@ else
 fi
 
 echo "Running a container with the new image"
-sudo docker run -dti -p "8082:8000" -e "mongodb_url=mongodb://172.17.0.1:27017" -e "swagger_url=http://testresults.opnfv.org/test" opnfv/testapi:latest
+$command:latest
 
 if check; then
     echo "TestResults Hosted."
@@ -71,7 +91,7 @@ else
     echo "TestResults Hosting Failed"
     if [[ $(sudo docker images | grep "opnfv/testapi" | grep "old" | awk '{print $3}') ]]; then
         echo "Running old Image"
-        sudo docker run -dti -p "8082:8000" -e "mongodb_url=mongodb://172.17.0.1:27017" -e "swagger_url=http://testresults.opnfv.org/test" opnfv/testapi:old
+        $command:old
         exit 1
     fi
 fi
