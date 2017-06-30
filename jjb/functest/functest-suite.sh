7269814b8b79c24c121c6a3e087284c52307a15b
Cédric Ollivier <cedric.ollivier@orange.com>
2017-06-30 22:03:09 +0200

    Directly call Functest console scripts
    It conforms jjobs with "Define console_scripts" [1].
    
    [1] https://gerrit.opnfv.org/gerrit/#/c/36781/
    
    Change-Id: I9209e6efa1b493e24135402a46df72aaa14115d1
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- jjb/functest/functest-suite.sh
+++ jjb/functest/functest-suite.sh
@@ -10,7 +10,7 @@ global_ret_val=0
 
 tests=($(echo $FUNCTEST_SUITE_NAME | tr "," "\n"))
 for test in ${tests[@]}; do
-    cmd="python /home/opnfv/repos/functest/functest/ci/run_tests.py -t $test"
+    cmd="run_tests -t $test"
     docker exec $container_id $cmd
     let global_ret_val+=$?
 done
