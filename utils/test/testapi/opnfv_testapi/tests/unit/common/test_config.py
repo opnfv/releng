6c2c7e39dc7a87117b3630fe5c1bb8406243a0a2
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-07-13 20:09:10 +0800

    let Config() itself to manage config_file
    let Config to check which resources shoule be used in the order:
    1) --config-file <file> transfer in by cli
    2) .venv/etc/opnfv_testapi/config.ini (under venv)
    3) /etc/opnfv_testapi/config.ini(normal usage, in container for example)
    
    Change-Id: I46887d122a98d478caebe9eeb7ab038941ce1f6b
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/testapi/opnfv_testapi/tests/unit/common/test_config.py
+++ utils/test/testapi/opnfv_testapi/tests/unit/common/test_config.py
@@ -1,16 +1,29 @@
-import os
-
-from opnfv_testapi.common import config
+import argparse
 
 
-def test_config_success():
-    config_file = os.path.join(os.path.dirname(__file__),
-                               '../../../../etc/config.ini')
-    config.Config.CONFIG = config_file
-    conf = config.Config()
-    assert conf.mongo_url == 'mongodb://127.0.0.1:27017/'
-    assert conf.mongo_dbname == 'test_results_collection'
-    assert conf.api_port == 8000
-    assert conf.api_debug is True
-    assert conf.api_authenticate is False
-    assert conf.swagger_base_url == 'http://localhost:8000'
+def test_config_normal(mocker, config_normal):
+    mocker.patch(
+        'argparse.ArgumentParser.parse_known_args',
+        return_value=(argparse.Namespace(config_file=config_normal), None))
+    from opnfv_testapi.common import config
+    CONF = config.Config()
+    assert CONF.mongo_url == 'mongodb://127.0.0.1:27017/'
+    assert CONF.mongo_dbname == 'test_results_collection'
+    assert CONF.api_port == 8000
+    assert CONF.api_debug is True
+    assert CONF.api_authenticate is False
+    assert CONF.swagger_base_url == 'http://localhost:8000'
+
+
+def test_config_token(mocker, config_token):
+    mocker.patch(
+        'argparse.ArgumentParser.parse_known_args',
+        return_value=(argparse.Namespace(config_file=config_token), None))
+    from opnfv_testapi.common import config
+    CONF = config.Config()
+    assert CONF.mongo_url == 'mongodb://127.0.0.1:27017/'
+    assert CONF.mongo_dbname == 'test_results_collection'
+    assert CONF.api_port == 8000
+    assert CONF.api_debug is True
+    assert CONF.api_authenticate is True
+    assert CONF.swagger_base_url == 'http://localhost:8000'
