67d8bac7b2a97531ad465f275747b3dc455d4fe4
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-07-17 21:35:20 +0200

    armband-deploy.sh: Inherit env vars for sudo
    Previous commit adding sudo for ci/deploy.sh call breaks Armband
    deploys, since ci/deploy.sh relies on $WORKSPACE env var being set.
    
    Change-Id: I948c8e3c00b430e0c186c0a727e1b7e027d7a089
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- jjb/armband/armband-deploy.sh
+++ jjb/armband/armband-deploy.sh
@@ -93,7 +93,7 @@ else
 fi
 
 # construct the command
-DEPLOY_COMMAND="sudo $WORKSPACE/ci/deploy.sh -b ${LAB_CONFIG_URL} \
+DEPLOY_COMMAND="sudo -E $WORKSPACE/ci/deploy.sh -b ${LAB_CONFIG_URL} \
     -l $LAB_NAME -p $POD_NAME -s $DEPLOY_SCENARIO -i file://${ISO_FILE} \
     -H -B ${DEFAULT_BRIDGE:-pxebr} -S $TMPDIR -L $WORKSPACE/$FUEL_LOG_FILENAME \
     ${DEPLOY_CACHE}"
