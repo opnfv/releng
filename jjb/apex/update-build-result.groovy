2b13f8ee486f4f8ae1c66ac3b95148d9a423318b
Tim Rozet <trozet@redhat.com>
2017-08-02 14:55:51 -0400

    Apex: fix groovy build result update
    The groovy script is working, but it is changing the 'apex test suite'
    phase to unstable and we really want it to change the 'scenario' job
    result to unstable if deploy passed but tests failed.  Therefore this
    patch moves the groovy plugin to execute on the scenario job, and looks
    for deploy successful before changing the build result.
    
    Change-Id: I1b6907f4fb2647e644c4a627be6e481fff32f195
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/apex/update-build-result.groovy
+++ jjb/apex/update-build-result.groovy
@@ -1,4 +1,5 @@
 import hudson.model.*
-if (manager.build.@result == hudson.model.Result.FAILURE) {
+if (manager.logContains("^.*apex-deploy-baremetal.*SUCCESS$")
+      && manager.build.@result == hudson.model.Result.FAILURE) {
     manager.build.@result = hudson.model.Result.UNSTABLE
 }
