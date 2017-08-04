af3d0f65484c7ad99f716dae0adcf9b2044bee05
Tim Rozet <trozet@redhat.com>
2017-08-03 21:39:22 -0400

    Apex: fixes groovy script
    The script had double quotes which will try to interpret dollar sign as
    a templated variable.  Switching to literal string.
    
    Change-Id: Ic8d09dc6c862b9db95bf9d1277f562e5bce02fc9
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/apex/update-build-result.groovy
+++ jjb/apex/update-build-result.groovy
@@ -1,5 +1,5 @@
 import hudson.model.*
-if (manager.logContains("^.*apex-deploy-baremetal.*SUCCESS$")
+if (manager.logContains('^.*apex-deploy-baremetal.*SUCCESS$')
       && manager.build.@result == hudson.model.Result.FAILURE) {
     manager.build.@result = hudson.model.Result.UNSTABLE
 }
