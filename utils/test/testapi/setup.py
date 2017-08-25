75f8fd597e060b1775192d581c04e8071cf56293
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-08-25 17:56:55 +0800

    leverage setup.py directly to install TestAPI
    1. delete additional script install.sh
    2. using 'python setup.py install' directly to install
    TestAPI, as traditional python project do
    
    Change-Id: Ifcdee7b2bae2553f523a722bff794c40d20f81cc
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/testapi/setup.py
+++ utils/test/testapi/setup.py
@@ -1,5 +1,7 @@
-import setuptools
+import os
+import subprocess
 
+import setuptools
 
 __author__ = 'serena'
 
@@ -7,6 +9,11 @@ try:
     import multiprocessing  # noqa
 except ImportError:
     pass
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+subprocess.call(['ln', '-s',
+                 '{}/3rd_party/static'.format(dirpath),
+                 '{}/opnfv_testapi/static'.format(dirpath)])
 
 setuptools.setup(
     setup_requires=['pbr==2.0.0'],
