cc53c861eb550b7fb882212f4abd6c0c63ba0f71
Morgan Richomme <morgan.richomme@orange.com>
2017-08-28 15:13:35 +0200

    Add functest-healthcheck alpine to xci-verify-healthcheck-macro
     - add the rc file retrieval in fetch_os.sh
     - adapt alpine to split healtcheck from daily
     (healthcheck + smoke + features)
     - adapt the xci builder
    
    Change-Id: I38836814240a1d769d379a10c9ef14def7e6df97
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- jjb/functest/functest-alpine.sh
+++ jjb/functest/functest-alpine.sh
@@ -68,9 +68,14 @@ volumes="${images_vol} ${results_vol} ${sshkey_vol} ${rc_file_vol} ${cacert_file
 
 set +e
 
-tiers=(healthcheck smoke features vnf)
+if ${FUNCTEST_SUITE_NAME} == 'healthcheck'; then
+    tiers=(healthcheck)
+else
+    tiers=(healthcheck smoke features vnf)
+fi
+
 for tier in ${tiers[@]}; do
-    FUNCTEST_IMAGE=ollivier/functest-${tier}
+    FUNCTEST_IMAGE=opnfv/functest-${tier}
     echo "Functest: Pulling Functest Docker image ${FUNCTEST_IMAGE} ..."
     docker pull ${FUNCTEST_IMAGE}>/dev/null
     cmd="docker run ${envs} ${volumes} ${FUNCTEST_IMAGE}"
