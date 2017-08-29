4cdef4b02e3e201dda3a90042e042e714ea71d8f
Manuel Buil <mbuil@suse.com>
2017-08-29 16:56:02 +0200

    [WIP] Add XCI adapter
    Add XCI adapter so that we could benefit from it in functest integration
    
    Change-Id: I010a6f2b6c75711d0cc80f8fd130604c41b6d1e6
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- modules/opnfv/deployment/factory.py
+++ modules/opnfv/deployment/factory.py
@@ -11,6 +11,7 @@
 from opnfv.deployment.apex import adapter as apex_adapter
 from opnfv.deployment.compass import adapter as compass_adapter
 from opnfv.deployment.fuel import adapter as fuel_adapter
+from opnfv.deployment.XCI import adapter as xci_adapter
 from opnfv.utils import opnfv_logger as logger
 
 logger = logger.Logger(__name__).getLogger()
@@ -46,6 +47,9 @@ class Factory(object):
                 installer_ip=installer_ip,
                 installer_user=installer_user,
                 installer_pwd=installer_pwd)
+        elif installer.lower() == "xci":    
+            return xci_adapter.XCIAdapter(installer_ip=installer_ip,
+                                          installer_user=installer_user)
         else:
             raise Exception("Installer adapter is not implemented for "
                             "the given installer.")
