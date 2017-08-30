b7da3d3eba7e2b813d208bab0f93c5e102fde382
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-08-30 11:59:46 +0800

    leverage LFID as Authentication
    delete openid authentication
    add LFID authentication
    
    Change-Id: Iead144b5130bce51448024e65092fdea3bb2f07a
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ utils/test/testapi/opnfv_testapi/common/constants.py
@@ -0,0 +1,4 @@
+TESTAPI_ID = 'testapi_id'
+CSRF_TOKEN = 'csrf_token'
+ROLE = 'role'
+TESTAPI_USERS = ['opnfv-testapi-users']
--- utils/test/testapi/opnfv_testapi/ui/root.py
+++ utils/test/testapi/opnfv_testapi/ui/root.py
@@ -1,10 +1,12 @@
-from opnfv_testapi.resources.handlers import GenericApiHandler
+from opnfv_testapi.common import check
 from opnfv_testapi.common.config import CONF
+from opnfv_testapi.resources import handlers
 
 
-class RootHandler(GenericApiHandler):
+class RootHandler(handlers.GenericApiHandler):
     def get_template_path(self):
         return CONF.static_path
 
+    @check.login
     def get(self):
         self.render('testapi-ui/index.html')
