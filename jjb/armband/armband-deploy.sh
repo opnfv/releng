900feabe79816b597ec9d865d39f803641f15212
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-08-31 22:20:50 +0200

    fuel, armband: Sync deploy scripts, cleanup
    Run shellcheck on touched files, fix minor warnings.
    Prepare for upcoming changes:
    - ISO artifact will be obsoleted (later);
    - Fuel and Armband should use the same deploy script;
    
    Fuel:
    - rename securedlab local copy to lab-config;
    
    Armband:
    - drop deploy cache, Danube jobs can work without it, master/next
      jobs no longer use it;
    - move TMPDIR to $HOME (was in $WORKSPACE), so it's persistent;
    
    Change-Id: I9f8c47f9ca50de3c53c62ca6a08df0cc3a73abd3
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- jjb/armband/armband-deploy.sh
+++ jjb/armband/armband-deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2016 Ericsson AB and others.
-#           (c) 2017 Enea Software AB
+# Copyright (c) 2017 Ericsson AB, Mirantis Inc., Enea Software AB and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
@@ -21,90 +20,84 @@ if [[ "$BRANCH" != 'master' ]]; then
     echo "Using ${OPNFV_ARTIFACT_URL/*\/} for deployment"
 fi
 
-if [[ "$JOB_NAME" =~ "merge" ]]; then
+# shellcheck disable=SC2153
+if [[ "${JOB_NAME}" =~ "merge" ]]; then
     # set simplest scenario for virtual deploys to run for merges
     DEPLOY_SCENARIO="os-nosdn-nofeature-ha"
-elif [[ "$BRANCH" != 'master' ]]; then
+elif [[ "${BRANCH}" != 'master' ]]; then
     # for none-merge deployments
     # checkout the commit that was used for building the downloaded artifact
     # to make sure the ISO and deployment mechanism uses same versions
-    echo "Checking out $OPNFV_GIT_SHA1"
-    git checkout $OPNFV_GIT_SHA1 --quiet
+    echo "Checking out ${OPNFV_GIT_SHA1}"
+    git checkout "${OPNFV_GIT_SHA1}" --quiet
 fi
 
 # set deployment parameters
-export TMPDIR=${WORKSPACE}/tmpdir
-
+export TMPDIR=${HOME}/tmpdir
+BRIDGE=${BRIDGE:-pxebr}
+# shellcheck disable=SC2153
 LAB_NAME=${NODE_NAME/-*}
+# shellcheck disable=SC2153
 POD_NAME=${NODE_NAME/*-}
+# Armband does not require deploy script to be ran with sudo
+SUDO=
 
-# we currently support enea
-if [[ ! $LAB_NAME =~ (arm|enea) ]]; then
-    echo "Unsupported/unidentified lab $LAB_NAME. Cannot continue!"
+# we currently support arm, enea labs
+if [[ ! "${LAB_NAME}" =~ (arm|enea) ]]; then
+    echo "Unsupported/unidentified lab ${LAB_NAME}. Cannot continue!"
     exit 1
 fi
 
-echo "Using configuration for $LAB_NAME"
+echo "Using configuration for ${LAB_NAME}"
 
-# create TMPDIR if it doesn't exist
-mkdir -p $TMPDIR
+# create TMPDIR if it doesn't exist, change permissions
+mkdir -p "${TMPDIR}"
+chmod a+x "${HOME}" "${TMPDIR}"
 
-cd $WORKSPACE
-if [[ $LAB_CONFIG_URL =~ ^(git|ssh):// ]]; then
-    echo "Cloning securedlab repo $BRANCH"
-    git clone --quiet --branch $BRANCH $LAB_CONFIG_URL lab-config
+cd "${WORKSPACE}" || exit 1
+if [[ "${LAB_CONFIG_URL}" =~ ^(git|ssh):// ]]; then
+    echo "Cloning securedlab repo ${BRANCH}"
+    git clone --quiet --branch "${BRANCH}" "${LAB_CONFIG_URL}" lab-config
     LAB_CONFIG_URL=file://${WORKSPACE}/lab-config
 
     # Source local_env if present, which contains POD-specific config
-    local_env="${WORKSPACE}/lab-config/labs/$LAB_NAME/$POD_NAME/fuel/config/local_env"
-    if [ -e $local_env ]; then
+    local_env="${WORKSPACE}/lab-config/labs/${LAB_NAME}/${POD_NAME}/fuel/config/local_env"
+    if [ -e "${local_env}" ]; then
         echo "-- Sourcing local environment file"
-        source $local_env
+        source "${local_env}"
     fi
 fi
 
 # releng wants us to use nothing else but opnfv.iso for now. We comply.
-ISO_FILE=$WORKSPACE/opnfv.iso
+ISO_FILE=file://${WORKSPACE}/opnfv.iso
 
 # log file name
 FUEL_LOG_FILENAME="${JOB_NAME}_${BUILD_NUMBER}.log.tar.gz"
 
-# Deploy Cache (to enable just create the deploy-cache subdir)
-# NOTE: Only available when ISO files are cached using ISOSTORE mechanism
-DEPLOY_CACHE=${ISOSTORE:-/iso_mount/opnfv_ci}/${BRANCH##*/}/deploy-cache
-if [[ -d "${DEPLOY_CACHE}" ]]; then
-    echo "Deploy cache dir present."
-    echo "--------------------------------------------------------"
-    echo "Fuel@OPNFV deploy cache: ${DEPLOY_CACHE}"
-    DEPLOY_CACHE="-C ${DEPLOY_CACHE}"
-else
-    DEPLOY_CACHE=""
-fi
-
 # construct the command
-DEPLOY_COMMAND="$WORKSPACE/ci/deploy.sh -b ${LAB_CONFIG_URL} \
-    -l $LAB_NAME -p $POD_NAME -s $DEPLOY_SCENARIO -i file://${ISO_FILE} \
-    -B ${DEFAULT_BRIDGE:-pxebr} -S $TMPDIR -L $WORKSPACE/$FUEL_LOG_FILENAME \
-    ${DEPLOY_CACHE}"
+DEPLOY_COMMAND="${SUDO} ${WORKSPACE}/ci/deploy.sh -b ${LAB_CONFIG_URL} \
+    -l ${LAB_NAME} -p ${POD_NAME} -s ${DEPLOY_SCENARIO} -i ${ISO_FILE} \
+    -B ${DEFAULT_BRIDGE:-${BRIDGE}} -S ${TMPDIR} \
+    -L ${WORKSPACE}/${FUEL_LOG_FILENAME}"
 
 # log info to console
 echo "Deployment parameters"
 echo "--------------------------------------------------------"
-echo "Scenario: $DEPLOY_SCENARIO"
-echo "Lab: $LAB_NAME"
-echo "POD: $POD_NAME"
-[[ "$BRANCH" != 'master' ]] && echo "ISO: ${OPNFV_ARTIFACT_URL/*\/}"
+echo "Scenario: ${DEPLOY_SCENARIO}"
+echo "Lab: ${LAB_NAME}"
+echo "POD: ${POD_NAME}"
+[[ "${BRANCH}" != 'master' ]] && echo "ISO: ${OPNFV_ARTIFACT_URL/*\/}"
 echo
-echo "Starting the deployment using $INSTALLER_TYPE. This could take some time..."
+echo "Starting the deployment using ${INSTALLER_TYPE}. This could take some time..."
 echo "--------------------------------------------------------"
 echo
 
 # start the deployment
 echo "Issuing command"
-echo "$DEPLOY_COMMAND"
+echo "${DEPLOY_COMMAND}"
 echo
 
-$DEPLOY_COMMAND
+${DEPLOY_COMMAND}
 exit_code=$?
 
 echo
@@ -112,16 +105,18 @@ echo "--------------------------------------------------------"
 echo "Deployment is done!"
 
 # upload logs for baremetal deployments
-# work with virtual deployments is still going on so we skip that for the timebeing
-if [[ "$JOB_NAME" =~ "baremetal-daily" ]]; then
+# work with virtual deployments is still going on, so skip that for now
+if [[ "${JOB_NAME}" =~ (baremetal-daily|baremetal-weekly) ]]; then
     echo "Uploading deployment logs"
-    gsutil cp $WORKSPACE/$FUEL_LOG_FILENAME gs://$GS_URL/logs/$FUEL_LOG_FILENAME > /dev/null 2>&1
-    echo "Logs are available as http://$GS_URL/logs/$FUEL_LOG_FILENAME"
+    gsutil cp "${WORKSPACE}/${FUEL_LOG_FILENAME}" \
+        "gs://${GS_URL}/logs/${FUEL_LOG_FILENAME}" > /dev/null 2>&1
+    echo "Logs are available at http://${GS_URL}/logs/${FUEL_LOG_FILENAME}"
 fi
 
-if [[ $exit_code -ne 0 ]]; then
+if [[ "${exit_code}" -ne 0 ]]; then
     echo "Deployment failed!"
-    exit $exit_code
-else
-    echo "Deployment is successful!"
+    exit "${exit_code}"
 fi
+
+echo "Deployment is successful!"
+exit 0
--- jjb/fuel/fuel-deploy.sh
+++ jjb/fuel/fuel-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2016 Ericsson AB and others.
+# Copyright (c) 2017 Ericsson AB, Mirantis Inc., Enea Software AB and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
@@ -20,82 +20,91 @@ if [[ "$BRANCH" != 'master' ]]; then
     echo "Using ${OPNFV_ARTIFACT_URL/*\/} for deployment"
 fi
 
-if [[ "$JOB_NAME" =~ "merge" ]]; then
+# shellcheck disable=SC2153
+if [[ "${JOB_NAME}" =~ "merge" ]]; then
     # set simplest scenario for virtual deploys to run for merges
     DEPLOY_SCENARIO="os-nosdn-nofeature-ha"
-elif [[ "$BRANCH" != 'master' ]]; then
+elif [[ "${BRANCH}" != 'master' ]]; then
     # for none-merge deployments
     # checkout the commit that was used for building the downloaded artifact
     # to make sure the ISO and deployment mechanism uses same versions
-    echo "Checking out $OPNFV_GIT_SHA1"
-    git checkout $OPNFV_GIT_SHA1 --quiet
+    echo "Checking out ${OPNFV_GIT_SHA1}"
+    git checkout "${OPNFV_GIT_SHA1}" --quiet
 fi
 
 # set deployment parameters
-export TMPDIR=$HOME/tmpdir
+export TMPDIR=${HOME}/tmpdir
 BRIDGE=${BRIDGE:-pxebr}
+# shellcheck disable=SC2153
 LAB_NAME=${NODE_NAME/-*}
+# shellcheck disable=SC2153
 POD_NAME=${NODE_NAME/*-}
+# Fuel requires deploy script to be ran with sudo
+SUDO=sudo
 
-if [[ "$NODE_NAME" =~ "virtual" ]]; then
+if [[ "${NODE_NAME}" =~ "virtual" ]]; then
     POD_NAME="virtual_kvm"
 fi
 
 # we currently support ericsson, intel, lf and zte labs
-if [[ ! "$LAB_NAME" =~ (ericsson|intel|lf|zte) ]]; then
-    echo "Unsupported/unidentified lab $LAB_NAME. Cannot continue!"
+if [[ ! "${LAB_NAME}" =~ (ericsson|intel|lf|zte) ]]; then
+    echo "Unsupported/unidentified lab ${LAB_NAME}. Cannot continue!"
     exit 1
-else
-    echo "Using configuration for $LAB_NAME"
 fi
 
-# create TMPDIR if it doesn't exist
-export TMPDIR=$HOME/tmpdir
-mkdir -p $TMPDIR
+echo "Using configuration for ${LAB_NAME}"
 
-# change permissions down to TMPDIR
-chmod a+x $HOME
-chmod a+x $TMPDIR
+# create TMPDIR if it doesn't exist, change permissions
+mkdir -p "${TMPDIR}"
+chmod a+x "${HOME}" "${TMPDIR}"
 
-# clone the securedlab repo
-cd $WORKSPACE
-echo "Cloning securedlab repo $BRANCH"
-git clone ssh://jenkins-ericsson@gerrit.opnfv.org:29418/securedlab --quiet \
-    --branch $BRANCH
+# TODO: move lab-config URL to Jenkins param
+LAB_CONFIG_URL='ssh://jenkins-ericsson@gerrit.opnfv.org:29418/securedlab'
 
-# Source local_env if present, which contains POD-specific config
-local_env="${WORKSPACE}/securedlab/labs/$LAB_NAME/$POD_NAME/fuel/config/local_env"
-if [ -e "${local_env}" ]; then
-    echo "-- Sourcing local environment file"
-    source "${local_env}"
+cd "${WORKSPACE}" || exit 1
+if [[ "${LAB_CONFIG_URL}" =~ ^(git|ssh):// ]]; then
+    echo "Cloning securedlab repo ${BRANCH}"
+    git clone --quiet --branch "${BRANCH}" "${LAB_CONFIG_URL}" lab-config
+    LAB_CONFIG_URL=file://${WORKSPACE}/lab-config
+
+    # Source local_env if present, which contains POD-specific config
+    local_env="${WORKSPACE}/lab-config/labs/${LAB_NAME}/${POD_NAME}/fuel/config/local_env"
+    if [ -e "${local_env}" ]; then
+        echo "-- Sourcing local environment file"
+        source "${local_env}"
+    fi
 fi
+
+# releng wants us to use nothing else but opnfv.iso for now. We comply.
+ISO_FILE=file://${WORKSPACE}/opnfv.iso
 
 # log file name
 FUEL_LOG_FILENAME="${JOB_NAME}_${BUILD_NUMBER}.log.tar.gz"
 
 # construct the command
-DEPLOY_COMMAND="sudo $WORKSPACE/ci/deploy.sh -b file://$WORKSPACE/securedlab \
-    -l $LAB_NAME -p $POD_NAME -s $DEPLOY_SCENARIO -i file://$WORKSPACE/opnfv.iso \
-    -B ${DEFAULT_BRIDGE:-${BRIDGE}} -S $TMPDIR -L $WORKSPACE/$FUEL_LOG_FILENAME"
+DEPLOY_COMMAND="${SUDO} ${WORKSPACE}/ci/deploy.sh -b ${LAB_CONFIG_URL} \
+    -l ${LAB_NAME} -p ${POD_NAME} -s ${DEPLOY_SCENARIO} -i ${ISO_FILE} \
+    -B ${DEFAULT_BRIDGE:-${BRIDGE}} -S ${TMPDIR} \
+    -L ${WORKSPACE}/${FUEL_LOG_FILENAME}"
 
 # log info to console
 echo "Deployment parameters"
 echo "--------------------------------------------------------"
-echo "Scenario: $DEPLOY_SCENARIO"
-echo "Lab: $LAB_NAME"
-echo "POD: $POD_NAME"
-[[ "$BRANCH" != 'master' ]] && echo "ISO: ${OPNFV_ARTIFACT_URL/*\/}"
+echo "Scenario: ${DEPLOY_SCENARIO}"
+echo "Lab: ${LAB_NAME}"
+echo "POD: ${POD_NAME}"
+[[ "${BRANCH}" != 'master' ]] && echo "ISO: ${OPNFV_ARTIFACT_URL/*\/}"
 echo
-echo "Starting the deployment using $INSTALLER_TYPE. This could take some time..."
+echo "Starting the deployment using ${INSTALLER_TYPE}. This could take some time..."
 echo "--------------------------------------------------------"
 echo
 
 # start the deployment
 echo "Issuing command"
-echo "$DEPLOY_COMMAND"
+echo "${DEPLOY_COMMAND}"
 echo
 
-$DEPLOY_COMMAND
+${DEPLOY_COMMAND}
 exit_code=$?
 
 echo
@@ -103,17 +112,18 @@ echo "--------------------------------------------------------"
 echo "Deployment is done!"
 
 # upload logs for baremetal deployments
-# work with virtual deployments is still going on so we skip that for the timebeing
-if [[ "$JOB_NAME" =~ (baremetal-daily|baremetal-weekly) ]]; then
+# work with virtual deployments is still going on, so skip that for now
+if [[ "${JOB_NAME}" =~ (baremetal-daily|baremetal-weekly) ]]; then
     echo "Uploading deployment logs"
-    gsutil cp $WORKSPACE/$FUEL_LOG_FILENAME gs://$GS_URL/logs/$FUEL_LOG_FILENAME > /dev/null 2>&1
-    echo "Logs are available as http://$GS_URL/logs/$FUEL_LOG_FILENAME"
+    gsutil cp "${WORKSPACE}/${FUEL_LOG_FILENAME}" \
+        "gs://${GS_URL}/logs/${FUEL_LOG_FILENAME}" > /dev/null 2>&1
+    echo "Logs are available at http://${GS_URL}/logs/${FUEL_LOG_FILENAME}"
 fi
 
-if [[ $exit_code -ne 0 ]]; then
+if [[ "${exit_code}" -ne 0 ]]; then
     echo "Deployment failed!"
-    exit $exit_code
-else
-    echo "Deployment is successful!"
-    exit 0
+    exit "${exit_code}"
 fi
+
+echo "Deployment is successful!"
+exit 0
