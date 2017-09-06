9a8fea348c6c37db65708fea0dc4458ae5d274d7
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-09-06 14:21:23 +0800

    hide cas ticket from web portal
    In the previous implementation, when login the url will shown as:
    http://localhost:8000/?ticket=ST-5WzYs6SD2A#/
    this patch aims to hide the ticket mechanism.
    
    1) add /api/v1/auth/signin_return to process login verify
    2) refactor code, leverage SignBaseHanlder() to manage casclient
    
    Change-Id: I62e23eb69ee52304c30753e861b4f0a4e0d45541
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/testapi/opnfv_testapi/ui/root.py
+++ utils/test/testapi/opnfv_testapi/ui/root.py
@@ -1,4 +1,3 @@
-from opnfv_testapi.common import check
 from opnfv_testapi.common.config import CONF
 from opnfv_testapi.resources import handlers
 
@@ -7,6 +6,5 @@ class RootHandler(handlers.GenericApiHandler):
     def get_template_path(self):
         return CONF.ui_static_path
 
-    @check.login
     def get(self):
         self.render('testapi-ui/index.html')
