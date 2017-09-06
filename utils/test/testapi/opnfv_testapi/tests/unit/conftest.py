6721a68595f52e39cdc56aefb41b7d24bd62d5a6
utkarshrai98 <utkarshrai98@gmail.com>
2017-09-06 21:25:31 +0530

    First Try; Testing for one raise in config.py Testing if I'm able to send code for review
    Change-Id: I211121f6e1c603abeb7be5f0b07858fe5e0b3f8b
    Signed-off-by: utkarshrai98 <utkarshrai98@gmail.com>

--- utils/test/testapi/opnfv_testapi/tests/unit/conftest.py
+++ utils/test/testapi/opnfv_testapi/tests/unit/conftest.py
@@ -1,8 +1,12 @@
 from os import path
-
+import random, string
 import pytest
 
 
 @pytest.fixture
 def config_normal():
     return path.join(path.dirname(__file__), '../../../etc/config.ini')
+
+@pytest.fixture
+def config_does_not_exist():
+    return path.join(path.dirname(__file__), '../../../etc/',''.join(random.choice(string.lowercase) for i in range(10)),'.ini')
