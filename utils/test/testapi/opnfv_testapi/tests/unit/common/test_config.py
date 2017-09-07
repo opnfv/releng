e9cb9d632dc6a2de5a6eee6661aa23ccbcad75c0
utkarshrai98 <utkarshrai98@gmail.com>
2017-09-07 12:58:41 +0530

    testing for raise in config.py
    Change-Id: I3b4f21307af55c8f32332502ddcb9315074b7202
    Signed-off-by: utkarshrai98 <utkarshrai98@gmail.com>
    
    Testing for the raise in test_config.py
    
    Change-Id: Iea7ae53b5eac51d3e3eaf9f23d6e8cd79919f103
    Signed-off-by: utkarshrai98 <utkarshrai98@gmail.com>

--- utils/test/testapi/opnfv_testapi/tests/unit/common/test_config.py
+++ utils/test/testapi/opnfv_testapi/tests/unit/common/test_config.py
@@ -1,4 +1,5 @@
 import argparse
+import mock
 
 
 def test_config_normal(mocker, config_normal):
@@ -13,3 +14,9 @@ def test_config_normal(mocker, config_normal):
     assert CONF.api_debug is True
     assert CONF.api_authenticate is False
     assert CONF.ui_url == 'http://localhost:8000'
+
+
+def test_config_file_does_not_exist(mocker, config_does_not_exist):
+    with mock.patch('os.path.exists'):
+        assert "config file not found"
+        return False
