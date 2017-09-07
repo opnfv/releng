71f169d869e5e9c4f336d15c0a7d36143d2613f2
utkarshrai98 <utkarshrai98@gmail.com>
2017-09-07 12:58:41 +0530

    testing for raise in config.py
    Change-Id: I3b4f21307af55c8f32332502ddcb9315074b7202
    Signed-off-by: utkarshrai98 <utkarshrai98@gmail.com>
    
    Testing for the raise in test_config.py
    
    Change-Id: Iea7ae53b5eac51d3e3eaf9f23d6e8cd79919f103
    Signed-off-by: utkarshrai98 <utkarshrai98@gmail.com>

--- utils/test/testapi/opnfv_testapi/tests/unit/conftest.py
+++ utils/test/testapi/opnfv_testapi/tests/unit/conftest.py
@@ -1,8 +1,12 @@
 from os import path
-
 import pytest
 
 
 @pytest.fixture
 def config_normal():
     return path.join(path.dirname(__file__), '../../../etc/config.ini')
+
+
+@pytest.fixture
+def config_does_not_exist():
+    return path.join(path.dirname(__file__), '../../../etc/notexist.ini')
