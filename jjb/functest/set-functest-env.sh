88adfdc08875d26dd6a36af51c02cb77f285a483
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-09-10 21:34:32 +0200

    fuel, armband: Remove SALT_MASTER_IP param
    Fuel@OPNFV mcpcontrol network (equiv to old admin/PXE net) has been
    configured to use 192.168.10.0/24 during E release cycle.
    However, due to overlapping with Ericsson-pod1 networks, we decided
    to re-use the old CIDR Fuel used before E release (10.20.0.0/24).
    
    This allows reusing the INSTALLER_IP param instead of recently added
    SALT_MASTER_IP param.
    
    Current change refactors all usage of SALT_MASTER_IP in favor of old
    INSTALLER_IP, thus reducing complexity and providing a familiar env
    to leagcy Fuel users.
    
    JIRA: FUEL-285
    
    Change-Id: I1e89cca08ce4cb9bf3a82a06043c91ee26aa0794
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- jjb/functest/set-functest-env.sh
+++ jjb/functest/set-functest-env.sh
@@ -51,14 +51,15 @@ envs="-e INSTALLER_TYPE=${INSTALLER_TYPE} -e INSTALLER_IP=${INSTALLER_IP} \
     -e NODE_NAME=${NODE_NAME} -e DEPLOY_SCENARIO=${DEPLOY_SCENARIO} \
     -e BUILD_TAG=${BUILD_TAG} -e CI_DEBUG=${CI_DEBUG} -e DEPLOY_TYPE=${DEPLOY_TYPE}"
 
-if [[ ${INSTALLER_TYPE} == 'fuel' && ! -z ${SALT_MASTER_IP} ]]; then
-  HOST_ARCH=$(ssh -l ubuntu ${SALT_MASTER_IP} -i ${SSH_KEY} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  "sudo salt 'cmp*' grains.get cpuarch --out yaml |awk '{print \$2; exit}'")
-  envs="${envs} -e POD_ARCH=${HOST_ARCH}"
+ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+if [ "${INSTALLER_TYPE}" == 'fuel' ] && [ "$BRANCH" != 'stable/danube' ]; then
+    COMPUTE_ARCH=$(ssh -l ubuntu ${INSTALLER_IP} -i ${SSH_KEY} ${ssh_options} \
+        "sudo salt 'cmp*' grains.get cpuarch --out yaml | awk '{print \$2; exit}'")
+    envs="${envs} -e POD_ARCH=${COMPUTE_ARCH}"
 fi
 
 if [[ ${INSTALLER_TYPE} == 'compass' && ${DEPLOY_SCENARIO} == *'os-nosdn-openo-ha'* ]]; then
-    ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
     openo_msb_port=${openo_msb_port:-80}
     openo_msb_endpoint="$(sshpass -p'root' ssh 2>/dev/null $ssh_options root@${installer_ip} \
     'mysql -ucompass -pcompass -Dcompass -e "select package_config from cluster;" \
