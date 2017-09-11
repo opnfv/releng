f1d1e9b9536337d4db8a28284df6d5a421f65eb2
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-09-11 12:37:22 +0800

    leverage LFID authentication to pod creation
    only valid linux foundation user is allowed to create the new pod
    
    Change-Id: Icada07152069f7c826bfa6122cb86db8c4e3bf68
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- utils/test/testapi/opnfv_testapi/resources/handlers.py
+++ utils/test/testapi/opnfv_testapi/resources/handlers.py
@@ -75,6 +75,7 @@ class GenericApiHandler(web.RequestHandler):
 
     @web.asynchronous
     @gen.coroutine
+    @check.is_authorized
     @check.valid_token
     @check.no_body
     @check.miss_fields
