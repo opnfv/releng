85ecd9483cfe6437180d0dbef95adc7e513e45a0
Tim Rozet <trozet@redhat.com>
2017-09-11 22:46:36 -0400

    Apex: Fix pythonpath for build
    Change-Id: I503250b2152686cb1c9168aeaa6a11f614b0e954
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/apex/apex-build.sh
+++ jjb/apex/apex-build.sh
@@ -31,7 +31,7 @@ BUILD_DIRECTORY=${WORKSPACE}/build
 pushd ${BUILD_DIRECTORY}
 make clean
 popd
-export PYTHONPATH=${WORKSPACE}:${PYTHONPATH}
+export PYTHONPATH=${WORKSPACE}
 python3 apex/build.py $BUILD_ARGS
 RPM_VERSION=$(grep Version: $WORKSPACE/build/rpm_specs/opnfv-apex.spec | awk '{ print $2 }')-$(echo $OPNFV_ARTIFACT_VERSION | tr -d '_-')
 # list the contents of BUILD_OUTPUT directory
