4f83018fb5cc399879748d023a22507679b4f075
Trevor Bramwell <tbramwell@linuxfoundation.org>
2017-09-21 15:52:35 -0700

    Change Anteater Report Directory Permissions
    Setting this to allow all users to read/write the permissions enable the
    container to write to the volume mounted reports directory even though
    it is owned by the Jenkins user.
    
    JIRA: RELENG-313
    
    Change-Id: Ib26e9b98cd17607c98a180888593c42376458f7f
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/ci_gate_security/anteater-security-audit.sh
+++ jjb/ci_gate_security/anteater-security-audit.sh
@@ -2,6 +2,8 @@
 cd $WORKSPACE
 REPORTDIR='.reports'
 mkdir -p $REPORTDIR
+# Ensure any user can read the reports directory
+chmod 777 $REPORTDIR
 echo "Generating patchset file to list changed files"
 git diff HEAD^1 --name-only | sed "s#^#/home/opnfv/anteater/$PROJECT/#" > $WORKSPACE/patchset
 echo "Changed files are"
