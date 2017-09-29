dfe753614f32cc74bcd7d73e088a4dad298e85eb
Jose Lausuch <jalausuch@suse.com>
2017-09-29 16:02:12 +0200

    [Functest] Remove former way to execute run_tests
    It was used in Danube and there are no more Functest jobs
    that are runnign stable/danube branch.
    
    Change-Id: If297487767e8e8ead39aa4a10f58c6554be197b0
    Signed-off-by: Jose Lausuch <jalausuch@suse.com>

--- jjb/functest/functest-suite.sh
+++ jjb/functest/functest-suite.sh
@@ -10,11 +10,7 @@ global_ret_val=0
 
 tests=($(echo $FUNCTEST_SUITE_NAME | tr "," "\n"))
 for test in ${tests[@]}; do
-    if [ "$BRANCH" == 'master' ]; then
-        cmd="run_tests -t $test"
-    else
-        cmd="python /home/opnfv/repos/functest/functest/ci/run_tests.py -t $test"
-    fi
+    cmd="run_tests -t $test"
     docker exec $container_id $cmd
     let global_ret_val+=$?
 done
