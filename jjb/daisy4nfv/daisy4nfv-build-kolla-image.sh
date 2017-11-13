34f24ee7644b726b29f84e1f2938304710600115
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-11-13 02:31:10 -0500

    [daisy] Skip signing kolla images
    No key avaliable at gs://opnfv-signing-keys/zte-virtual-subkey,
    so this PS skip signing key for kolla images.
    
    Change-Id: I1b934f01faeec0657a0019cd35ed209a511cdcdd
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- jjb/daisy4nfv/daisy4nfv-build-kolla-image.sh
+++ jjb/daisy4nfv/daisy4nfv-build-kolla-image.sh
@@ -14,34 +14,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-importkey () {
-    # clone releng repository
-    echo "Cloning releng repository..."
-    [ -d releng ] && rm -rf releng
-    git clone https://gerrit.opnfv.org/gerrit/releng ./releng/ &> /dev/null
-    #this is where we import the siging key
-    if [ -f ./releng/utils/gpg_import_key.sh ]; then
-        source ./releng/utils/gpg_import_key.sh
-    fi
-}
-
 upload_image_to_opnfv () {
     image=$1
-
-    importkey
-    if gpg2 --list-keys | grep "opnfv-helpdesk@rt.linuxfoundation.org"; then
-        echo "Signing Key avaliable"
-        SIGN_ARTIFACT="true"
-    fi
-
-    if [[ -n "$SIGN_ARTIFACT" && "$SIGN_ARTIFACT" == "true" ]]; then
-        gpg2 -vvv --batch --yes --no-tty \
-            --default-key opnfv-helpdesk@rt.linuxfoundation.org  \
-            --passphrase besteffort \
-            --detach-sig $image
-        gsutil cp $image.sig gs://$GS_URL/upstream/$image.sig
-        echo "Image signature upload complete!"
-    fi
 
     sha512sum -b $image > $image.sha512sum
     gsutil cp $image.sha512sum gs://$GS_URL/upstream/$image.sha512sum
