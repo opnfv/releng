c3e54573e1f70c483c1772a0a3cb9fb7aaf217a3
Jose Lausuch <jalausuch@suse.com>
2017-11-15 15:47:27 +0100

    [Functest] Remove prepare_env from the docker cmds
    Change-Id: Ia41906f9e8ee15409cb28bb9bb621a7ab1964eee
    Signed-off-by: Jose Lausuch <jalausuch@suse.com>

--- jjb/functest/functest-alpine.sh
+++ jjb/functest/functest-alpine.sh
@@ -4,10 +4,25 @@ set -e
 set +u
 set +o pipefail
 
+check_os_deployment() {
+    FUNCTEST_IMAGE=opnfv/functest-healthcheck:${DOCKER_TAG}
+    cmd="docker run --privileged=true ${volumes} ${FUNCTEST_IMAGE} /bin/bash -c check_deployment"
+    eval ${cmd}
+    ret_value=$?
+    if [ ${ret_value} != 0 ]; then
+        echo "ERROR: Problem while checking OpenStack deployment."
+        exit 1
+    else
+        echo "OpenStack deployment OK."
+    fi
+}
+
+
 run_tiers() {
     tiers=$1
-    cmd_opt="prepare_env start && run_tests -r -t all"
-    [[ $BUILD_TAG =~ "suite" ]] && cmd_opt="prepare_env start && run_tests -t all"
+    check_os_deployment
+    cmd_opt="run_tests -r -t all"
+    [[ $BUILD_TAG =~ "suite" ]] && cmd_opt="run_tests -t all"
     ret_val_file="${HOME}/opnfv/functest/results/${BRANCH##*/}/return_value"
     echo 0 > ${ret_val_file}
 
@@ -31,8 +46,9 @@ run_tiers() {
 
 run_test() {
     test_name=$1
-    cmd_opt="prepare_env start && run_tests -t ${test_name}"
-    [[ $BUILD_TAG =~ "suite" ]] && cmd_opt="prepare_env start && run_tests -t ${test_name}"
+    check_os_deployment
+    cmd_opt="run_tests -t ${test_name}"
+    [[ $BUILD_TAG =~ "suite" ]] && cmd_opt="run_tests -t ${test_name}"
     ret_val_file="${HOME}/opnfv/functest/results/${BRANCH##*/}/return_value"
     echo 0 > ${ret_val_file}
     # Determine which Functest image should be used for the test case
