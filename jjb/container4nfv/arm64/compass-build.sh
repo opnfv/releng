73a261bb6d60af7916d006a58b915cfced509273
Yibo Cai <yibo.cai@arm.com>
2018-01-10 17:52:27 +0800

    container4nfv: add arm64 test scenarios
    Change-Id: I2a91409fb3a650e9d9cacd3f6ffe4b17bc394950
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>

---
+++ jjb/container4nfv/arm64/compass-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd compass4nfv
+
+COMPASS_WORK_DIR=$WORKSPACE/../compass-work
+mkdir -p $COMPASS_WORK_DIR
+ln -s $COMPASS_WORK_DIR work
+
+#TODO: remove workaround after all arm64 patches merged
+curl -s http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash -s {scenario}
+
+# build tarball
+COMPASS_ISO_REPO='http://people.linaro.org/~yibo.cai/compass' ./build.sh
