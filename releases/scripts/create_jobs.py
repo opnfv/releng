c1d531cea8e3dcc25554bd626c2e2c790734a3f8
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-01-11 14:27:48 -0800

    Release Automation
    Tracking releases through yaml file similar to the openstack/releases
    project.
    
    Includes a schema file to be for validation, jobs for creating gerrit
    branches and stable branch jobs, and documentation for projects on
    creating their releases.
    
    Change-Id: Id1876482723e01806c0a6932126dff5ea314eae5
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ releases/scripts/create_jobs.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+"""
+Create Gerrit Branches
+"""
+
+import argparse
+import logging
+import os
+import re
+import yaml
+
+from ruamel.yaml import YAML
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def has_string(filepath, string):
+    """
+    Return True if the given filepath contains the regex string
+    """
+    with open(filepath) as yaml_file:
+        for line in yaml_file:
+            if string.search(line):
+                return True
+    return False
+
+
+def jjb_files(project, release):
+    """
+    Return sets of YAML file names that contain 'stream' for a given
+    project, and file that already contain the stream.
+    """
+    files, skipped = set(), set()
+    file_ending = re.compile(r'ya?ml$')
+    search_string = re.compile(r'^\s+stream:')
+    release_string = re.compile(r'- %s:' % release)
+    jjb_path = os.path.join('jjb', project)
+
+    for file_name in os.listdir(jjb_path):
+        file_path = os.path.join(jjb_path, file_name)
+        if file_ending.search(file_path) and os.path.isfile(file_path):
+            if has_string(file_path, release_string):
+                skipped.add(file_path)
+            elif has_string(file_path, search_string):
+                files.add(file_path)
+    return (files, skipped)
+
+
+def main():
+    """
+    Create Jenkins Jobs for stable branches in Release File
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', '-f',
+                        type=argparse.FileType('r'),
+                        required=True)
+    args = parser.parse_args()
+
+    project_yaml = yaml.safe_load(args.file)
+
+    # Get the release name from the file path
+    release = os.path.split(os.path.dirname(args.file.name))[1]
+
+    create_jobs(release, project_yaml)
+
+
+def create_jobs(release, project_yaml):
+    """Add YAML to JJB files for release stream"""
+    logger = logging.getLogger()
+
+    # We assume here project keep their subrepo jobs under the part
+    # project name. Otherwise we'll have to look for jjb/<repo> for each
+    # branch listed.
+    project, _ = next(iter(project_yaml['branches'][0]['location'].items()))
+
+    yaml_parser = YAML()
+    yaml_parser.preserve_quotes = True
+    yaml_parser.explicit_start = True
+    # yaml_parser.indent(mapping=2, sequence=1, offset=0)
+
+    (job_files, skipped_files) = jjb_files(project, release)
+
+    logger.info("Jobs already exists for %s in files: %s",
+                project, ', '.join(skipped_files))
+    # Exit if there are not jobs to create
+    if not job_files:
+        return
+    logger.info("Creating Jenkins Jobs for %s in files: %s",
+                project, ', '.join(job_files))
+
+    stable_branch_stream = """\
+      %s:
+          branch: 'stable/{stream}'
+          gs-pathname: '/{stream}'
+          disabled: false
+    """ % release
+
+    stable_branch_yaml = yaml_parser.load(stable_branch_stream)
+    stable_branch_yaml[release].yaml_set_anchor(release, always_dump=True)
+
+    for job_file in job_files:
+        yaml_jjb = yaml_parser.load(open(job_file))
+
+        project_config = yaml_jjb[0]['project']['stream']
+        # There is an odd issue where just appending adds a newline before the
+        # branch config, so we append (presumably after master) instead.
+        project_config.insert(1, stable_branch_yaml)
+
+        # TODO: Fix indentation so only sublists are indented by 2
+        #   as currently this is modifying almost every line of the file
+        #   http://yaml.readthedocs.io/en/latest/example.html
+        #   https://stackoverflow.com/questions/48029461/ \
+        #      how-to-preserve-indenting-in-key-value-when-dumping-yaml
+        yaml_parser.dump(yaml_jjb, open(job_file, 'w'))
+
+
+if __name__ == "__main__":
+    main()
