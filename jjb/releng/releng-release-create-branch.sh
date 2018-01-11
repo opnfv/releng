7a5ec5c54a40f3b6c78076597a82d8120ac9e4a4
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-01-11 14:27:48 -0800

    Release Automation
    Tracking releases through yaml file similar to the openstack/releases
    project.
    
    Includes a schema file to be for validation, jobs for creating gerrit
    branches and stable branch jobs, and documentation for projects on
    creating their releases.
    
    Change-Id: Id1876482723e01806c0a6932126dff5ea314eae5
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/releng/releng-release-create-branch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -x
+
+# Activate virtualenv, supressing shellcheck warning
+# shellcheck source=/dev/null
+. $WORKSPACE/venv/bin/activate
+
+pip install -r releases/scripts/requirements.txt
+
+STREAM=${STREAM:-'nostream'}
+RELEASE_FILES=$(git diff HEAD^1 --name-only | grep "^releases/$STREAM")
+
+for release_file in $RELEASE_FILES; do
+    python releases/scripts/create_branch.py -f $release_file
+    python releases/scripts/create_jobs.py -f $release_file
+done
