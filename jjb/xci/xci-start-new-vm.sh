a7a6bda9bcc86266650f23d561e640cc715e41c9
Fatih Degirmenci <fdegir@gmail.com>
2018-01-22 09:43:23 +0100

    xci: Move start new vm execute shell to external script
    The script used by verify jobs to create VMs for CI can be reused
    by other jobs as well so it is moved out of the yaml file.
    
    Apart from that, if we decide to change the way we run CI (not
    using VMs for performance reasons), we don't have to make big changes
    to yaml either.
    
    Change-Id: If132a1ebe7c9c3d6999b98dc3d7e441694227306
    Signed-off-by: Fatih Degirmenci <fdegir@gmail.com>

---
+++ jjb/xci/xci-start-new-vm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------
+# This script is used by CI and executed by Jenkins jobs.
+# You are not supposed to use this script manually if you don't know
+# what you are doing.
+#----------------------------------------------------------------------
+
+# skip the deployment if the patch doesn't impact the deployment
+if [[ "$GERRIT_TOPIC" =~ 'skip-verify' ]]; then
+    echo "Skipping the deployment!"
+    exit 0
+fi
+
+cd $WORKSPACE
+
+# The start-new-vm.sh script will copy the entire releng-xci directory
+# so lets prepare the test script now so it can be copied by the script.
+# Please do not move it elsewhere or you would have to move it to the VM
+# yourself.
+cat > xci_test.sh<<EOF
+#!/bin/bash
+export DISTRO=$DISTRO
+export DEPLOY_SCENARIO=$DEPLOY_SCENARIO
+export FUNCTEST_MODE=$FUNCTEST_MODE
+export FUNCTEST_SUITE_NAME=$FUNCTEST_SUITE_NAME
+export XCI_FLAVOR=$XCI_FLAVOR
+export CLEAN_DIB_IMAGES=$CLEAN_DIB_IMAGES
+export OPNFV_RELENG_DEV_PATH=/home/devuser/releng-xci/
+export INSTALLER_TYPE=$INSTALLER_TYPE
+export GIT_BASE=$GIT_BASE
+export JENKINS_HOME=$JENKINS_HOME
+
+cd xci
+./xci-deploy.sh
+EOF
+chmod a+x xci_test.sh
+
+export XCI_BUILD_CLEAN_VM_OS=false
+export XCI_UPDATE_CLEAN_VM_OS=true
+
+./xci/scripts/vm/start-new-vm.sh $DISTRO
