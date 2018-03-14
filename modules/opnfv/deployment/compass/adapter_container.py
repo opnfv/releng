624b9ab39c11e1cf2e37af8f2275824c848c7e91
Yifei Xue <xueyifei@huawei.com>
2018-03-14 19:16:29 +0800

    Add a new adapter for containerized Compass installer
    JIRA: -
    
    Compass installer has been containerized since OPNFV 5.0.
    To enable SFC test cases in Functest for Compass installer,
    a new adapter is needed to be compatible with both containerized
    Compass installer and SFC test cases.
    
    Change-Id: I4e1a9e5fcbec016a0c3fde77ace9c2cc677e3e4c
    Signed-off-by: Yifei Xue <xueyifei@huawei.com>

---
+++ modules/opnfv/deployment/compass/adapter_container.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from opnfv.deployment import manager
+from opnfv.utils import opnfv_logger as logger
+from opnfv.utils import ssh_utils
+
+import yaml
+import os
+import docker
+
+logger = logger.Logger(__name__).getLogger()
+
+class ContainerizedCompassAdapter():
+
+    def __init__(self, installer_ip, installer_user, pkey_file):
+
+        self.installer = 'compass'
+        self.installer_ip = installer_ip
+        self.installer_user = installer_user
+        self.pkey_file = pkey_file
+        self.DEST_PATH_UC = "/tmp/openstack_user_config.yml"
+        self.nodes = []
+        self.ROLES = {}
+
+        if pkey_file is not None and not os.path.isfile(pkey_file):
+            raise Exception(
+                'The private key file %s does not exist!' % pkey_file)
+
+    def _find_nodes(self, file):
+        nodes = file['compute_hosts']
+        for compute in nodes:
+            self.ROLES[compute] = 'compute'
+        controllers = file['haproxy_hosts']
+        for controller in controllers:
+            nodes[controller] = controllers[controller]
+            self.ROLES[controller] = 'controller'
+        return nodes
+
+    def _process_nodes(self, raw_nodes):
+        nodes = []
+
+        for node in raw_nodes:
+            name = node
+            ip = raw_nodes[node]['ip']
+            status = 'active'
+            id = None
+            if self.ROLES[node] == 'controller':
+                roles = 'controller'
+            elif self.ROLES[node] == 'compute':
+                roles = 'compute'
+            ssh_client = ssh_utils.get_ssh_client(hostname=ip,
+                                                  username=self.installer_user,
+                                                  pkey_file=self.pkey_file)
+            node = manager.Node(id, ip, name, status, roles, ssh_client)
+            nodes.append(node)
+
+        return nodes
+
+    def get_nodes(self, options=None):
+        try:
+            # if we have retrieved previously all the nodes, don't do it again
+            # This fails the first time when the constructor calls this method
+            # therefore the try/except
+            if len(self.nodes) > 0:
+                return self.nodes
+        except:
+            pass
+
+        client = docker.from_env()
+        functest_container = client.containers.list(
+            filters={'ancestor':'opnfv/functest-features'})
+        functest_container[0].exec_run(
+            'docker cp compass-tasks:/etc/openstack_deploy/openstack_user_config.yml /tmp')
+
+        with open(self.DEST_PATH_UC, 'r') as stream:
+            try:
+                file = yaml.load(stream)
+                raw_nodes = self._find_nodes(file)
+            except yaml.YAMLError as exc:
+                logger.error(exc)
+        self.nodes = self._process_nodes(raw_nodes)
+        return self.nodes
