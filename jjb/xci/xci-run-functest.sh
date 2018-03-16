d59237b18e4621ee11d1656c4ad2beb24ec7947b
Markos Chandras <mchandras@suse.de>
2018-03-16 18:33:11 +0000

    jjb: xci: xci-run-functest: Fix functest execution
    We need to enter the $HOME/releng-xci/xci directory instead of
    /root/relneg-xci/xci since we are ssh'ing as 'devuser'.
    
    Change-Id: Id05d5012d58443c7f415a58f9f3d1ae16fd25b55
    Signed-off-by: Markos Chandras <mchandras@suse.de>

--- jjb/xci/xci-run-functest.sh
+++ jjb/xci/xci-run-functest.sh
@@ -51,7 +51,7 @@ if ! sed -n "/^- scenario: $DEPLOY_SCENARIO$/,/^$/p" $OPNFV_SCENARIO_REQUIREMENT
     exit 0
 fi
 
-ssh -F $HOME/.ssh/${DISTRO}-xci-vm-config ${DISTRO}_xci_vm "cd /root/releng-xci/xci && PATH=/home/devuser/.local/bin:$PATH ansible-playbook -i installer/osa/files/$XCI_FLAVOR/inventory playbooks/prepare-functest.yml"
+ssh -F $HOME/.ssh/${DISTRO}-xci-vm-config ${DISTRO}_xci_vm "cd releng-xci/xci && PATH=/home/devuser/.local/bin:$PATH ansible-playbook -i installer/osa/files/$XCI_FLAVOR/inventory playbooks/prepare-functest.yml"
 echo "Running functest"
 ssh -F $HOME/.ssh/${DISTRO}-xci-vm-config ${DISTRO}_xci_vm_opnfv "/root/run-functest.sh"
 echo "Functest log"
