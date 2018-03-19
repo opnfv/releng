6f6cf24f53add8df9cd976da3ba211ab564814fd
Cédric Ollivier <cedric.ollivier@orange.com>
2018-03-19 11:20:40 +0100

    Use /root/.kube/config for all installers
    There is no reason to use one specific dir per installer in Functest
    containers.
    
    Change-Id: Ib2e1443b14c15aa385e65194f70f6442e0c9f7b9
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- jjb/functest/functest-k8.sh
+++ jjb/functest/functest-k8.sh
@@ -19,7 +19,7 @@ if [[ ${INSTALLER_TYPE} == 'compass' ]]; then
     KUBE_MASTER_IP=$(echo $KUBE_MASTER_URL|awk -F'https://|:[0-9]+' '$0=$2')
     echo "export KUBE_MASTER_IP=$KUBE_MASTER_IP" >> $rc_file
 elif [[ ${INSTALLER_TYPE} == 'joid' && ${BRANCH} == 'master' ]]; then
-    admin_conf_file_vol="-v ${HOME}/joid_config/config:/root/joid_config/config"
+    admin_conf_file_vol="-v ${HOME}/joid_config/config:/root/.kube/config"
     rc_file=${HOME}/joid_config/k8config
 else
     echo "Not supported by other installers yet"
