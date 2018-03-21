4060688edd500c75a17e5c4a981d8cd3aba6321f
chenjiankun <chenjiankun1@huawei.com>
2018-03-21 02:12:15 +0000

    Fetch creds according scenario type(os or k8)
    We have two kinds of scenario: os and k8, their creds
    is defferent, so we need to fetch creds according different
    scenario type.
    
    Change-Id: I54d1a1fcdfa981fed2a1433693aa3483abf4baf0
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- jjb/yardstick/yardstick-get-k8s-conf.sh
+++ jjb/yardstick/yardstick-get-k8s-conf.sh
@@ -7,6 +7,7 @@ if [[ "${DEPLOY_SCENARIO:0:2}" == "k8" ]];then
     if [[ ${INSTALLER_TYPE} == 'joid' ]];then
         juju scp kubernetes-master/0:config "${dest_path}"
     elif [[ ${INSTALLER_TYPE} == 'compass' ]];then
+        echo "Copy admin.conf to ${dest_path}"
         docker cp compass-tasks:/opt/admin.conf "${dest_path}"
     fi
 fi
