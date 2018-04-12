801948cb91c29aceb05a4faa4845048fee7c90af
Feng Pan <fpan@redhat.com>
2018-04-12 16:51:07 -0400

    APEX: Fix gate check scenario detection
    - Change scenario parsing to allow k8s scenarios in gate jobs.
    - Remove scenario detection in shell scripts.
    
    Also changes network setting file for fd.io scenarios as we
    don't use separate config files any more.
    
    Change-Id: Icbcc6cb7b6b66c7cc43f5d6c58dd97730e41ea5f
    Signed-off-by: Feng Pan <fpan@redhat.com>

--- jjb/apex/apex-deploy.sh
+++ jjb/apex/apex-deploy.sh
@@ -13,21 +13,8 @@ echo
 if [ -z "$DEPLOY_SCENARIO" ]; then
   echo "Deploy scenario not set!"
   exit 1
-elif [[ "$DEPLOY_SCENARIO" == *gate* ]]; then
-  echo "Detecting Gating scenario..."
-  if [ -z "$GERRIT_EVENT_COMMENT_TEXT" ]; then
-    echo "ERROR: Gate job triggered without comment!"
-    exit 1
-  else
-    DEPLOY_SCENARIO=$(echo ${GERRIT_EVENT_COMMENT_TEXT} | grep start-gate-scenario | grep -Eo 'os-.*$')
-    if [ -z "$DEPLOY_SCENARIO" ]; then
-      echo "ERROR: Unable to detect scenario in Gerrit Comment!"
-      echo "Format of comment to trigger gate should be 'start-gate-scenario: <scenario>'"
-      exit 1
-    else
-      echo "Gate scenario detected: ${DEPLOY_SCENARIO}"
-    fi
-  fi
+else
+  echo "Deploy scenario: ${DEPLOY_SCENARIO}"
 fi
 
 # Dev or RPM/ISO build
@@ -143,8 +130,6 @@ fi
 
 if [ "$IPV6_FLAG" == "True" ]; then
   NETWORK_FILE="${NETWORK_SETTINGS_DIR}/network_settings_v6.yaml"
-elif echo ${DEPLOY_SCENARIO} | grep fdio; then
-  NETWORK_FILE="${NETWORK_SETTINGS_DIR}/network_settings_vpp.yaml"
 else
   NETWORK_FILE="${NETWORK_SETTINGS_DIR}/network_settings.yaml"
 fi
