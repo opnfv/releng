f39b860650068f625c91ef94979a1f6257f32039
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-04-27 14:00:16 -0700

    Verify and Create Tags from Release File
    Updates the releases verify and merge jobs to check release files for
    the following:
    
     - commits exist
     - commits are on stable-branch
    
    If both of these criteria are met and the patch is merged, the repos
    will be tagged at the specified commits and pushed.
    
    If a repo has already been tagged but not on stable-branch the job will
    not exit, but will warn that the commit doesn't exist on the correct
    branch. This is because tags should never be removed.
    
    This adds an additional script 'repos.py' that provides an quick
    interface for pulling out information from release files, along with the
    script 'release-status.sh' for checking the status of tags.
    
    NOTE: The branch creation script has been disabled until it can be reworked.
    
    Change-Id: I498bc74f20aa50d2bd321771f20a77905b246399
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/releng/releng-release-create-venv.sh
+++ jjb/releng/releng-release-create-venv.sh
@@ -7,21 +7,12 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-set -xe
+set -e -o pipefail
+echo "---> Create virtualenv"
 
-# Activate virtualenv, supressing shellcheck warning
-# shellcheck source=/dev/null
-. $WORKSPACE/venv/bin/activate
+sudo pip install virtualenv
+virtualenv $WORKSPACE/venv
+# shellcheck source=$WORKSPACE/venv/bin/activate disable=SC1091
+source $WORKSPACE/venv/bin/activate
+pip install --upgrade pip
 pip install -r releases/scripts/requirements.txt
-
-STREAM=${STREAM:-'nostream'}
-RELEASE_FILES=$(git diff HEAD^1 --name-only -- "releases/$STREAM")
-
-# TODO: The create_branch.py should be refactored so it can be used here
-# to verify the commit exists that is being added, along with
-# jjb/<project>
-for release_file in $RELEASE_FILES; do
-    python releases/scripts/verify_schema.py \
-    -s releases/schema.yaml \
-    -y $release_file
-done
---
+++ releases/scripts/release-status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o pipefail
+
+TAG="${TAG:-opnfv-6.0.0}"
+RELEASE="${RELEASE:-fraser}"
+
+for project in releases/$RELEASE/*; do
+    python releases/scripts/repos.py -n -f $project >> repos.txt
+done
+
+while read -r repo
+do
+    tag="$(git ls-remote "https://gerrit.opnfv.org/gerrit/$repo.git" "refs/tags/$TAG")"
+    echo "$repo $tag"
+done < repos.txt
+
+# rm repos.txt
