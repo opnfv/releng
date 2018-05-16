8239f39f5804d5262a4d2c4f566debe41fd46616
Fatih Degirmenci <fdegir@gmail.com>
2018-05-16 20:32:16 +0200

    openci: Add OPNFV scenario composition and test jobs for the demo
    The flow is
    - OPNFV compose job gets triggered by the ArtifactPublishedEvents published by
    ODL and ONAP autorelease jobs, composing the corresponding scenarios. At the
    end, CompositionDefinedEvent is published for the composed scenario.
    - OPNFV test job gets triggered by the CompositionDefinedEvents published by
    OPNFV compose jobs, deploying and testing the composed scenario. At the end,
    ConfidenceLevelModifiedEvent is published for the tested scenario which can
    be consumed by ODL and/or ONAP Jenkinses, doing further promotion.
    
    Change-Id: Ice1736d65f3f2b26e849302fec2bb0c579da4a1c
    Signed-off-by: Fatih Degirmenci <fdegir@gmail.com>

---
+++ jjb/openci/create-clme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 Ericsson AB and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script creates ConfidenceLevelModifiedEvent
+# The JMS Messaging Plugin doesn't handle the newlines well so the eventBody is
+# constructed on a single line. This is something that needs to be fixed properly
+
+cat << EOF > $WORKSPACE/event.properties
+type=$PUBLISH_EVENT_TYPE
+origin=$PUBLISH_EVENT_ORIGIN
+eventBody="{ 'type': '$EVENT_TYPE', 'id': '$(uuidgen)', 'time': '$(date -u +%Y-%m-%d_%H:%M:%SUTC)', 'origin': '$EVENT_ORIGIN', 'buildUrl': '$BUILD_URL', 'branch': 'master', 'compositionName': '$DEPLOY_SCENARIO', 'compositionMetadataUrl': '$SCENARIO_METADATA', 'confidenceLevel': { $CONFIDENCE_LEVEL } }"
+EOF
+echo "Constructed $EVENT_TYPE"
+echo "--------------------------------------------"
+cat $WORKSPACE/event.properties
+echo "--------------------------------------------"
