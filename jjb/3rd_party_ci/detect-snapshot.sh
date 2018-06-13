10e353197c0164e3b1972e070aa6cea265879992
Jamo Luhrsen <jluhrsen@redhat.com>
2018-06-12 17:00:04 -0700

    Run ODL CSIT on Apex from Cperf container
    This patch adds the ability to deploy a cperf container and execute ODL
    csit against an OPNFV deployment with ODL. The cperf job to execute csit
    requires an RC file, SSH key to access OPNFV nodes, and a yaml
    descriptor file that contains per node information about each
    control/compute node.
    
    This patch also adds triggering the cperf CSIT job inside of the 3rd
    party ODL netvirt verification suite. That job uses Apex snapshots to
    bring up deployment (no undercloud).
    
    Additionally this patch includes some changes to allow multi version
    snapshots to work. Multiple snapshots are now being created for
    different OpenStack branches along with noha or HA type topologies. This
    patch includes the ability to detect the desired scenario as triggered
    by ODL Netvirt gerrit. Now in ODL netvirt gerrit a user may provide:
    "opnfv-check <OS version>-<noha|ha>" style syntax to initiate 3rd Party
    OPNFV CI on a particular OS version/HA setup.
    
    Change-Id: I51a27545c985ce74c1c72fe0933eb451939a8c05
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>
    Signed-off-by: Tim Rozet <trozet@redhat.com>

---
+++ jjb/3rd_party_ci/detect-snapshot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Detecting requested OpenStack branch and topology type in gerrit comment"
+parsed_comment=$(echo $GERRIT_EVENT_COMMENT_TEXT | sed -n 's/^opnfv-check\s*//p')
+parsed_comment=$(echo $parsed_comment | sed -n 's/\s*$//p')
+if [ ! -z "$parsed_comment" ]; then
+  if echo $parsed_comment | grep -E '^[a-z]+-(no)?ha'; then
+    IFS='-' read -r -a array <<< "$parsed_comment"
+    os_version=${array[0]}
+    topo=${array[1]}
+    echo "OS version detected in gerrit comment: ${os_version}"
+    echo "Topology type detected in gerrit comment: ${topo}"
+  else
+    echo "Invalid format given for scenario in gerrit comment: ${parsed_comment}...aborting"
+    exit 1
+  fi
+else
+  echo "No scenario given in gerrit comment, will use default (master OpenStack, noha)"
+  os_version='master'
+  topo='noha'
+fi
+
+echo "Writing variables to file"
+echo > detected_snapshot << EOI
+OS_VERSION=$os_version
+TOPOLOGY=$topo
+SNAP_CACHE=$HOME/snap_cache/$os_version/$topo
+EOI
