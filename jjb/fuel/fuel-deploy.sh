2acf3b2a0b91ec9158b5025f53ef2a6490f61ab3
Aric Gardner <agardner@linuxfoundation.org>
2018-07-11 14:00:36 -0400

    Remove securedlab as a default and it's verify job
    seruredlab repo is only for builds on legacy releases. (danube|euphrates)
    
    Change-Id: I7c5e48eec7576547941c4a2c41a503767305f5fe
    Signed-off-by: Aric Gardner <agardner@linuxfoundation.org>

--- jjb/fuel/fuel-deploy.sh
+++ jjb/fuel/fuel-deploy.sh
@@ -43,7 +43,7 @@ LAB_NAME=${NODE_NAME/-*}
 # shellcheck disable=SC2153
 POD_NAME=${NODE_NAME/*-}
 # Armband might override LAB_CONFIG_URL, all others use the default
-LAB_CONFIG_URL=${LAB_CONFIG_URL:-'ssh://jenkins-ericsson@gerrit.opnfv.org:29418/securedlab'}
+LAB_CONFIG_URL=${LAB_CONFIG_URL:-'ssh://jenkins-ericsson@gerrit.opnfv.org:29418/pharos'}
 
 # Fuel requires deploy script to be ran with sudo, Armband does not
 SUDO='sudo -E'
@@ -70,6 +70,7 @@ chmod a+x "${HOME}" "${TMPDIR}"
 
 cd "${WORKSPACE}" || exit 1
 if [[ "$BRANCH" =~ (danube|euphrates) ]]; then
+    LAB_CONFIG_URL='ssh://jenkins-ericsson@gerrit.opnfv.org:29418/securedlab'
     if [[ "${LAB_CONFIG_URL}" =~ ^(git|ssh):// ]]; then
         echo "Cloning securedlab repo ${BRANCH}"
         LOCAL_CFG="${TMPDIR}/securedlab"
