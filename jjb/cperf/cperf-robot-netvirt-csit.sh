8d98031c20f0ecbff308f191e1fb944dcfd90f49
Jamo Luhrsen <jluhrsen@redhat.com>
2018-08-04 09:16:51 -0700

    Change OS_DEPLOY to OPENSTACK_TOPO
    Change-Id: Ie9c2e9418dc7a2c8d6fcc798a9132303401a1128
    Signed-off-by: Jamo Luhrsen <jluhrsen@redhat.com>

--- jjb/cperf/cperf-robot-netvirt-csit.sh
+++ jjb/cperf/cperf-robot-netvirt-csit.sh
@@ -93,8 +93,8 @@ robot_cmd="pybot \
   -v ODL_STREAM:$ODL_STREAM \
   -v ODL_SYSTEM_IP:$CONTROLLER_1_IP \
   -v OS_CONTROL_NODE_IP:$CONTROLLER_1_IP \
-  -v OS_DEPLOY="0cmb-1ctl-2cmp" \
   -v OPENSTACK_BRANCH:$FULL_OS_VER \
+  -v OPENSTACK_TOPOLOGY="0cmb-1ctl-2cmp" \
   -v OS_USER:heat-admin \
   -v ODL_ENABLE_L3_FWD:yes \
   -v ODL_SYSTEM_USER:heat-admin \
