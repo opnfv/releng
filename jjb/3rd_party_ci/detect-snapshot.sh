7859ef27fbd2060bc20868cea3c06e32ef536a5d
Tim Rozet <trozet@redhat.com>
2018-08-17 10:13:48 -0400

    Apex: Update snapshot detection
    Detection now allows for allinone types.
    
    Change-Id: I61d0e22bd3c940c509917db820281dab1a5fc66f
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- jjb/3rd_party_ci/detect-snapshot.sh
+++ jjb/3rd_party_ci/detect-snapshot.sh
@@ -8,9 +8,8 @@ parsed_comment=$(echo $GERRIT_EVENT_COMMENT_TEXT | sed -n 's/^.*check-opnfv\s*//
 parsed_comment=$(echo $parsed_comment | sed -n 's/\s*$//p')
 if [ ! -z "$parsed_comment" ]; then
   if echo $parsed_comment | grep -E '^[a-z]+-(no)?ha'; then
-    IFS='-' read -r -a array <<< "$parsed_comment"
-    os_version=${array[0]}
-    topo=${array[1]}
+    os_version=${parsed_comment%%"-"*}
+    topo=${parsed_comment#*"-"}
     echo "OS version detected in gerrit comment: ${os_version}"
     echo "Topology type detected in gerrit comment: ${topo}"
   else
