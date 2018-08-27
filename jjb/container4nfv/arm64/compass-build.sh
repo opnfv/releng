d134335bef97be471c7953bafe530fcdc1e2af47
Yibo Cai <yibo.cai@arm.com>
2018-08-27 09:41:31 +0800

    container4nfv: fix command line syntax error
    Jira: ENTWLS-702
    
    Change-Id: Ib4bbf639c3a1c80eec9a9e3d52c17e934a465ce1
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>

--- jjb/container4nfv/arm64/compass-build.sh
+++ jjb/container4nfv/arm64/compass-build.sh
@@ -9,6 +9,6 @@ ln -s $COMPASS_WORK_DIR work
 
 sudo docker rm -f `docker ps | grep compass | cut -f1 -d' '` || true
 
-curl -s http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash -s {scenario} || true
+curl -s http://people.linaro.org/~yibo.cai/compass/compass4nfv-arm64-fixup.sh | bash || true
 
 ./build.sh
