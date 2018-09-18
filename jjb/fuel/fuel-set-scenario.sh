1ed0a84333cc7d112ac8b4c626b971bd6f4ad296
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-09-18 18:19:47 +0200

    [fuel] verify: Add baremetal testing on request
    Duplicate fuel-verify jobs based on cluster type (virtual, baremetal)
    and allow (re)checking changes using a specific scenario:
    - ALL changes will trigger a virtual POD CI cycle (build, deploy,
      smoke) on submit, rebase etc., just like they used to;
    - changes with a topic name containing 'baremetal' will also trigger
      a baremetal HA deployment using the simplest HA scenario;
    - change comments containing a substring formatted like:
      * `recheck: os-odl-nofeature-ha`
      * `reverify: os-odl-nofeature-noha`
      * `recheck`
      will trigger only virtual or baremetal deployments of the required
      scenario (default os-nosdn-nofeature-noha) on:
      * virtual   PODs if scenario is NOHA;
      * baremetal PODs if scenario is HA;
    
    Limitations:
    - non-default HA scenarios can only be verified on manual request via
      a change comment (e.g. 'recheck: os-odl-ovs-ha');
    
    JIRA: FUEL-390
    
    Change-Id: I6368cfd045180d6a7202b8d817b17a4b123ba2af
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

---
+++ jjb/fuel/fuel-set-scenario.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 SUSE, Mirantis Inc., Enea Software AB and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o pipefail
+set -x
+
+#----------------------------------------------------------------------
+# This script is used by CI and executed by Jenkins jobs.
+# You are not supposed to use this script manually if you don't know
+# what you are doing.
+#----------------------------------------------------------------------
+
+# This function allows developers to specify the impacted scenario by
+# requesting a RE-check via a gerrit change comment under a specific format,
+# or defaulting to the simplest HA scenario for baremetal via topic name.
+#
+# Patterns to be searched in change comment:
+#   recheck: <scenario-name>
+#   reverify: <scenario-name>
+# Examples:
+#   recheck: os-odl-ovs-noha
+#   reverify: os-nosdn-nofeature-ha
+#
+# Patterns to be searched in topic name:
+#   baremetal
+
+function set_scenario() {
+    # process gerrit event comment text (if present), topic branch name
+    DEPLOY_SCENARIO=$(echo "${GERRIT_EVENT_COMMENT_TEXT}" | \
+                      grep -Po '(?!:(recheck|reverify):\s*)([-\w]+ha)')
+    if [ -z "${DEPLOY_SCENARIO}" ]; then
+        if [[ "$GERRIT_TOPIC" =~ baremetal ]] && [[ "$JOB_NAME" =~ baremetal ]]; then
+            DEPLOY_SCENARIO='os-nosdn-nofeature-ha'
+        else
+            DEPLOY_SCENARIO='os-nosdn-nofeature-noha'
+        fi
+    fi
+    # save the scenario names into java properties file
+    # so they can be injected to downstream jobs via envInject
+    echo "Recording the scenario '${DEPLOY_SCENARIO}' for downstream jobs"
+    echo "DEPLOY_SCENARIO=${DEPLOY_SCENARIO}" > "$WORK_DIRECTORY/scenario.properties"
+}
+
+# ensure GERRIT vars are set
+[ -n "${GERRIT_CHANGE_NUMBER}" ] || exit 1
+GERRIT_TOPIC="${GERRIT_TOPIC:-''}"
+GERRIT_EVENT_COMMENT_TEXT="${GERRIT_EVENT_COMMENT_TEXT:-''}"
+
+# this directory is where the temporary properties file will be stored
+WORK_DIRECTORY=/tmp/$GERRIT_CHANGE_NUMBER
+/bin/rm -rf "$WORK_DIRECTORY" && mkdir -p "$WORK_DIRECTORY"
+
+set_scenario
