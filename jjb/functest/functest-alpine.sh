a5058e0157f7ded34faba01bd97c8dbc33e7ba14
Stamatis Katsaounis <mokats@intracom-telecom.com>
2018-10-04 15:03:15 +0300

    Provide libvirt credentials
    JIRA: FUNCTEST-1004
    
    This patch provides lbivirt credentials which are required by intel
    testcases run only in fuel at os-nosdn-nofeature-noha scenario.
    
    Change-Id: I18eeb5dd78c09ea88a0ad49428584824547b6a89
    Signed-off-by: Stamatis Katsaounis <mokats@intracom-telecom.com>

--- jjb/functest/functest-alpine.sh
+++ jjb/functest/functest-alpine.sh
@@ -138,6 +138,10 @@ if [ "${INSTALLER_TYPE}" == 'fuel' ]; then
     envs="${envs} -e POD_ARCH=${COMPUTE_ARCH}"
 fi
 
+if [[ ${INSTALLER_TYPE} == 'fuel' && ${DEPLOY_SCENARIO} == 'os-nosdn-nofeature-noha' ]]; then
+    libvirt_vol="-v ${ssh_key}:${FUNCTEST_DIR}/conf/libvirt_key"
+    envs="${envs} -e LIBVIRT_USER=ubuntu -e LIBVIRT_KEY_PATH=${FUNCTEST_DIR}/conf/libvirt_key"
+fi
 
 if [[ ${INSTALLER_TYPE} == 'compass' && ${DEPLOY_SCENARIO} =~ 'sfc' ]]; then
     ssh_key="/tmp/id_rsa"
@@ -150,7 +154,7 @@ if [[ ${INSTALLER_TYPE} == 'compass' && ${DEPLOY_SCENARIO} =~ 'sfc' ]]; then
 fi
 
 
-volumes="${images_vol} ${results_vol} ${sshkey_vol} ${userconfig_vol} ${rc_file_vol} ${cacert_file_vol}"
+volumes="${images_vol} ${results_vol} ${sshkey_vol} ${libvirt_vol} ${userconfig_vol} ${rc_file_vol} ${cacert_file_vol}"
 
 ret_val_file="${HOME}/opnfv/functest/results/${BRANCH##*/}/return_value"
 echo 0 > ${ret_val_file}
