532d544beffedc438c6cc7f2f59d1880d05c779d
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-10-30 12:32:33 -0700

    [Apex] Move Functest Scenario selection to script
    When using 'shell' builder directly '{}' is interpreted as a JJB
    substitution. This can be escaped with '{{}}' but breaks due to Apex
    use of Jinja2. Moving the entire script to a shell fixes this issue.
    
    Change-Id: I874a45cf25ca69373275849f98c89c534f3de0bb
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/apex/apex-functest-scenario.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+features=$(echo $DEPLOY_SCENARIO | sed -r -n 's/os-.+-(.+)-(noha|ha)/\1/p')
+if [ "$features" == 'rocky' ]; then
+  functest_scenario=$(echo $DEPLOY_SCENARIO | sed -r -n 's/(os-.+?)-rocky-(noha|ha)/\1-nofeature-\2/p')
+  echo "DOCKER_TAG=hunter" > functest_scenario
+elif [[ "$features" =~ 'rocky' ]]; then
+  functest_scenario=$(echo $DEPLOY_SCENARIO | sed -r -n 's/(os-.+?)-(.+)_rocky-(noha|ha)/\1-\2-\3/p')
+  echo "DOCKER_TAG=hunter" > functest_scenario
+else
+  functest_scenario=$DEPLOY_SCENARIO
+  echo "DOCKER_TAG=$([[ ${BRANCH##*/} == "master" ]] && \
+    echo "latest" || echo ${BRANCH##*/})" > functest_scenario
+fi
+echo "DEPLOY_SCENARIO=$functest_scenario" >> functest_scenario
