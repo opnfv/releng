eb72439885a4614c83213dc4b15fc7d629d29aba
Cédric Ollivier <cedric.ollivier@orange.com>
2020-08-03 11:28:11 +0200

    Remove Apex jobs
    Apex dropped a few years ago and false dependencies between Apex and
    Functest jobs forbid Functest maintainance.
    
    Change-Id: Ic63995325a281e0308abc28e9a89e8054c123391
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- jjb/apex/update-build-result.groovy
+++
@@ -1,5 +0,0 @@
-import hudson.model.*
-if (manager.logContains('^.*apex-deploy-baremetal.*SUCCESS$')
-      && manager.build.@result == hudson.model.Result.FAILURE) {
-    manager.build.@result = hudson.model.Result.UNSTABLE
-}
