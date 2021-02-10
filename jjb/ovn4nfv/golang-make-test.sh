e8aac8f2dc3933b1675072376d6f7cf7e0a13d2b
Trevor Bramwell <tbramwell@linuxfoundation.org>
2021-02-10 13:55:49 -0800

    Revert "Remove obsolete ovn4nfv jjbs"
    This reverts commit 6938d76e3081a059ddb342d3a4c186d404d0446b.
    
    Change-Id: I633f22157e054aa5fb147eb70da25f5f1f363700
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/ovn4nfv/golang-make-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 Intel Corporation.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -o errexit
+set -o pipefail
+
+source /etc/os-release || source /usr/lib/os-release
+case ${ID,,} in
+    ubuntu|debian)
+    sudo add-apt-repository -y ppa:longsleep/golang-backports
+    sudo apt-get update
+    sudo apt-get install -y build-essential golang-go
+    sudo apt-get -y clean && sudo apt-get -y autoremove
+    ;;
+esac
+
+echo "Running unit tests in Go ${golang_version} ..."
+cd $WORKSPACE
+make test
