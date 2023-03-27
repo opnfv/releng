6b0a773aef2361d6acdc0a29ed664ea23caff029
Cédric Ollivier <cedric.ollivier@orange.com>
2023-03-27 16:22:52 +0200

    Remove lfdocs_conf
    This project is unmaintained and basically useless.
    
    Change-Id: I8d373a8365166af050b397a786e7ddb34f988256
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
-from docs_conf.conf import *  # flake8: noqa
+project = 'Releng'
+extensions = [
+    'sphinx.ext.autosectionlabel'
+]
+html_theme = "piccolo_theme"
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 4
+numfig = True
+numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
+                 'code-block': 'Listing %s', 'section': 'Section %s'}
